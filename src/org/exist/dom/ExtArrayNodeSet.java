evolvedbinary/elemental

commit e495d6cd4bf5790fc673dde1abae8f85ada237d2

Small performance improvements in node sets part 3.
svn path=/trunk/eXist/; revision=5017

src/org/exist/dom/ExtArrayNodeSet.java
@@ -114,7 +114,7 @@ public class ExtArrayNodeSet extends AbstractNodeSet {
             part = parts[idx];
         } else if (create) {
             idx = - (idx + 1);
-            part = new Part(sizeHint, doc);
+            part = new Part(sizeHint);
             insertPart(doc.getDocId(), part, idx);
         }
         return part;
@@ -394,7 +394,7 @@ public class ExtArrayNodeSet extends AbstractNodeSet {
 
     public NodeSet filterDocuments(ExtArrayNodeSet otherSet) {
         LOG.debug("Filtering...");
-        ExtArrayNodeSet other = (ExtArrayNodeSet) otherSet;
+        ExtArrayNodeSet other = otherSet;
         ExtArrayNodeSet result = new ExtArrayNodeSet(partCount, other.initalSize);
         for (int i = 0; i < other.partCount; i++) {
             int idx = ArrayUtils.binarySearch(documentIds, other.documentIds[i], partCount);
@@ -612,7 +612,7 @@ public class ExtArrayNodeSet extends AbstractNodeSet {
         private NodeProxy array[];
         private int length = 0;
 
-        Part(int initialSize, DocumentImpl myDoc) {
+        Part(int initialSize) {
             array = new NodeProxy[initialSize];
         }
 
@@ -794,13 +794,13 @@ public class ExtArrayNodeSet extends AbstractNodeSet {
             int high = length - 1;
             int mid = 0;
             int cmp;
-            NodeProxy p;
+            NodeId id;
             while (low <= high) {
                 mid = (low + high) / 2;
-                p = array[mid];
-                if (p.getNodeId().isDescendantOrSelfOf(ancestorId))
+                id = array[mid].getNodeId();
+                if (id.isDescendantOrSelfOf(ancestorId))
                     break;	// found a child node, break out.
-                cmp = p.getNodeId().compareTo(ancestorId);
+                cmp = id.compareTo(ancestorId);
                 if (cmp > 0)
                     high = mid - 1;
                 else
@@ -813,18 +813,14 @@ public class ExtArrayNodeSet extends AbstractNodeSet {
                 --mid;
             NodeProxy ancestor = new NodeProxy(getDocument(), ancestorId, Node.ELEMENT_NODE);
             for (int i = mid; i < length; i++) {
-                cmp = array[i].getNodeId().computeRelation(ancestorId);
-                if (cmp > -1) {
-                    boolean add = true;
-                    if (cmp == NodeId.IS_SELF)
-                        add = includeSelf;
-                    if (add) {
-                        if (Expression.NO_CONTEXT_ID != contextId)
-                            ancestor.deepCopyContext(array[i], contextId);
-                        else
-                            ancestor.copyContext(array[i]);
+                boolean add = (includeSelf ? array[i].getNodeId().isDescendantOrSelfOf(ancestorId) :
+                        array[i].getNodeId().isDescendantOf(ancestorId));
+                if (add) {
+                    if (Expression.NO_CONTEXT_ID != contextId)
+                        ancestor.deepCopyContext(array[i], contextId);
+                    else
+                        ancestor.copyContext(array[i]);
 //                        ancestor.addMatches(array[i]);
-                    }
                 } else
                     break;
             }
@@ -849,7 +845,7 @@ public class ExtArrayNodeSet extends AbstractNodeSet {
             // document nodes are treated specially
             if (parentId == NodeId.DOCUMENT_NODE) {
             	for (int i = 0; i < length; i++) {
-            		boolean add = false;
+            		boolean add;
             		if (childOnly)
             			add = array[i].getNodeId().getTreeLevel() == 1;
             		else if (includeSelf)

src/org/exist/collections/CollectionConfigurationManager.java
@@ -117,7 +117,6 @@ public class CollectionConfigurationManager {
      * 
      * @param broker
      * @param collection
-     * @param collectionPath
      * @return The collection configuration
      * @throws CollectionConfigurationException
      */
@@ -143,14 +142,16 @@ public class CollectionConfigurationManager {
     			        DocumentImpl confDoc = (DocumentImpl) i.next();
     			        if(confDoc.getFileURI().endsWith(CollectionConfiguration.COLLECTION_CONFIG_SUFFIX_URI)) {
                             if (!configFound) {
-                                LOG.debug("Reading collection configuration for '" + collection.getURI() + "' from '" + confDoc.getURI() + "'");
+                                if (LOG.isTraceEnabled())
+                                    LOG.trace("Reading collection configuration for '" + collection.getURI() + "' from '" + confDoc.getURI() + "'");
         			            conf.read(broker, confDoc, path, confDoc.getFileURI());                            
                                 configFound = true;
                                 //Allow just one configuration document per collection
                                 //TODO : do not break if a system property allows several ones -pb
         			            break;
                             } else {
-                                LOG.debug("Found another collection configuration for '" + collection.getURI() + "' in '" + confDoc.getURI() + "'");
+                                if (LOG.isDebugEnabled())
+                                    LOG.debug("Found another collection configuration for '" + collection.getURI() + "' in '" + confDoc.getURI() + "'");
                             }
     			        }
     			    }                    
@@ -162,7 +163,8 @@ public class CollectionConfigurationManager {
     		path = path.removeLastSegment();
 	    }
         if (!configFound) {
-            LOG.debug("Reading collection configuration for '" + collection.getURI() + "' from index configuration");
+            if (LOG.isTraceEnabled())
+                LOG.trace("Reading collection configuration for '" + collection.getURI() + "' from index configuration");
             // use default configuration
             conf.setIndexConfiguration(broker.getIndexConfiguration());
         }

src/org/exist/numbering/DLN.java
@@ -215,8 +215,8 @@ public class DLN extends DLNBase implements NodeId {
 
     public boolean isDescendantOrSelfOf(NodeId other) {
     	DLN ancestor = (DLN) other;
-        return startsWith((DLN) ancestor) &&
-        	(bitIndex == ancestor.bitIndex || isLevelSeparator(((DLN)ancestor).bitIndex + 1));
+        return startsWith(ancestor) &&
+        	(bitIndex == ancestor.bitIndex || isLevelSeparator((ancestor).bitIndex + 1));
     }
 
     public boolean isChildOf(NodeId parent) {

commit 0c9a220ae5c90309b01c5df65ad219d94c086461

Fixed regression introduced with r5017.
svn path=/trunk/eXist/; revision=5026

src/org/exist/dom/ExtArrayNodeSet.java
@@ -813,14 +813,18 @@ public class ExtArrayNodeSet extends AbstractNodeSet {
                 --mid;
             NodeProxy ancestor = new NodeProxy(getDocument(), ancestorId, Node.ELEMENT_NODE);
             for (int i = mid; i < length; i++) {
-                boolean add = (includeSelf ? array[i].getNodeId().isDescendantOrSelfOf(ancestorId) :
-                        array[i].getNodeId().isDescendantOf(ancestorId));
-                if (add) {
-                    if (Expression.NO_CONTEXT_ID != contextId)
-                        ancestor.deepCopyContext(array[i], contextId);
-                    else
-                        ancestor.copyContext(array[i]);
+                cmp = array[i].getNodeId().computeRelation(ancestorId);
+                if (cmp > -1) {
+                    boolean add = true;
+                    if (cmp == NodeId.IS_SELF)
+                        add = includeSelf;
+                    if (add) {
+                        if (Expression.NO_CONTEXT_ID != contextId)
+                            ancestor.deepCopyContext(array[i], contextId);
+                        else
+                            ancestor.copyContext(array[i]);
 //                        ancestor.addMatches(array[i]);
+                    }
                 } else
                     break;
             }