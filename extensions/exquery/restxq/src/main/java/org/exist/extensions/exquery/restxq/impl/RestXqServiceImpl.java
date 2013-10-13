evolvedbinary/elemental

commit a35c933c31794856017f514dfde1986f8662a42e

[bugfix] Improve typing of function parameters. Fixed POST and PUT with Request bodies
svn path=/trunk/eXist/; revision=17052

extensions/exquery/restxq/src/main/java/org/exist/extensions/exquery/restxq/impl/RestXqServiceImpl.java
@@ -32,8 +32,12 @@ import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.io.input.CloseShieldInputStream;
 import org.apache.log4j.Logger;
+import org.exist.extensions.exquery.xdm.type.impl.BinaryTypedValue;
+import org.exist.extensions.exquery.xdm.type.impl.DocumentTypedValue;
+import org.exist.extensions.exquery.xdm.type.impl.StringTypedValue;
 import org.exist.external.org.apache.commons.io.output.ByteArrayOutputStream;
 import org.exist.memtree.DocumentBuilderReceiver;
+import org.exist.memtree.DocumentImpl;
 import org.exist.memtree.MemTreeBuilder;
 import org.exist.storage.BrokerPool;
 import org.exist.util.Configuration;
@@ -47,8 +51,6 @@ import org.exist.xquery.value.Base64BinaryValueType;
 import org.exist.xquery.value.BinaryValue;
 import org.exist.xquery.value.BinaryValueFromInputStream;
 import org.exist.xquery.value.BinaryValueManager;
-import org.exist.xquery.value.Item;
-import org.exist.xquery.value.NodeValue;
 import org.exist.xquery.value.StringValue;
 import org.exquery.http.HttpRequest;
 import org.exquery.http.HttpResponse;
@@ -58,10 +60,8 @@ import org.exquery.restxq.RestXqErrorCodes;
 import org.exquery.restxq.RestXqServiceException;
 import org.exquery.restxq.RestXqServiceSerializer;
 import org.exquery.restxq.impl.AbstractRestXqService;
-import org.exquery.xdm.type.AbstractTypedValue;
 import org.exquery.xdm.type.SequenceImpl;
 import org.exquery.xquery.Sequence;
-import org.exquery.xquery.Type;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -157,7 +157,7 @@ public class RestXqServiceImpl extends AbstractRestXqService {
             throw new RestXqServiceException(RestXqErrorCodes.RQDY0014, ioe);
         }
 
-        Item result = null;
+        Sequence result = null;
         try {
 
             //was there any POST content?
@@ -186,7 +186,11 @@ public class RestXqServiceImpl extends AbstractRestXqService {
 
                         //binary data
                         try {
-                            result = BinaryValueFromInputStream.getInstance(binaryValueManager, new Base64BinaryValueType(), is);
+                            
+                            final BinaryValue binaryValue = BinaryValueFromInputStream.getInstance(binaryValueManager, new Base64BinaryValueType(), is);
+                            if(binaryValue != null) {
+                                result = new SequenceImpl<BinaryValue>(new BinaryTypedValue(binaryValue));
+                            }
                         } catch(XPathException xpe) {
                             throw new RestXqServiceException(RestXqErrorCodes.RQDY0014, xpe);
                         }
@@ -195,7 +199,10 @@ public class RestXqServiceImpl extends AbstractRestXqService {
 
                 if(result == null) {
                     //2) not binary, try and parse as an XML documemnt
-                    result = parseAsXml(is);
+                    final DocumentImpl doc = parseAsXml(is);
+                    if(doc != null) {
+                        result = new SequenceImpl<Document>(new DocumentTypedValue(doc));
+                    }
                 }
 
                 if(result == null) {
@@ -210,7 +217,10 @@ public class RestXqServiceImpl extends AbstractRestXqService {
                         //reset the stream, as we need to reuse for string parsing
                         is.reset();
 
-                        result = parseAsString(is, encoding);
+                        final StringValue str = parseAsString(is, encoding);
+                        if(str != null) {
+                            result = new SequenceImpl<StringValue>(new StringTypedValue(str));
+                        }
                     } catch(IOException ioe) {
                         throw new RestXqServiceException(RestXqErrorCodes.RQDY0014, ioe);
                     }
@@ -236,19 +246,20 @@ public class RestXqServiceImpl extends AbstractRestXqService {
             }
         }
         
-        return new SequenceImpl<Item>(new ItemTypedValue(result));
+        return result;
     }
     
+    /*
     private class ItemTypedValue extends AbstractTypedValue<Item> {
 
         public ItemTypedValue(final Item value) {
             super(Type.ITEM, value);
         }
-    }
+    }*/
     
-    private NodeValue parseAsXml(final InputStream is) {
+    private DocumentImpl parseAsXml(final InputStream is) {
 
-        NodeValue result = null;
+        DocumentImpl result = null;
         XMLReader reader = null;
 
         try {
@@ -264,9 +275,9 @@ public class RestXqServiceImpl extends AbstractRestXqService {
             reader.setContentHandler(receiver);
             reader.parse(src);
             builder.endDocument();
-            Document doc = receiver.getDocument();
+            final Document doc = receiver.getDocument();
 
-            result = (NodeValue)doc;
+            result = (DocumentImpl)doc;
         } catch(SAXException saxe) {
             //do nothing, we will default to trying to return a string below
         } catch(IOException ioe) {

extensions/exquery/restxq/src/main/java/org/exist/extensions/exquery/restxq/impl/ResourceFunctionExecutorImpl.java
@@ -31,9 +31,9 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.log4j.Logger;
 import org.exist.EXistException;
 import org.exist.dom.QName;
-import org.exist.xmldb.XmldbURI;
 import org.exist.extensions.exquery.restxq.RestXqServiceCompiledXQueryCache;
 import org.exist.extensions.exquery.restxq.impl.adapters.SequenceAdapter;
 import org.exist.extensions.exquery.restxq.impl.adapters.TypeAdapter;
@@ -43,6 +43,7 @@ import org.exist.security.PermissionDeniedException;
 import org.exist.storage.BrokerPool;
 import org.exist.storage.DBBroker;
 import org.exist.storage.ProcessMonitor;
+import org.exist.xmldb.XmldbURI;
 import org.exist.xquery.AbstractExpression;
 import org.exist.xquery.AnalyzeContextInfo;
 import org.exist.xquery.CompiledXQuery;
@@ -54,6 +55,9 @@ import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
 import org.exist.xquery.util.ExpressionDumper;
 import org.exist.xquery.value.AnyURIValue;
+import org.exist.xquery.value.AtomicValue;
+import org.exist.xquery.value.Base64BinaryValueType;
+import org.exist.xquery.value.BinaryValueFromInputStream;
 import org.exist.xquery.value.BooleanValue;
 import org.exist.xquery.value.DateTimeValue;
 import org.exist.xquery.value.DateValue;
@@ -83,6 +87,8 @@ import org.exquery.xquery3.FunctionSignature;
  * @author Adam Retter <adam.retter@googlemail.com>
  */
 public class ResourceFunctionExecutorImpl implements ResourceFunctionExecuter {
+    
+    private final static Logger LOG = Logger.getLogger(ResourceFunctionExecutorImpl.class);
     
     private final BrokerPool brokerPool;
 
@@ -242,38 +248,57 @@ public class ResourceFunctionExecutorImpl implements ResourceFunctionExecuter {
     }
 
     
-    //TODO this needs to be abstracted into EXQuery library
-    private <X> TypedValue<X> convertToType(final TypedValue typedValue, final org.exquery.xquery.Type destinationType, final Class<X> underlyingDestinationClass) throws RestXqServiceException {
+    //TODO this needs to be abstracted into EXQuery library / or not, see the TODOs below
+    private <X> TypedValue<X> convertToType(final XQueryContext xqueryContext, final String argumentName, final TypedValue typedValue, final org.exquery.xquery.Type destinationType, final Class<X> underlyingDestinationClass) throws RestXqServiceException {
         
-        //TODO check type conversion from typedValue.getType() -> destinationType is even possible? if not throw exception
         //TODO consider changing Types that can be used as <T> to TypedValue to a set of interfaces for XDM types that
         //require absolute minimal implementation, and we provide some default or abstract implementations if possible
         
         final Item convertedValue;
         try {
-            convertedValue = new StringValue((String)typedValue.getValue()).convertTo(TypeAdapter.toExistType(destinationType));
-        } catch(XPathException xpe) {
-            throw new RestXqServiceException("TODO need to implement error code for problem with parameter conversion!");
-        }
-        
-        if(typedValue.getType() == org.exquery.xquery.Type.STRING) {
+            final int existDestinationType = TypeAdapter.toExistType(destinationType);
             
-            return new TypedValue<X>() {
-
-                @Override
-                public org.exquery.xquery.Type getType() {
-                    return destinationType;
-                }
-
-                @Override
-                public X getValue() {
-                    return (X)convertedValue;
-                }
-            };
+            final Item value;
             
-        } else {
-            return null;
+            //TODO This type system is a complete mess:
+            //EXQuery XDM should not have any concrete types, just interfaces
+            //some of the abstract code in EXQuery needs to be able to instantiate types.
+            //Consider a factory or java.util.ServiceLoader pattern
+            if(typedValue instanceof org.exquery.xdm.type.StringTypedValue) {
+                value = new StringValue(((org.exquery.xdm.type.StringTypedValue)typedValue).getValue());
+            } else if(typedValue instanceof org.exquery.xdm.type.Base64BinaryTypedValue) {
+                value = BinaryValueFromInputStream.getInstance(xqueryContext, new Base64BinaryValueType(), ((org.exquery.xdm.type.Base64BinaryTypedValue)typedValue).getValue());
+            } else {
+                value = (Item)typedValue.getValue();
+            }
+
+            if(existDestinationType == value.getType()) {
+                convertedValue = value;
+            } else if(value instanceof AtomicValue) {
+                convertedValue = value.convertTo(existDestinationType);
+            } else {
+                LOG.warn("Could not convert parameter '" + argumentName + "' from '" + typedValue.getType().name() + "' to '" + destinationType.name() + "'.");
+                convertedValue = value;
+            }
+            
+        } catch(final XPathException xpe) {
+            //TODO define an ErrorCode
+            throw new RestXqServiceException("TODO need to implement error code for problem with parameter conversion!: " +  xpe.getMessage(), xpe);
         }
+            
+        return new TypedValue<X>() {
+
+            @Override
+            public org.exquery.xquery.Type getType() {
+                //return destinationType;
+                return TypeAdapter.toExQueryType(convertedValue.getType());
+            }
+
+            @Override
+            public X getValue() {
+                return (X)convertedValue;
+            }
+        };
     }
     
     private org.exist.xquery.value.Sequence convertToExistSequence(final XQueryContext xqueryContext, final TypedArgumentValue argument, final int fnParameterType) throws RestXqServiceException, XPathException  {
@@ -344,7 +369,7 @@ public class ResourceFunctionExecutorImpl implements ResourceFunctionExecuter {
                     destinationClass = Item.class;
             }
             
-            final TypedValue<? extends Item> val = convertToType(value, destinationType, destinationClass);
+            final TypedValue<? extends Item> val = convertToType(xqueryContext, argument.getArgumentName(), value, destinationType, destinationClass);
             
             sequence.add(val.getValue());
         }

extensions/exquery/restxq/src/main/java/org/exist/extensions/exquery/xdm/type/impl/BinaryTypedValue.java
@@ -1,0 +1,43 @@
+/*
+Copyright (c) 2012, Adam Retter
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Adam Retter Consulting nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Adam Retter BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.exist.extensions.exquery.xdm.type.impl;
+
+import org.exist.extensions.exquery.restxq.impl.adapters.TypeAdapter;
+import org.exist.xquery.value.BinaryValue;
+import org.exquery.xdm.type.AbstractTypedValue;
+
+/**
+ * eXist-db xs:base64Binary and xs:hexBinary Typed Values
+ *
+ * @author Adam Retter <adam.retter@googlemail.com>
+ */
+public class BinaryTypedValue extends AbstractTypedValue<BinaryValue> {
+    
+    public BinaryTypedValue(final BinaryValue value) {
+        super(TypeAdapter.toExQueryType(value.getType()), value);
+    }
+}

extensions/exquery/restxq/src/main/java/org/exist/extensions/exquery/xdm/type/impl/DocumentTypedValue.java
@@ -1,0 +1,47 @@
+/*
+Copyright (c) 2012, Adam Retter
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Adam Retter Consulting nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Adam Retter BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.exist.extensions.exquery.xdm.type.impl;
+
+import org.exquery.xdm.type.AbstractTypedValue;
+import org.exquery.xquery.Type;
+import org.w3c.dom.Document;
+
+/**
+ * eXist-db document() Typed value
+ *
+ * @author Adam Retter <adam.retter@googlemail.com>
+ */
+public class DocumentTypedValue extends AbstractTypedValue<Document> {
+
+    public DocumentTypedValue(final org.exist.dom.DocumentImpl document) {
+        super(Type.DOCUMENT, document);
+    }
+    
+    public DocumentTypedValue(final org.exist.memtree.DocumentImpl document) {
+        super(Type.DOCUMENT, document);
+    }   
+}

extensions/exquery/restxq/src/main/java/org/exist/extensions/exquery/xdm/type/impl/StringTypedValue.java
@@ -1,0 +1,43 @@
+/*
+Copyright (c) 2012, Adam Retter
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Adam Retter Consulting nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Adam Retter BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.exist.extensions.exquery.xdm.type.impl;
+
+import org.exist.xquery.value.StringValue;
+import org.exquery.xdm.type.AbstractTypedValue;
+import org.exquery.xquery.Type;
+
+/**
+ * eXist-db String Typed Value
+ *
+ * @author Adam Retter <adam.retter@googlemail.com>
+ */
+public class StringTypedValue extends AbstractTypedValue<StringValue> {
+
+    public StringTypedValue(final StringValue stringValue) {
+        super(Type.STRING, stringValue);
+    }
+}

commit ec9eef1480d12d2a907f51ed7abecdc442e33430

[bugfix] A regression was introduced in Git commit a35c933 which closed the input stream of HTTP Request bodies too early when they were binary values. This commit fixes that.

extensions/exquery/restxq/src/main/java/org/exist/extensions/exquery/restxq/impl/RestXqServiceImpl.java
@@ -62,6 +62,7 @@ import org.exquery.restxq.RestXqServiceSerializer;
 import org.exquery.restxq.impl.AbstractRestXqService;
 import org.exquery.xdm.type.SequenceImpl;
 import org.exquery.xquery.Sequence;
+import org.exquery.xquery.Type;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -236,12 +237,27 @@ public class RestXqServiceImpl extends AbstractRestXqService {
                 }
             }
 
-            //dont close the stream if its a binary value, because we will need it later for serialization
-            if(is != null && !(result instanceof BinaryValue)) {
-                try {
-                    is.close();
-                } catch(final IOException ioe) {
-                    LOG.error(ioe.getMessage(), ioe);
+            if(is != null) {
+                /*
+                 * Do NOT close the stream if its a binary value,
+                 * because we will need it later for serialization
+                 */
+                boolean isBinaryType = false;
+                if(result != null) {
+                    try {
+                        final Type type = result.head().getType();
+                        isBinaryType = (type == Type.BASE64_BINARY || type == Type.HEX_BINARY);
+                    } catch(final IndexOutOfBoundsException ioe) {
+                        LOG.warn("Called head on an empty HTTP Request body sequence", ioe);
+                    }
+                }
+                
+                if(!isBinaryType) {
+                    try {
+                        is.close();
+                    } catch(final IOException ioe) {
+                        LOG.error(ioe.getMessage(), ioe);
+                    }
                 }
             }
         }

extensions/exquery/restxq/src/main/java/org/exist/extensions/exquery/restxq/impl/adapters/SequenceAdapter.java
@@ -76,24 +76,7 @@ public class SequenceAdapter implements Sequence<Item> {
 
             @Override
             public TypedValue<Item> next() {
-                return new TypedValue<Item>(){
-
-                    final Item item = getIterator().nextItem();
-                    
-                    @Override
-                    public Type getType() {
-                        return TypeAdapter.toExQueryType(item.getType());
-                    }
-
-                    @Override
-                    public Item getValue() {
-                        if(item instanceof NodeProxy) {
-                            return DomEnhancingNodeProxyAdapter.create((NodeProxy)item); //RESTXQ expects to find DOM Nodes not NodeProxys
-                        } else {
-                            return item;
-                        }
-                    }
-                };
+                return createTypedValue(getIterator().nextItem());
             }
 
             @Override
@@ -102,7 +85,34 @@ public class SequenceAdapter implements Sequence<Item> {
             }
         };
     }
+    
+    private TypedValue<Item> createTypedValue(final Item item) {
+        return new TypedValue<Item>(){
+            @Override
+            public Type getType() {
+                return TypeAdapter.toExQueryType(item.getType());
+            }
 
+            @Override
+            public Item getValue() {
+                if(item instanceof NodeProxy) {
+                    return DomEnhancingNodeProxyAdapter.create((NodeProxy)item); //RESTXQ expects to find DOM Nodes not NodeProxys
+                } else {
+                    return item;
+                }
+            }
+        };
+    }
+
+    @Override
+    public TypedValue<Item> head() {
+        if(sequence.isEmpty()) {
+            return Sequence.EMPTY_SEQUENCE.head();
+        } else {
+            return createTypedValue(sequence.itemAt(0));
+        }
+    }
+    
     @Override
     public Sequence<Item> tail() {
         try {