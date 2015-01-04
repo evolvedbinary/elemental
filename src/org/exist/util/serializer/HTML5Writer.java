eXist-db/exist

[feature] Add HTML5 serialiser, which does not produce well-formed XHTML but follows the HTML5 syntax: empty elements are not closed; characters in script, style and textarea elements are not escaped; empty attributes do not have a value. Old HTML5 serialiser has been renamed to XHTML5 serialiser.

src/org/exist/util/serializer/HTML5Writer.java
@@ -1,92 +1,173 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-2014 The eXist-db Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  $Id$
+ */
 package org.exist.util.serializer;
 
-import java.io.Writer;
-import javax.xml.transform.TransformerException;
+import org.exist.dom.QName;
 import org.exist.util.hashtable.ObjectHashSet;
 
-public class HTML5Writer extends XHTMLWriter {
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+import java.io.Writer;
 
-    private final static ObjectHashSet<String> inlineTags = new ObjectHashSet<String>(31);
-    
+/**
+ * HTML5 writer which does not produce well-formed XHTML.
+ *
+ * @author Wolfgang
+ */
+public class HTML5Writer extends XHTML5Writer {
+
+    private final static ObjectHashSet<String> EMPTY_TAGS = new ObjectHashSet<String>(31);
     static {
-    	inlineTags.add("a");
-    	inlineTags.add("abbr");
-    	inlineTags.add("area");
-    	inlineTags.add("audio");
-    	inlineTags.add("b");
-    	inlineTags.add("bdi");
-    	inlineTags.add("bdo");
-    	inlineTags.add("br");
-    	inlineTags.add("button");
-    	inlineTags.add("canvas");
-    	inlineTags.add("cite");
-    	inlineTags.add("code");
-    	inlineTags.add("command");
-    	inlineTags.add("datalist");
-    	inlineTags.add("del");
-    	inlineTags.add("dfn");
-    	inlineTags.add("em");
-    	inlineTags.add("embed");
-    	inlineTags.add("i");
-    	inlineTags.add("img");
-    	inlineTags.add("ins");
-    	inlineTags.add("input");
-    	inlineTags.add("kbd");
-    	inlineTags.add("keygen");
-    	inlineTags.add("label");
-    	inlineTags.add("map");
-    	inlineTags.add("mark");
-    	inlineTags.add("math");
-    	inlineTags.add("meter");
-    	inlineTags.add("object");
-    	inlineTags.add("output");
-    	inlineTags.add("progress");
-    	inlineTags.add("q");
-    	inlineTags.add("ruby");
-    	inlineTags.add("s");
-    	inlineTags.add("samp");
-    	inlineTags.add("select");
-    	inlineTags.add("small");
-    	inlineTags.add("span");
-    	inlineTags.add("strong");
-    	inlineTags.add("sub");
-    	inlineTags.add("sup");
-    	inlineTags.add("svg");
-    	inlineTags.add("textarea");
-    	inlineTags.add("time");
-    	inlineTags.add("tt");
-    	inlineTags.add("u");
-    	inlineTags.add("var");
-    	inlineTags.add("video");
+        EMPTY_TAGS.add("area");
+        EMPTY_TAGS.add("base");
+        EMPTY_TAGS.add("br");
+        EMPTY_TAGS.add("col");
+        EMPTY_TAGS.add("embed");
+        EMPTY_TAGS.add("hr");
+        EMPTY_TAGS.add("img");
+        EMPTY_TAGS.add("input");
+        EMPTY_TAGS.add("keygen");
+        EMPTY_TAGS.add("link");
+        EMPTY_TAGS.add("meta");
+        EMPTY_TAGS.add("param");
+        EMPTY_TAGS.add("source");
+        EMPTY_TAGS.add("track");
+        EMPTY_TAGS.add("wbr");
     }
-    
+
+    private final static ObjectHashSet<String> RAW_TEXT_ELEMENTS = new ObjectHashSet<String>(31);
+    static {
+        RAW_TEXT_ELEMENTS.add("script");
+        RAW_TEXT_ELEMENTS.add("style");
+        RAW_TEXT_ELEMENTS.add("textarea");
+        RAW_TEXT_ELEMENTS.add("title");
+    }
+
     public HTML5Writer() {
-            super();
+        super(EMPTY_TAGS, INLINE_TAGS);
     }
 
-    public HTML5Writer(final Writer writer) {
-            super(writer);
+    public HTML5Writer(Writer writer) {
+        super(writer, EMPTY_TAGS, INLINE_TAGS);
     }
 
     @Override
-    protected void writeDoctype(final String rootElement) throws TransformerException {
-        if(doctypeWritten) {
-            return;
+    public void endElement(QName qname) throws TransformerException {
+        if (!isEmptyTag(qname.getLocalName())) {
+            super.endElement(qname);
+        } else {
+            closeStartTag(true);
+            endIndent(qname.getNamespaceURI(), qname.getLocalName());
         }
-
-        documentType(rootElement, null, null);
-        doctypeWritten = true;
     }
 
     @Override
-    protected boolean isInlineTag(final String namespaceURI, final String localName) {
-        return inlineTags.contains(localName);
+    public void endElement(String namespaceURI, String localName, String qname) throws TransformerException {
+        if (!isEmptyTag(localName)) {
+            super.endElement(namespaceURI, localName, qname);
+        } else {
+            closeStartTag(true);
+            endIndent(namespaceURI, localName);
+        }
     }
 
     @Override
-    protected boolean needsEscape(final char ch) {
-        if("script".equals(currentTag)) {
-            return !(ch == '<' || ch == '>' || ch == '&');
+    public void attribute(String qname, String value) throws TransformerException {
+        try {
+            if(!tagIsOpen) {
+                characters(value);
+                return;
+            }
+            final Writer writer = getWriter();
+            writer.write(' ');
+            writer.write(qname);
+            if (!qname.equals(value)) {
+                writer.write("=\"");
+                writeChars(value, true);
+                writer.write('"');
+            }
+        } catch(final IOException ioe) {
+            throw new TransformerException(ioe.getMessage(), ioe);
+        }
+    }
+
+    @Override
+    public void attribute(QName qname, String value) throws TransformerException {
+        try {
+            if(!tagIsOpen) {
+                characters(value);
+                return;
+                // throw new TransformerException("Found an attribute outside an
+                // element");
+            }
+            final Writer writer = getWriter();
+            writer.write(' ');
+            if(qname.getPrefix() != null && qname.getPrefix().length() > 0) {
+                writer.write(qname.getPrefix());
+                writer.write(':');
+            }
+            if (!qname.getLocalName().equals(value)) {
+                writer.write(qname.getLocalName());
+                writer.write("=\"");
+                writeChars(value, true);
+                writer.write('"');
+            }
+        } catch(final IOException ioe) {
+            throw new TransformerException(ioe.getMessage(), ioe);
+        }
+    }
+
+    @Override
+    public void namespace(String prefix, String nsURI) throws TransformerException {
+        // no namespaces allowed in HTML5
+    }
+
+    @Override
+    protected void closeStartTag(boolean isEmpty) throws TransformerException {
+        try {
+            if (tagIsOpen) {
+                if (isEmpty) {
+                    if (isEmptyTag(currentTag)) {
+                        getWriter().write(">");
+                    } else {
+                        getWriter().write('>');
+                        getWriter().write("</");
+                        getWriter().write(currentTag);
+                        getWriter().write('>');
+                    }
+                } else {
+                    getWriter().write('>');
+                }
+                tagIsOpen = false;
+            }
+        } catch (final IOException ioe) {
+            throw new TransformerException(ioe.getMessage(), ioe);
+        }
+    }
+
+    @Override
+    protected boolean needsEscape(char ch) {
+        if (RAW_TEXT_ELEMENTS.contains(currentTag)) {
+            return false;
         }
         return super.needsEscape(ch);
     }

src/org/exist/util/serializer/IndentingXMLWriter.java
@@ -66,7 +66,7 @@ public class IndentingXMLWriter extends XMLWriter {
             indent();
         }
         super.startElement(namespaceURI, localName, qname);
-        level++;
+        addIndent();
         afterTag = true;
         sameline = true;
     }
@@ -80,7 +80,7 @@ public class IndentingXMLWriter extends XMLWriter {
             indent();
         }
         super.startElement(qname);
-        level++;
+        addIndent();
         afterTag = true;
         sameline = true;
     }
@@ -90,10 +90,7 @@ public class IndentingXMLWriter extends XMLWriter {
      */
     @Override
     public void endElement(final String namespaceURI, final String localName, final String qname) throws TransformerException {
-        level--;
-        if (afterTag && !sameline && !isInlineTag(namespaceURI, localName)){
-            indent();
-        }
+        endIndent(namespaceURI, localName);
         super.endElement(namespaceURI, localName, qname);
         sameline = false;
         afterTag = true;
@@ -104,10 +101,7 @@ public class IndentingXMLWriter extends XMLWriter {
      */
     @Override
     public void endElement(final QName qname) throws TransformerException {
-        level--;
-        if (afterTag && !sameline && !isInlineTag(qname.getNamespaceURI(), qname.getLocalName())){
-            indent();
-        }
+        endIndent(qname.getNamespaceURI(), qname.getLocalName());
         super.endElement(qname);
         sameline = false;
         afterTag = true;
@@ -197,6 +191,17 @@ public class IndentingXMLWriter extends XMLWriter {
         super.characters("\n");
         super.characters(indentChars.subSequence(0, spaces));
         sameline = false;
+    }
+
+    protected void addIndent() {
+        level++;
+    }
+
+    protected void endIndent(String namespaceURI, String localName) throws TransformerException {
+        level--;
+        if (afterTag && !sameline && !isInlineTag(namespaceURI, localName)){
+            indent();
+        }
     }
 
     protected static boolean isWhiteSpace(final char ch) {

evolvedbinary/elemental

src/org/exist/util/serializer/HTML5Writer.java
@@ -1,92 +1,173 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-2014 The eXist-db Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  $Id$
+ */
 package org.exist.util.serializer;
 
-import java.io.Writer;
-import javax.xml.transform.TransformerException;
+import org.exist.dom.QName;
 import org.exist.util.hashtable.ObjectHashSet;
 
-public class HTML5Writer extends XHTMLWriter {
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+import java.io.Writer;
 
-    private final static ObjectHashSet<String> inlineTags = new ObjectHashSet<String>(31);
-    
+/**
+ * HTML5 writer which does not produce well-formed XHTML.
+ *
+ * @author Wolfgang
+ */
+public class HTML5Writer extends XHTML5Writer {
+
+    private final static ObjectHashSet<String> EMPTY_TAGS = new ObjectHashSet<String>(31);
     static {
-    	inlineTags.add("a");
-    	inlineTags.add("abbr");
-    	inlineTags.add("area");
-    	inlineTags.add("audio");
-    	inlineTags.add("b");
-    	inlineTags.add("bdi");
-    	inlineTags.add("bdo");
-    	inlineTags.add("br");
-    	inlineTags.add("button");
-    	inlineTags.add("canvas");
-    	inlineTags.add("cite");
-    	inlineTags.add("code");
-    	inlineTags.add("command");
-    	inlineTags.add("datalist");
-    	inlineTags.add("del");
-    	inlineTags.add("dfn");
-    	inlineTags.add("em");
-    	inlineTags.add("embed");
-    	inlineTags.add("i");
-    	inlineTags.add("img");
-    	inlineTags.add("ins");
-    	inlineTags.add("input");
-    	inlineTags.add("kbd");
-    	inlineTags.add("keygen");
-    	inlineTags.add("label");
-    	inlineTags.add("map");
-    	inlineTags.add("mark");
-    	inlineTags.add("math");
-    	inlineTags.add("meter");
-    	inlineTags.add("object");
-    	inlineTags.add("output");
-    	inlineTags.add("progress");
-    	inlineTags.add("q");
-    	inlineTags.add("ruby");
-    	inlineTags.add("s");
-    	inlineTags.add("samp");
-    	inlineTags.add("select");
-    	inlineTags.add("small");
-    	inlineTags.add("span");
-    	inlineTags.add("strong");
-    	inlineTags.add("sub");
-    	inlineTags.add("sup");
-    	inlineTags.add("svg");
-    	inlineTags.add("textarea");
-    	inlineTags.add("time");
-    	inlineTags.add("tt");
-    	inlineTags.add("u");
-    	inlineTags.add("var");
-    	inlineTags.add("video");
+        EMPTY_TAGS.add("area");
+        EMPTY_TAGS.add("base");
+        EMPTY_TAGS.add("br");
+        EMPTY_TAGS.add("col");
+        EMPTY_TAGS.add("embed");
+        EMPTY_TAGS.add("hr");
+        EMPTY_TAGS.add("img");
+        EMPTY_TAGS.add("input");
+        EMPTY_TAGS.add("keygen");
+        EMPTY_TAGS.add("link");
+        EMPTY_TAGS.add("meta");
+        EMPTY_TAGS.add("param");
+        EMPTY_TAGS.add("source");
+        EMPTY_TAGS.add("track");
+        EMPTY_TAGS.add("wbr");
     }
-    
+
+    private final static ObjectHashSet<String> RAW_TEXT_ELEMENTS = new ObjectHashSet<String>(31);
+    static {
+        RAW_TEXT_ELEMENTS.add("script");
+        RAW_TEXT_ELEMENTS.add("style");
+        RAW_TEXT_ELEMENTS.add("textarea");
+        RAW_TEXT_ELEMENTS.add("title");
+    }
+
     public HTML5Writer() {
-            super();
+        super(EMPTY_TAGS, INLINE_TAGS);
     }
 
-    public HTML5Writer(final Writer writer) {
-            super(writer);
+    public HTML5Writer(Writer writer) {
+        super(writer, EMPTY_TAGS, INLINE_TAGS);
     }
 
     @Override
-    protected void writeDoctype(final String rootElement) throws TransformerException {
-        if(doctypeWritten) {
-            return;
+    public void endElement(QName qname) throws TransformerException {
+        if (!isEmptyTag(qname.getLocalPart())) {
+            super.endElement(qname);
+        } else {
+            closeStartTag(true);
+            endIndent(qname.getNamespaceURI(), qname.getLocalPart());
         }
-
-        documentType(rootElement, null, null);
-        doctypeWritten = true;
     }
 
     @Override
-    protected boolean isInlineTag(final String namespaceURI, final String localName) {
-        return inlineTags.contains(localName);
+    public void endElement(String namespaceURI, String localName, String qname) throws TransformerException {
+        if (!isEmptyTag(localName)) {
+            super.endElement(namespaceURI, localName, qname);
+        } else {
+            closeStartTag(true);
+            endIndent(namespaceURI, localName);
+        }
     }
 
     @Override
-    protected boolean needsEscape(final char ch) {
-        if("script".equals(currentTag)) {
-            return !(ch == '<' || ch == '>' || ch == '&');
+    public void attribute(String qname, String value) throws TransformerException {
+        try {
+            if(!tagIsOpen) {
+                characters(value);
+                return;
+            }
+            final Writer writer = getWriter();
+            writer.write(' ');
+            writer.write(qname);
+            if (!qname.equals(value)) {
+                writer.write("=\"");
+                writeChars(value, true);
+                writer.write('"');
+            }
+        } catch(final IOException ioe) {
+            throw new TransformerException(ioe.getMessage(), ioe);
+        }
+    }
+
+    @Override
+    public void attribute(QName qname, String value) throws TransformerException {
+        try {
+            if(!tagIsOpen) {
+                characters(value);
+                return;
+                // throw new TransformerException("Found an attribute outside an
+                // element");
+            }
+            final Writer writer = getWriter();
+            writer.write(' ');
+            if(qname.getPrefix() != null && qname.getPrefix().length() > 0) {
+                writer.write(qname.getPrefix());
+                writer.write(':');
+            }
+            if (!qname.getLocalPart().equals(value)) {
+                writer.write(qname.getLocalPart());
+                writer.write("=\"");
+                writeChars(value, true);
+                writer.write('"');
+            }
+        } catch(final IOException ioe) {
+            throw new TransformerException(ioe.getMessage(), ioe);
+        }
+    }
+
+    @Override
+    public void namespace(String prefix, String nsURI) throws TransformerException {
+        // no namespaces allowed in HTML5
+    }
+
+    @Override
+    protected void closeStartTag(boolean isEmpty) throws TransformerException {
+        try {
+            if (tagIsOpen) {
+                if (isEmpty) {
+                    if (isEmptyTag(currentTag)) {
+                        getWriter().write(">");
+                    } else {
+                        getWriter().write('>');
+                        getWriter().write("</");
+                        getWriter().write(currentTag);
+                        getWriter().write('>');
+                    }
+                } else {
+                    getWriter().write('>');
+                }
+                tagIsOpen = false;
+            }
+        } catch (final IOException ioe) {
+            throw new TransformerException(ioe.getMessage(), ioe);
+        }
+    }
+
+    @Override
+    protected boolean needsEscape(char ch) {
+        if (RAW_TEXT_ELEMENTS.contains(currentTag)) {
+            return false;
         }
         return super.needsEscape(ch);
     }

src/org/exist/util/serializer/IndentingXMLWriter.java
@@ -66,7 +66,7 @@ public class IndentingXMLWriter extends XMLWriter {
             indent();
         }
         super.startElement(namespaceURI, localName, qname);
-        level++;
+        addIndent();
         afterTag = true;
         sameline = true;
     }
@@ -80,7 +80,7 @@ public class IndentingXMLWriter extends XMLWriter {
             indent();
         }
         super.startElement(qname);
-        level++;
+        addIndent();
         afterTag = true;
         sameline = true;
     }
@@ -90,10 +90,7 @@ public class IndentingXMLWriter extends XMLWriter {
      */
     @Override
     public void endElement(final String namespaceURI, final String localName, final String qname) throws TransformerException {
-        level--;
-        if (afterTag && !sameline && !isInlineTag(namespaceURI, localName)){
-            indent();
-        }
+        endIndent(namespaceURI, localName);
         super.endElement(namespaceURI, localName, qname);
         sameline = false;
         afterTag = true;
@@ -104,10 +101,7 @@ public class IndentingXMLWriter extends XMLWriter {
      */
     @Override
     public void endElement(final QName qname) throws TransformerException {
-        level--;
-        if (afterTag && !sameline && !isInlineTag(qname.getNamespaceURI(), qname.getLocalPart())){
-            indent();
-        }
+        endIndent(qname.getNamespaceURI(), qname.getLocalPart());
         super.endElement(qname);
         sameline = false;
         afterTag = true;
@@ -197,6 +191,17 @@ public class IndentingXMLWriter extends XMLWriter {
         super.characters("\n");
         super.characters(indentChars.subSequence(0, spaces));
         sameline = false;
+    }
+
+    protected void addIndent() {
+        level++;
+    }
+
+    protected void endIndent(String namespaceURI, String localName) throws TransformerException {
+        level--;
+        if (afterTag && !sameline && !isInlineTag(namespaceURI, localName)){
+            indent();
+        }
     }
 
     protected static boolean isWhiteSpace(final char ch) {