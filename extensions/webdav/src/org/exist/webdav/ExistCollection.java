evolvedbinary/elemental

commit a146e84437e74a488671bef00120817fc6a48892

[refactor] Move away from direct use of BrokerPool.release() to DBBroker.close() and preferably ARM (e.g. try-with-resources)

extensions/webdav/src/org/exist/webdav/ExistCollection.java
@@ -97,82 +97,68 @@ public class ExistCollection extends ExistResource {
             return;
         }
 
-        DBBroker broker = null;
-        Collection collection = null;
-        try {
+        try(final DBBroker broker = brokerPool.get(Optional.of(subject))) {
             // Get access to collection
-            broker = brokerPool.get(Optional.of(subject));
-            collection = broker.openCollection(xmldbUri, Lock.READ_LOCK);
+            Collection collection = null;
+            try {
+                collection = broker.openCollection(xmldbUri, Lock.READ_LOCK);
 
-            if (collection == null) {
-                LOG.error(String.format("Collection for %s cannot be opened for metadata", xmldbUri));
-                return;
+                if (collection == null) {
+                    LOG.error(String.format("Collection for %s cannot be opened for metadata", xmldbUri));
+                    return;
+                }
+
+                // Retrieve some meta data
+                permissions = collection.getPermissionsNoLock();
+                readAllowed = permissions.validate(subject, Permission.READ);
+                writeAllowed = permissions.validate(subject, Permission.WRITE);
+                executeAllowed = permissions.validate(subject, Permission.EXECUTE);
+
+                creationTime = collection.getCreationTime();
+                lastModified = creationTime; // Collection does not have more information.
+
+                ownerUser = permissions.getOwner().getUsername();
+                ownerGroup = permissions.getGroup().getName();
+            } finally {
+                // Clean up collection
+                if (collection != null) {
+                    collection.release(Lock.READ_LOCK);
+                }
             }
-
-            // Retrieve some meta data
-            permissions = collection.getPermissionsNoLock();
-            readAllowed = permissions.validate(subject, Permission.READ);
-            writeAllowed = permissions.validate(subject, Permission.WRITE);
-            executeAllowed = permissions.validate(subject, Permission.EXECUTE);
-
-            creationTime = collection.getCreationTime();
-            lastModified = creationTime; // Collection does not have more information.
-
-            ownerUser = permissions.getOwner().getUsername();
-            ownerGroup = permissions.getGroup().getName();
-
-        } catch (PermissionDeniedException | EXistException pde) {
+        } catch (final PermissionDeniedException | EXistException pde) {
             LOG.error(pde);
-
-        } finally {
-
-            // Clean up collection
-            if (collection != null) {
-                collection.release(Lock.READ_LOCK);
-            }
-
-            // Return broker
-            brokerPool.release(broker);
-
-            // Set flag
-            isInitialized = true;
         }
+
+        // Set flag
+        isInitialized = true;
     }
 
     /**
      * Retrieve full URIs of all Collections in this collection.
      */
     public List<XmldbURI> getCollectionURIs() {
+        final List<XmldbURI> collectionURIs = new ArrayList<>();
 
-
-        List<XmldbURI> collectionURIs = new ArrayList<>();
-
-        DBBroker broker = null;
-        Collection collection = null;
-        try {
+        try(final DBBroker broker = brokerPool.get(Optional.of(subject))) {
             // Try to read as specified subject
-            broker = brokerPool.get(Optional.of(subject));
-            collection = broker.openCollection(xmldbUri, Lock.READ_LOCK);
+            Collection collection = null;
+            try {
+                collection = broker.openCollection(xmldbUri, Lock.READ_LOCK);
+                // Get all collections
+                final Iterator<XmldbURI> collections = collection.collectionIteratorNoLock(broker); // QQ: use collectionIterator ?
+                while (collections.hasNext()) {
+                    collectionURIs.add(xmldbUri.append(collections.next()));
 
-            // Get all collections
-            Iterator<XmldbURI> collections = collection.collectionIteratorNoLock(broker); // QQ: use collectionIterator ?
-            while (collections.hasNext()) {
-                collectionURIs.add(xmldbUri.append(collections.next()));
-
+                }
+            } finally {
+                if (collection != null) {
+                    collection.release(Lock.READ_LOCK);
+                }
             }
-
-        } catch (EXistException | PermissionDeniedException e) {
+        } catch (final EXistException | PermissionDeniedException e) {
             LOG.error(e);
             //return empty list
             return new ArrayList<>();
-
-        } finally {
-
-            if (collection != null) {
-                collection.release(Lock.READ_LOCK);
-            }
-
-            brokerPool.release(broker);
         }
 
         return collectionURIs;
@@ -182,34 +168,29 @@ public class ExistCollection extends ExistResource {
      * Retrieve full URIs of all Documents in the collection.
      */
     public List<XmldbURI> getDocumentURIs() {
+        final List<XmldbURI> documentURIs = new ArrayList<>();
 
-        List<XmldbURI> documentURIs = new ArrayList<>();
+        try(final DBBroker broker = brokerPool.get(Optional.of(subject))) {
+            Collection collection = null;
+            try {
+                // Try to read as specified subject
+                collection = broker.openCollection(xmldbUri, Lock.READ_LOCK);
 
-        DBBroker broker = null;
-        Collection collection = null;
-        try {
-            // Try to read as specified subject
-            broker = brokerPool.get(Optional.of(subject));
-            collection = broker.openCollection(xmldbUri, Lock.READ_LOCK);
-
-            // Get all documents
-            Iterator<DocumentImpl> documents = collection.iteratorNoLock(broker); // QQ: use 'iterator'
-            while (documents.hasNext()) {
-                documentURIs.add(documents.next().getURI());
+                // Get all documents
+                final Iterator<DocumentImpl> documents = collection.iteratorNoLock(broker); // QQ: use 'iterator'
+                while (documents.hasNext()) {
+                    documentURIs.add(documents.next().getURI());
+                }
+            } finally {
+                // Clean up resources
+                if (collection != null) {
+                    collection.release(Lock.READ_LOCK);
+                }
             }
-
-        } catch (PermissionDeniedException | EXistException e) {
+        } catch (final PermissionDeniedException | EXistException e) {
             LOG.error(e);
             //return empty list
             return new ArrayList<>();
-            
-        } finally {
-            // Clean up resources
-            if (collection != null) {
-                collection.release(Lock.READ_LOCK);
-            }
-
-            brokerPool.release(broker);
         }
 
         return documentURIs;

extensions/webdav/src/org/exist/webdav/ExistDocument.java
@@ -91,52 +91,44 @@ public class ExistDocument extends ExistResource {
             return;
         }
 
-        DBBroker broker = null;
-        DocumentImpl document = null;
-        try {
-            broker = brokerPool.get(Optional.of(subject));
+        try(final DBBroker broker = brokerPool.get(Optional.of(subject))) {
+            DocumentImpl document = null;
+            try {
+                // If it is not a collection, check if it is a document
+                document = broker.getXMLResource(xmldbUri, Lock.READ_LOCK);
 
-            // If it is not a collection, check if it is a document
-            document = broker.getXMLResource(xmldbUri, Lock.READ_LOCK);
+                if (document.getResourceType() == DocumentImpl.XML_FILE) {
+                    isXmlDocument = true;
+                }
 
-            if (document.getResourceType() == DocumentImpl.XML_FILE) {
-                isXmlDocument = true;
+                // Get meta data
+                creationTime = document.getMetadata().getCreated();
+                lastModified = document.getMetadata().getLastModified();
+                mimeType = document.getMetadata().getMimeType();
+
+                // Retrieve perssions
+                permissions = document.getPermissions();
+                readAllowed = permissions.validate(subject, Permission.READ);
+                writeAllowed = permissions.validate(subject, Permission.WRITE);
+                executeAllowed = permissions.validate(subject, Permission.EXECUTE);
+
+
+                ownerUser = permissions.getOwner().getUsername();
+                ownerGroup = permissions.getGroup().getName();
+
+                // Get (estimated) file size
+                contentLength = document.getContentLength();
+            }  finally {
+                // Cleanup resources
+                if (document != null) {
+                    document.getUpdateLock().release(Lock.READ_LOCK);
+                }
             }
-
-            // Get meta data         
-            creationTime = document.getMetadata().getCreated();
-            lastModified = document.getMetadata().getLastModified();
-            mimeType = document.getMetadata().getMimeType();
-
-            // Retrieve perssions
-            permissions = document.getPermissions();
-            readAllowed = permissions.validate(subject, Permission.READ);
-            writeAllowed = permissions.validate(subject, Permission.WRITE);
-            executeAllowed = permissions.validate(subject, Permission.EXECUTE);
-
-
-            ownerUser = permissions.getOwner().getUsername();
-            ownerGroup = permissions.getGroup().getName();
-
-            // Get (estimated) file size
-            contentLength = document.getContentLength();
-
-        } catch (EXistException | PermissionDeniedException e) {
+        } catch (final EXistException | PermissionDeniedException e) {
             LOG.error(e);
-
-        } finally {
-
-            // Cleanup resources
-            if (document != null) {
-                document.getUpdateLock().release(Lock.READ_LOCK);
-            }
-
-            if(broker != null) {
-                brokerPool.release(broker);
-            }
-
-            isInitialized = true;
         }
+
+        isInitialized = true;
     }
     private String mimeType;
 

extensions/commands/src/org/exist/commands/info/Lock.java
@@ -45,40 +45,33 @@ public class Lock extends AbstractCommand {
 	 */
 	@Override
 	public void process(XmldbURI collectionURI, String[] commandData) throws CommandException {
-
-		Database db = null;
-		DBBroker broker = null;
 		try {
+			final Database db = BrokerPool.getInstance();
 			
-			db = BrokerPool.getInstance();
-			
-			broker = db.getBroker();
-			
-			Collection collection = broker.getCollection(collectionURI);
-			
-			out().println("Collection lock:");
-			collection.getLock().debug(out());
-			
-			if (commandData.length == 0) return;
-			
-			DocumentImpl doc = collection.getDocument(broker, XmldbURI.create(commandData[0]) );
-			
-			if (doc == null) {
-				err().println("Resource '"+commandData[0]+"' not found.");
-				return;
-			}
-			
-			out().println("Locked by "+doc.getUserLock());
-			out().println("Lock token: "+doc.getMetadata().getLockToken());
-			
-			out().println("Update lock: ");
-			doc.getUpdateLock().debug(out());
+			try(final DBBroker broker = db.getBroker()) {
 
+				Collection collection = broker.getCollection(collectionURI);
+
+				out().println("Collection lock:");
+				collection.getLock().debug(out());
+
+				if (commandData.length == 0) return;
+
+				DocumentImpl doc = collection.getDocument(broker, XmldbURI.create(commandData[0]));
+
+				if (doc == null) {
+					err().println("Resource '" + commandData[0] + "' not found.");
+					return;
+				}
+
+				out().println("Locked by " + doc.getUserLock());
+				out().println("Lock token: " + doc.getMetadata().getLockToken());
+
+				out().println("Update lock: ");
+				doc.getUpdateLock().debug(out());
+			}
 		} catch (Exception e) {
 			throw new CommandException(e);
-		} finally {
-			if (db != null)
-				db.release(broker);
 		}
 	}
 

extensions/debuggee/src/org/exist/debuggee/DebuggeeJointImpl.java
@@ -460,10 +460,9 @@ public class DebuggeeJointImpl implements DebuggeeJoint, Status {
 	public String evalution(String script) throws Exception {
 		
 		Database db = compiledXQuery.getContext().getDatabase();
-		DBBroker broker = null;
-		try {
-			broker = db.getBroker(); //TODO: account required
 
+		//TODO: account required
+		try(final DBBroker broker = db.getBroker()) {
 			XQueryContext context = compiledXQuery.getContext().copyContext();
 			context.setDebuggeeJoint(null);
 			context.undeclareGlobalVariable(Debuggee.SESSION);
@@ -496,8 +495,6 @@ public class DebuggeeJointImpl implements DebuggeeJoint, Status {
 	                SerializerPool.getInstance().returnObject(sax);
 	            }
 	        }
-		} finally {
-			db.release(broker);
 		}
 	}
 }

extensions/debuggee/src/org/exist/debuggee/dbgp/packets/PropertyGet.java
@@ -132,12 +132,10 @@ public class PropertyGet extends Command {
 
 	protected static StringBuilder getPropertyString(Variable variable, XQueryContext context) {
         Sequence value = variable.getValue();
-        
-        DBBroker broker = null;
 
         Database db = context.getDatabase();
-        try {
-        	broker = db.getBroker();
+        try(final DBBroker broker = db.getBroker()) {
+
 	        Serializer serializer = broker.getSerializer();
 	        serializer.reset();
 	
@@ -192,9 +190,7 @@ public class PropertyGet extends Command {
 	        }
         } catch (EXistException e) {
 			e.printStackTrace();
-		} finally {
-        	db.release(broker);
-        }
+		}
         return null;
     }
 	

extensions/debuggee/src/org/exist/debuggee/dbgp/packets/Source.java
@@ -107,9 +107,7 @@ public class Source extends Command {
 	        		XmldbURI pathUri = XmldbURI.create( URLDecoder.decode( fileURI.substring(15) , "UTF-8" ) );
 	
 	        		Database db = getJoint().getContext().getDatabase();
-	        		DBBroker broker = null;
-	        		try {
-		        		broker = db.getBroker();
+	        		try(final DBBroker broker = db.getBroker()) {
 		    			DocumentImpl resource = broker.getXMLResource(pathUri, Lock.READ_LOCK);
 		
 		    			if (resource.getResourceType() == DocumentImpl.BINARY_FILE) {
@@ -120,9 +118,7 @@ public class Source extends Command {
 		    			}
 	        		} catch (EXistException e) {
 	                    exception = e;
-					} finally {
-	        			db.release(broker);
-	        		}
+					}
         		}
         	} else {
         		URL url = new URL(fileURI);

extensions/images/src/org/exist/http/servlets/ScaleImageJAI.java
@@ -424,21 +424,17 @@ public class ScaleImageJAI extends HttpServlet {
     	private File baseDir;
     	
     	public DBStorage(String baseCollection) throws ServletException {
-    		BrokerPool pool = null;
-    		DBBroker broker = null;
     		try {
-    			pool = BrokerPool.getInstance();
-    			broker = pool.get(pool.getSecurityManager().getGuestSubject());
-    			
-    			XmldbURI uri = XmldbURI.xmldbUriFor(baseCollection);
-    			this.baseDir = ((NativeBroker)broker).getCollectionBinaryFileFsPath(uri.toCollectionPathURI());
-    			log("baseDir = " + baseDir.getAbsolutePath());
+    			final BrokerPool pool = BrokerPool.getInstance();
+
+    			try(final DBBroker broker = pool.get(pool.getSecurityManager().getGuestSubject())) {
+					XmldbURI uri = XmldbURI.xmldbUriFor(baseCollection);
+					this.baseDir = ((NativeBroker) broker).getCollectionBinaryFileFsPath(uri.toCollectionPathURI());
+					log("baseDir = " + baseDir.getAbsolutePath());
+				}
     			
     		} catch (Exception e) {
     			throw new ServletException("Unable to access image collection: " + baseCollection, e);
-    		} finally {
-    			if (pool != null)
-    				pool.release(broker);
     		}
     	}
     	

extensions/indexes/lucene/test/src/org/exist/indexing/lucene/LuceneMatchListenerTest.java
@@ -23,11 +23,13 @@ import org.custommonkey.xmlunit.NamespaceContext;
 import org.custommonkey.xmlunit.SimpleNamespaceContext;
 import org.custommonkey.xmlunit.XMLAssert;
 import org.custommonkey.xmlunit.XMLUnit;
+import org.custommonkey.xmlunit.exceptions.XpathException;
 import org.exist.EXistException;
 import org.exist.TestUtils;
 import org.exist.collections.Collection;
 import org.exist.collections.CollectionConfigurationManager;
 import org.exist.collections.IndexInfo;
+import org.exist.security.PermissionDeniedException;
 import org.exist.security.xacml.AccessContext;
 import org.exist.storage.BrokerPool;
 import org.exist.storage.DBBroker;
@@ -51,7 +53,7 @@ import org.junit.Test;
 import org.xml.sax.SAXException;
 
 import javax.xml.transform.OutputKeys;
-import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Optional;
@@ -119,12 +121,11 @@ public class LuceneMatchListenerTest {
      * &lt;create qname="a"/&gt;.
      */
     @Test
-    public void indexByQName() {
-        DBBroker broker = null;
-        try {
-            configureAndStore(CONF2, XML);
+    public void indexByQName() throws EXistException, PermissionDeniedException, XPathException, SAXException {
 
-            broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()));
+        configureAndStore(CONF2, XML);
+
+        try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()))) {
 
             XQuery xquery = pool.getXQueryService();
             assertNotNull(xquery);
@@ -167,22 +168,13 @@ public class LuceneMatchListenerTest {
             XMLAssert.assertEquals("<hit><para>" + MATCH_START + "double" + MATCH_END + " " +
                     MATCH_START + "match" + MATCH_END + " " + MATCH_START + "double" + MATCH_END + " " +
                     MATCH_START + "match" + MATCH_END + "</para></hit>", result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        } finally {
-            pool.release(broker);
         }
     }
 
     @Test
-    public void matchInAncestor() {
-        DBBroker broker = null;
-        try {
-            configureAndStore(CONF1, XML);
-
-            broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()));
-
+    public void matchInAncestor() throws EXistException, PermissionDeniedException, XPathException, SAXException, IOException, XpathException {
+        configureAndStore(CONF1, XML);
+        try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()))) {
             XQuery xquery = pool.getXQueryService();
             assertNotNull(xquery);
             Sequence seq = xquery.execute(broker, "//para[ft:query(., 'mixed')]/hi", null, AccessContext.TEST);
@@ -196,22 +188,13 @@ public class LuceneMatchListenerTest {
             assertEquals(1, seq.getItemCount());
             result = queryResult2String(broker, seq);
             XMLAssert.assertXpathEvaluatesTo("1", "count(//hi/exist:match)", result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        } finally {
-            pool.release(broker);
         }
     }
 
     @Test
-    public void matchInDescendant() {
-        DBBroker broker = null;
-        try {
-            configureAndStore(CONF3, XML);
-
-            broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()));
-
+    public void matchInDescendant() throws EXistException, PermissionDeniedException, XPathException, SAXException, IOException, XpathException {
+        configureAndStore(CONF3, XML);
+        try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()))) {
             XQuery xquery = pool.getXQueryService();
             assertNotNull(xquery);
             Sequence seq = xquery.execute(broker, "//hi[ft:query(., 'mixed')]/ancestor::para", null, AccessContext.TEST);
@@ -225,22 +208,14 @@ public class LuceneMatchListenerTest {
             assertEquals(1, seq.getItemCount());
             result = queryResult2String(broker, seq);
             XMLAssert.assertXpathEvaluatesTo("1", "count(//hi/exist:match)", result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        } finally {
-            pool.release(broker);
         }
     }
 
     @Test
-    public void inlineNodes() {
-        DBBroker broker = null;
-        try {
-            configureAndStore(CONF4, XML1);
+    public void inlineNodes() throws EXistException, PermissionDeniedException, XPathException, SAXException {
+        configureAndStore(CONF4, XML1);
 
-            broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()));
-
+        try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()))) {
             XQuery xquery = pool.getXQueryService();
             assertNotNull(xquery);
             Sequence seq = xquery.execute(broker, "//p[ft:query(., 'mixed')]", null, AccessContext.TEST);
@@ -277,11 +252,6 @@ public class LuceneMatchListenerTest {
             result = queryResult2String(broker, seq);
             XMLAssert.assertEquals("<head>The <b>" + MATCH_START + "title" + MATCH_END + "</b>of it</head>",
                     result);
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail(e.getMessage());
-        } finally {
-            pool.release(broker);
         }
     }
 

commit 5ce8c874b6ab0a8d12bdd1371610f1e20ebbba3f

[bugfix] Address an NPE issue in WebDAV introduced by a146e84. Closes https://github.com/eXist-db/exist/issues/835

extensions/webdav/src/org/exist/webdav/ExistCollection.java
@@ -139,7 +139,7 @@ public class ExistCollection extends ExistResource {
     public List<XmldbURI> getCollectionURIs() {
         final List<XmldbURI> collectionURIs = new ArrayList<>();
 
-        try(final DBBroker broker = brokerPool.get(Optional.of(subject))) {
+        try(final DBBroker broker = brokerPool.get(Optional.ofNullable(subject))) {
             // Try to read as specified subject
             Collection collection = null;
             try {
@@ -170,7 +170,7 @@ public class ExistCollection extends ExistResource {
     public List<XmldbURI> getDocumentURIs() {
         final List<XmldbURI> documentURIs = new ArrayList<>();
 
-        try(final DBBroker broker = brokerPool.get(Optional.of(subject))) {
+        try(final DBBroker broker = brokerPool.get(Optional.ofNullable(subject))) {
             Collection collection = null;
             try {
                 // Try to read as specified subject
@@ -209,7 +209,7 @@ public class ExistCollection extends ExistResource {
 
         final TransactionManager txnManager = brokerPool.getTransactionManager();
 
-        try(final DBBroker broker = brokerPool.get(Optional.of(subject));
+        try(final DBBroker broker = brokerPool.get(Optional.ofNullable(subject));
             final Txn txn = txnManager.beginTransaction()) {
 
             // Open collection if possible, else abort
@@ -254,7 +254,7 @@ public class ExistCollection extends ExistResource {
 
         final TransactionManager txnManager = brokerPool.getTransactionManager();
 
-        try(final DBBroker broker = brokerPool.get(Optional.of(subject));
+        try(final DBBroker broker = brokerPool.get(Optional.ofNullable(subject));
             final Txn txn = txnManager.beginTransaction()) {
 
             // Check if collection exists. not likely to happen since availability is
@@ -348,7 +348,7 @@ public class ExistCollection extends ExistResource {
 
         final TransactionManager txnManager = brokerPool.getTransactionManager();
 
-        try(final DBBroker broker = brokerPool.get(Optional.of(subject));
+        try(final DBBroker broker = brokerPool.get(Optional.ofNullable(subject));
             final Txn txn = txnManager.beginTransaction()) {
 
             // Check if collection exists. not likely to happen since availability is checked
@@ -446,7 +446,7 @@ public class ExistCollection extends ExistResource {
 
         final TransactionManager txnManager = brokerPool.getTransactionManager();
 
-        try(final DBBroker broker = brokerPool.get(Optional.of(subject));
+        try(final DBBroker broker = brokerPool.get(Optional.ofNullable(subject));
             final Txn txn = txnManager.beginTransaction()) {
 
             // This class contains already the URI of the resource that shall be moved/copied

extensions/webdav/src/org/exist/webdav/ExistDocument.java
@@ -159,7 +159,7 @@ public class ExistDocument extends ExistResource {
 
         long startTime = System.currentTimeMillis();
 
-        try(final DBBroker broker = brokerPool.get(Optional.of(subject))) {
+        try(final DBBroker broker = brokerPool.get(Optional.ofNullable(subject))) {
 
             DocumentImpl document = null;
             try {
@@ -230,7 +230,7 @@ public class ExistDocument extends ExistResource {
 
         final TransactionManager txnManager = brokerPool.getTransactionManager();
 
-        try(final DBBroker broker = brokerPool.get(Optional.of(subject));
+        try(final DBBroker broker = brokerPool.get(Optional.ofNullable(subject));
             final Txn txn = txnManager.beginTransaction()) {
 
             // Need to split path into collection and document name
@@ -295,7 +295,7 @@ public class ExistDocument extends ExistResource {
 
         DocumentImpl document = null;
 
-        try(final DBBroker broker = brokerPool.get(Optional.of(subject))) {
+        try(final DBBroker broker = brokerPool.get(Optional.ofNullable(subject))) {
 
             // If it is not a collection, check if it is a document
             document = broker.getXMLResource(xmldbUri, Lock.READ_LOCK);
@@ -361,7 +361,7 @@ public class ExistDocument extends ExistResource {
 
         DocumentImpl document = null;
 
-        try(final DBBroker broker = brokerPool.get(Optional.of(subject))) {
+        try(final DBBroker broker = brokerPool.get(Optional.ofNullable(subject))) {
 
             // Try to get document (add catch?)
             document = broker.getXMLResource(xmldbUri, Lock.WRITE_LOCK);
@@ -459,7 +459,7 @@ public class ExistDocument extends ExistResource {
 
         final TransactionManager txnManager = brokerPool.getTransactionManager();
 
-        try(final DBBroker broker = brokerPool.get(Optional.of(subject));
+        try(final DBBroker broker = brokerPool.get(Optional.ofNullable(subject));
             final Txn txn = txnManager.beginTransaction()) {
 
 
@@ -538,7 +538,7 @@ public class ExistDocument extends ExistResource {
 
         final TransactionManager txnManager = brokerPool.getTransactionManager();
 
-        try(final DBBroker broker = brokerPool.get(Optional.of(subject));
+        try(final DBBroker broker = brokerPool.get(Optional.ofNullable(subject));
             final Txn txn = txnManager.beginTransaction()) {
 
             // Need to split path into collection and document name
@@ -630,7 +630,7 @@ public class ExistDocument extends ExistResource {
             throw new EXistException("token is null");
         }
 
-        try(final DBBroker broker = brokerPool.get(Optional.of(subject))) {
+        try(final DBBroker broker = brokerPool.get(Optional.ofNullable(subject))) {
 
             // Try to get document (add catch?)
             document = broker.getXMLResource(xmldbUri, Lock.WRITE_LOCK);