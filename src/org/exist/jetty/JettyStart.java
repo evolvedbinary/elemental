evolvedbinary/elemental

commit 46e1fcd77c7ede68b3de8e434fee45a21f5a6ffb

[bugfix] Ensure access to org/exist/system.properties is consistent and is not loaded more times than necessary, also default values should be available in its absence
svn path=/trunk/eXist/; revision=14244

src/org/exist/jetty/JettyStart.java
@@ -23,11 +23,9 @@ package org.exist.jetty;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.SocketException;
 import java.util.Observer;
-import java.util.Properties;
 
 import org.exist.cluster.ClusterComunication;
 import org.exist.cluster.ClusterException;
@@ -57,8 +55,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 import javax.servlet.Servlet;
 import org.apache.log4j.Logger;
-
-import org.exist.xquery.functions.system.GetVersion;
+import org.exist.SystemProperties;
 
 /**
  * This class provides a main method to start Jetty with eXist. It registers shutdown
@@ -110,16 +107,6 @@ public class JettyStart implements LifeCycle.Listener {
         String shutdownHookOption = System.getProperty("exist.register-shutdown-hook", "true");
         boolean registerShutdownHook = shutdownHookOption.equals("true");
 
-        Properties sysProperties = new Properties();
-		try
-		{
-			sysProperties.load(GetVersion.class.getClassLoader().getResourceAsStream("org/exist/system.properties"));
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-
         // configure database
         logger.info("Configuring eXist from " + SingleInstanceConfiguration.getPath());
         logger.info("");
@@ -131,13 +118,13 @@ public class JettyStart implements LifeCycle.Listener {
         logger.info("");
 
         String msg;
-        msg = "[eXist Version : " + sysProperties.get("product-version") + "]";
+        msg = "[eXist Version : " + SystemProperties.getInstance().getSystemProperty("product-version", "unknown") + "]";
         logger.info(msg);
-        msg = "[eXist Build : " + sysProperties.get("product-build") + "]";
+        msg = "[eXist Build : " + SystemProperties.getInstance().getSystemProperty("product-build", "unknown") + "]";
         logger.info(msg);
-        msg = "[eXist Home : " + System.getProperty("exist.home") + "]";
+        msg = "[eXist Home : " + SystemProperties.getInstance().getSystemProperty("exist.home", "unknown") + "]";
         logger.info(msg);
-        msg = "[SVN Revision : " + sysProperties.get("svn-revision") + "]";
+        msg = "[SVN Revision : " + SystemProperties.getInstance().getSystemProperty("svn-revision", "unknown") + "]";
         logger.info(msg);
         msg = "[Operating System : " +
         		System.getProperty("os.name") +

src/org/exist/SystemProperties.java
@@ -1,0 +1,47 @@
+package org.exist;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author aretter
+ */
+public class SystemProperties {
+
+    private final static Logger LOG = Logger.getLogger(SystemProperties.class);
+
+    private final static SystemProperties instance = new SystemProperties();
+    private Properties properties = null;
+
+    public final static SystemProperties getInstance() {
+        return instance;
+    }
+
+    private SystemProperties() {
+    }
+
+    public synchronized String getSystemProperty(String propertyName, String defaultValue) {
+
+        if(properties == null) {
+            properties = new Properties();
+            InputStream is = null;
+            try {
+                is = SystemProperties.class.getResourceAsStream("system.properties");
+                if(is != null) {
+                    properties.load(is);
+                }
+            } catch (IOException ioe) {
+                LOG.debug("Unable to load system.properties from class loader: " +  ioe.getMessage(), ioe);
+            } finally {
+                if(is != null) {
+                    try { is.close(); } catch(IOException ioe) { }
+                }
+            }
+        }
+
+        return properties.getProperty(propertyName, defaultValue);
+    }
+}

src/org/exist/client/ClientFrame.java
@@ -91,6 +91,7 @@ import javax.swing.text.DefaultStyledDocument;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.xml.transform.OutputKeys;
+import org.exist.SystemProperties;
 
 import org.exist.backup.Backup;
 import org.exist.backup.CreateBackupDialog;
@@ -1464,20 +1465,7 @@ public class ClientFrame extends JFrame
     }
     
     private void AboutAction() {
-        
-        Properties sysProperties = InteractiveClient.getSystemProperties();
-        
-        // Original text eXist version 1.0, Copyright (C) 2001-2006 Wolfgang Meier
-        JOptionPane.showMessageDialog(this, 
-                sysProperties.getProperty("product-name") + " version " 
-                + sysProperties.getProperty("product-version")
-                + " (revision " + sysProperties.getProperty("svn-revision") + ") \n\n"
-                + "Copyright (C) 2001-2009 Wolfgang Meier\n\n"
-                + "eXist comes with ABSOLUTELY NO WARRANTY.\n"
-                + "This is free software, and you are welcome to\n"
-                + "redistribute it under certain conditions;\n"
-                + "for details read the license file."  );
-        return;
+        JOptionPane.showMessageDialog(this, client.getNotice());
     }
     
     class TableMouseListener extends MouseAdapter {
@@ -1657,13 +1645,11 @@ public class ClientFrame extends JFrame
      */
     
     protected static Properties getLoginData(Properties properties) {
-        
-        Properties sysProperties = InteractiveClient.getSystemProperties();
 
         LoginPanel login = new LoginPanel(properties);
         if (JOptionPane.showOptionDialog(null, login, 
-                sysProperties.getProperty("product-name") + " " 
-                + sysProperties.getProperty("product-version") + " Database Login" , 
+                SystemProperties.getInstance().getSystemProperty("product-name", "eXist-db") + " "
+                + SystemProperties.getInstance().getSystemProperty("product-version", "unknown") + " Database Login" ,
                 JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
                 null, null, null) == JOptionPane.OK_OPTION) {
             return login.getProperties();

src/org/exist/client/InteractiveClient.java
@@ -80,6 +80,7 @@ import org.apache.avalon.excalibur.cli.CLArgsParser;
 import org.apache.avalon.excalibur.cli.CLOption;
 import org.apache.avalon.excalibur.cli.CLUtil;
 import org.apache.log4j.Logger;
+import org.exist.SystemProperties;
 import org.exist.dom.XMLUtil;
 import org.exist.security.Permission;
 import org.exist.security.SecurityManager;
@@ -2620,16 +2621,18 @@ public class InteractiveClient {
     }
     
     public void printNotice() {
-        
-        Properties sysProperties = getSystemProperties();
-        
-        messageln(sysProperties.getProperty("product-name") + " version " 
-                + sysProperties.getProperty("product-version") 
-                +", Copyright (C) 2001-2011 Wolfgang Meier");
-        messageln("eXist comes with ABSOLUTELY NO WARRANTY.");
-        messageln("This is free software, and you are welcome to "
-                + "redistribute it\nunder certain conditions; "
-                + "for details read the license file.\n");
+        messageln(getNotice());
+    }
+
+    public String getNotice() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(SystemProperties.getInstance().getSystemProperty("product-name", "eXist-db"));
+        builder.append(" version ");
+        builder.append(SystemProperties.getInstance().getSystemProperty("product-version", "unknown"));
+        builder.append(", Copyright (C) 2001-2011 Wolfgang Meier\n");
+        builder.append("eXist comes with ABSOLUTELY NO WARRANTY.\n");
+        builder.append("This is free software, and you are welcome to redistribute it\nunder certain conditions; for details read the license file.\n");
+        return builder.toString();
     }
     
     private final void message(String msg) {

src/org/exist/management/impl/SystemInfo.java
@@ -21,13 +21,10 @@
  */
 package org.exist.management.impl;
 
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Locale;
-import java.util.Properties;
 
-import org.apache.log4j.Logger;
-import org.exist.storage.BrokerPool;
+import org.exist.SystemProperties;
 
 /**
  * Class SystemInfo
@@ -36,30 +33,22 @@ import org.exist.storage.BrokerPool;
  */
 public class SystemInfo implements SystemInfoMBean {
 
-    private final static Logger LOG = Logger.getLogger(SystemInfo.class);
-    private Properties sysProperties = new Properties();
-
     public SystemInfo() {
-        try {
-            sysProperties.load(SystemInfo.class.getClassLoader().getResourceAsStream("org/exist/system.properties"));
-        } catch (IOException e) {
-            LOG.debug("Unable to load system.properties from class loader");
-        }
     }
 
     @Override
     public String getExistVersion() {
-        return sysProperties.getProperty("product-version","unknown");
+        return SystemProperties.getInstance().getSystemProperty("product-version","unknown");
     }
 
     @Override
     public String getExistBuild() {
-        return sysProperties.getProperty("product-build","unknown");
+        return SystemProperties.getInstance().getSystemProperty("product-build","unknown");
     }
 
     @Override
     public String getSvnRevision() {
-        return sysProperties.getProperty("svn-revision","unknown");
+        return SystemProperties.getInstance().getSystemProperty("svn-revision","unknown");
     }
 
     @Override

src/org/exist/xquery/functions/system/GetBuild.java
@@ -22,10 +22,7 @@
  */
 package org.exist.xquery.functions.system;
 
-import java.io.IOException;
-import java.util.Properties;
-
-import org.apache.log4j.Logger;
+import org.exist.SystemProperties;
 import org.exist.dom.QName;
 import org.exist.xquery.BasicFunction;
 import org.exist.xquery.Cardinality;
@@ -42,38 +39,24 @@ import org.exist.xquery.value.Type;
  * 
  * @author Adam Retter (adam.retter@devon.gov.uk)
  */
-public class GetBuild extends BasicFunction
-{
+public class GetBuild extends BasicFunction {
 
-    protected final static Logger logger = Logger.getLogger(GetBuild.class);
+    public final static FunctionSignature signature = new FunctionSignature(
+        new QName("get-build", SystemModule.NAMESPACE_URI, SystemModule.PREFIX),
+        "Returns the build of eXist running this query.",
+        FunctionSignature.NO_ARGS,
+        new FunctionReturnSequenceType(Type.STRING, Cardinality.EXACTLY_ONE, "the build number")
+    );
 
-	public final static FunctionSignature signature =
-		new FunctionSignature(
-			new QName("get-build", SystemModule.NAMESPACE_URI, SystemModule.PREFIX),
-			"Returns the build of eXist running this query.",
-			FunctionSignature.NO_ARGS,
-			new FunctionReturnSequenceType(Type.STRING, Cardinality.EXACTLY_ONE, "the build number"));
+    public GetBuild(XQueryContext context) {
+        super(context, signature);
+    }
 
-	public GetBuild(XQueryContext context)
-	{
-		super(context, signature);
-	}
-
-	/* (non-Javadoc)
-	 * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
-	 */
-	public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException
-	{
-		Properties sysProperties = new Properties();
-		try
-		{
-			sysProperties.load(GetVersion.class.getClassLoader().getResourceAsStream("org/exist/system.properties"));
-		}
-		catch (IOException e)
-		{
-			logger.debug("Unable to load system.properties from class loader");
-		}
-		StringValue stringValue = new StringValue(sysProperties.getProperty("product-build", "unknown build"));
-		return stringValue;
-	}
+    /* (non-Javadoc)
+     * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
+     */
+    @Override
+    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
+        return new StringValue(SystemProperties.getInstance().getSystemProperty("product-build", "unknown build"));
+    }
 }

src/org/exist/xquery/functions/system/GetRevision.java
@@ -22,10 +22,7 @@
  */
 package org.exist.xquery.functions.system;
 
-import java.io.IOException;
-import java.util.Properties;
-
-import org.apache.log4j.Logger;
+import org.exist.SystemProperties;
 import org.exist.dom.QName;
 import org.exist.xquery.BasicFunction;
 import org.exist.xquery.Cardinality;
@@ -42,37 +39,24 @@ import org.exist.xquery.value.Type;
  * 
  * @author Dannes Wessels
  */
-public class GetRevision extends BasicFunction
-{
+public class GetRevision extends BasicFunction {
 
-    protected final static Logger logger = Logger.getLogger(GetRevision.class);
+    public final static FunctionSignature signature = new FunctionSignature(
+        new QName("get-revision", SystemModule.NAMESPACE_URI, SystemModule.PREFIX),
+        "Returns the SubVersion (SVN) revision ID of eXist running this query.",
+        FunctionSignature.NO_ARGS,
+        new FunctionReturnSequenceType(Type.STRING, Cardinality.EXACTLY_ONE, "the revision ID.")
+    );
 
-	public final static FunctionSignature signature =
-		new FunctionSignature(
-			new QName("get-revision", SystemModule.NAMESPACE_URI, SystemModule.PREFIX),
-			"Returns the SubVersion (SVN) revision ID of eXist running this query.",
-			FunctionSignature.NO_ARGS,
-			new FunctionReturnSequenceType(Type.STRING, Cardinality.EXACTLY_ONE, "the revision ID."));
+    public GetRevision(XQueryContext context) {
+        super(context, signature);
+    }
 
-	public GetRevision(XQueryContext context)
-	{
-		super(context, signature);
-	}
-
-	/* (non-Javadoc)
-	 * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
-	 */
-	public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException
-	{
-		Properties sysProperties = new Properties();
-		try
-		{
-			sysProperties.load(GetVersion.class.getClassLoader().getResourceAsStream("org/exist/system.properties"));
-		}
-		catch (IOException e)
-		{
-			LOG.debug("Unable to load system.properties from class loader");
-		}
-		return new StringValue(sysProperties.getProperty("svn-revision", "unknown revision"));
-	}
+    /* (non-Javadoc)
+     * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
+     */
+    @Override
+    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
+        return new StringValue(SystemProperties.getInstance().getSystemProperty("svn-revision", "unknown revision"));
+    }
 }

src/org/exist/xquery/functions/system/GetVersion.java
@@ -22,10 +22,7 @@
  */
 package org.exist.xquery.functions.system;
 
-import java.io.IOException;
-import java.util.Properties;
-
-import org.apache.log4j.Logger;
+import org.exist.SystemProperties;
 import org.exist.dom.QName;
 import org.exist.xquery.BasicFunction;
 import org.exist.xquery.Cardinality;
@@ -42,38 +39,24 @@ import org.exist.xquery.value.Type;
  * 
  * @author wolf
  */
-public class GetVersion extends BasicFunction
-{
+public class GetVersion extends BasicFunction {
 
-    protected final static Logger logger = Logger.getLogger(GetVersion.class);
+    public final static FunctionSignature signature = new FunctionSignature(
+        new QName("get-version", SystemModule.NAMESPACE_URI, SystemModule.PREFIX),
+        "Returns the version of eXist running this query.",
+        FunctionSignature.NO_ARGS,
+        new FunctionReturnSequenceType(Type.STRING, Cardinality.EXACTLY_ONE, "the version string")
+    );
 
-	public final static FunctionSignature signature =
-		new FunctionSignature(
-			new QName("get-version", SystemModule.NAMESPACE_URI, SystemModule.PREFIX),
-			"Returns the version of eXist running this query.",
-			FunctionSignature.NO_ARGS,
-			new FunctionReturnSequenceType(Type.STRING, Cardinality.EXACTLY_ONE, "the version string"));
-	
-	public GetVersion(XQueryContext context)
-	{
-		super(context, signature);
-	}
+    public GetVersion(XQueryContext context) {
+        super(context, signature);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
-	 */
-	public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException
-	{
-    	logger.debug("Entering the " + SystemModule.PREFIX + ":get-version XQuery function");
-		Properties sysProperties = new Properties();
-		try
-		{
-			sysProperties.load(GetVersion.class.getClassLoader().getResourceAsStream("org/exist/system.properties"));
-		}
-		catch (IOException e)
-		{
-			logger.debug("Unable to load system.properties from class loader");
-		}
-		return new StringValue(sysProperties.getProperty("product-version", "unknown version"));
-	}
+    /* (non-Javadoc)
+     * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
+     */
+    @Override
+    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
+        return new StringValue(SystemProperties.getInstance().getSystemProperty("product-version", "unknown version"));
+    }
 }

commit 743f9c673546207c62578564b17363d3b3014e4c

[bugfix] exist.home is not in src/org/exist/system.properties. Fixes a bug introduced in 46e1fcd

src/org/exist/jetty/JettyStart.java
@@ -129,13 +129,12 @@ public class JettyStart extends Observable implements LifeCycle.Listener {
 
         logger.info("Running as user '" 
                 + System.getProperty("user.name", "(unknown user.name)") + "'");
-
+        logger.info("[eXist Home : "
+                + System.getProperty("exist.home", "unknown") + "]");
         logger.info("[eXist Version : " 
                 + SystemProperties.getInstance().getSystemProperty("product-version", "unknown") + "]");
         logger.info("[eXist Build : " 
                 + SystemProperties.getInstance().getSystemProperty("product-build", "unknown") + "]");
-        logger.info("[eXist Home : " 
-                + SystemProperties.getInstance().getSystemProperty("exist.home", "unknown") + "]");
         logger.info("[Git commmit : " 
                 + SystemProperties.getInstance().getSystemProperty("git-commit", "unknown") + "]");
         