evolvedbinary/elemental

commit 48e2073cd50ad3756f2f252a97f2026371aaa411

Generic code improvements (mechanically found)

extensions/modules/src/org/exist/xquery/modules/simpleql/SimpleQLLexer.java
@@ -8,23 +8,20 @@ import antlr.TokenStreamIOException;
 import antlr.TokenStreamRecognitionException;
 import antlr.CharStreamException;
 import antlr.CharStreamIOException;
-import antlr.ANTLRException;
+
 import java.io.Reader;
 import java.util.Hashtable;
-import antlr.CharScanner;
+
 import antlr.InputBuffer;
 import antlr.ByteBuffer;
 import antlr.CharBuffer;
 import antlr.Token;
-import antlr.CommonToken;
 import antlr.RecognitionException;
 import antlr.NoViableAltForCharException;
-import antlr.MismatchedCharException;
 import antlr.TokenStream;
 import antlr.ANTLRHashString;
 import antlr.LexerSharedInputState;
 import antlr.collections.impl.BitSet;
-import antlr.SemanticException;
 
 public class SimpleQLLexer extends antlr.CharScanner implements SimpleQLParserTokenTypes, TokenStream
  {
@@ -42,12 +39,12 @@ public SimpleQLLexer(LexerSharedInputState state) {
 	caseSensitiveLiterals = true;
 	setCaseSensitive(true);
 	literals = new Hashtable();
-	literals.put(new ANTLRHashString("NOT", this), Integer.valueOf(11));
-	literals.put(new ANTLRHashString("OR", this), Integer.valueOf(9));
-	literals.put(new ANTLRHashString("NICHT", this), Integer.valueOf(12));
-	literals.put(new ANTLRHashString("UND", this), Integer.valueOf(8));
-	literals.put(new ANTLRHashString("ODER", this), Integer.valueOf(10));
-	literals.put(new ANTLRHashString("AND", this), Integer.valueOf(7));
+	literals.put(new ANTLRHashString("NOT", this), 11);
+	literals.put(new ANTLRHashString("OR", this), 9);
+	literals.put(new ANTLRHashString("NICHT", this), 12);
+	literals.put(new ANTLRHashString("UND", this), 8);
+	literals.put(new ANTLRHashString("ODER", this), 10);
+	literals.put(new ANTLRHashString("AND", this), 7);
 }
 
 public Token nextToken() throws TokenStreamException {

extensions/exiftool/src/org/exist/exiftool/xquery/MetadataFunctions.java
@@ -185,7 +185,7 @@ public class MetadataFunctions extends BasicFunction {
                 //make sure process is complete
                 p.waitFor();
 
-                return ModuleUtils.inputSourceToXML(context, new InputSource(new ByteArrayInputStream(baos.toByteArray())));
+                return ModuleUtils.inputSourceToXML(context, new InputSource(baos.toInputStream()));
             }
 
         } catch (final IOException ex) {

extensions/modules/src/org/exist/xquery/modules/cache/CacheFunctions.java
@@ -231,12 +231,8 @@ public class CacheFunctions extends BasicFunction {
         // we must test for preemption, i.e the cache may have already been created
         final Cache newOrExisting = CacheModule.caches.computeIfAbsent(cacheName, key -> new Cache(config));
 
-        if(newOrExisting.getConfig() == config) {
-            // is new
-            return true;
-        } else {
-            return false;
-        }
+        // is new
+        return newOrExisting.getConfig() == config;
     }
 
     private Sequence put(final String cacheName, final String key, final Sequence value) throws XPathException {
@@ -244,7 +240,7 @@ public class CacheFunctions extends BasicFunction {
 
         // check permissions
         if(!context.getEffectiveUser().hasDbaRole()) {
-            final Optional<String> putGroup = cache.getConfig().getPermissions().flatMap(p -> p.getPutGroup());
+            final Optional<String> putGroup = cache.getConfig().getPermissions().flatMap(CacheConfig.Permissions::getPutGroup);
             if (putGroup.isPresent()) {
                 if (!context.getEffectiveUser().hasGroup(putGroup.get())) {
                     throw new XPathException(this, INSUFFICIENT_PERMISSIONS, "User does not have the appropriate permissions to put data into this cache");
@@ -260,7 +256,7 @@ public class CacheFunctions extends BasicFunction {
 
         // check permissions
         if(!context.getEffectiveUser().hasDbaRole()) {
-            final Optional<String> getGroup = cache.getConfig().getPermissions().flatMap(p -> p.getGetGroup());
+            final Optional<String> getGroup = cache.getConfig().getPermissions().flatMap(CacheConfig.Permissions::getGetGroup);
             if (getGroup.isPresent()) {
                 if (!context.getEffectiveUser().hasGroup(getGroup.get())) {
                     throw new XPathException(this, INSUFFICIENT_PERMISSIONS, "User does not have the appropriate permissions to list data in this cache");
@@ -276,7 +272,7 @@ public class CacheFunctions extends BasicFunction {
 
         // check permissions
         if(!context.getEffectiveUser().hasDbaRole()) {
-            final Optional<String> getGroup = cache.getConfig().getPermissions().flatMap(p -> p.getGetGroup());
+            final Optional<String> getGroup = cache.getConfig().getPermissions().flatMap(CacheConfig.Permissions::getGetGroup);
             if (getGroup.isPresent()) {
                 if (!context.getEffectiveUser().hasGroup(getGroup.get())) {
                     throw new XPathException(this, INSUFFICIENT_PERMISSIONS, "User does not have the appropriate permissions to get data from this cache");
@@ -292,7 +288,7 @@ public class CacheFunctions extends BasicFunction {
 
         // check permissions
         if(!context.getEffectiveUser().hasDbaRole()) {
-            final Optional<String> removeGroup = cache.getConfig().getPermissions().flatMap(p -> p.getRemoveGroup());
+            final Optional<String> removeGroup = cache.getConfig().getPermissions().flatMap(CacheConfig.Permissions::getRemoveGroup);
             if (removeGroup.isPresent()) {
                 if (!context.getEffectiveUser().hasGroup(removeGroup.get())) {
                     throw new XPathException(this, INSUFFICIENT_PERMISSIONS, "User does not have the appropriate permissions to remove data from this cache");
@@ -309,7 +305,7 @@ public class CacheFunctions extends BasicFunction {
         // check all permissions first
         if(!context.getEffectiveUser().hasDbaRole()) {
             for (final Cache cache : caches) {
-                final Optional<String> clearGroup = cache.getConfig().getPermissions().flatMap(p -> p.getClearGroup());
+                final Optional<String> clearGroup = cache.getConfig().getPermissions().flatMap(CacheConfig.Permissions::getClearGroup);
                 if (clearGroup.isPresent()) {
                     if (!context.getEffectiveUser().hasGroup(clearGroup.get())) {
                         throw new XPathException(this, INSUFFICIENT_PERMISSIONS, "User does not have the appropriate permissions to clear data from all caches");
@@ -329,7 +325,7 @@ public class CacheFunctions extends BasicFunction {
 
         // check permissions
         if(!context.getEffectiveUser().hasDbaRole()) {
-            final Optional<String> clearGroup = cache.getConfig().getPermissions().flatMap(p -> p.getClearGroup());
+            final Optional<String> clearGroup = cache.getConfig().getPermissions().flatMap(CacheConfig.Permissions::getClearGroup);
             if (clearGroup.isPresent()) {
                 if (!context.getEffectiveUser().hasGroup(clearGroup.get())) {
                     throw new XPathException(this, INSUFFICIENT_PERMISSIONS, "User does not have the appropriate permissions to clear data from this cache");

extensions/modules/src/org/exist/xquery/modules/cache/CacheModule.java
@@ -60,7 +60,7 @@ public class CacheModule extends AbstractInternalModule {
 
     static final Map<String, Cache> caches = new ConcurrentHashMap<>();
 
-    public CacheModule(final Map<String, List<? extends Object>> parameters) {
+    public CacheModule(final Map<String, List<?>> parameters) {
         super(functions, parameters);
     }
 

extensions/modules/src/org/exist/xquery/modules/compression/CompressionModule.java
@@ -62,7 +62,7 @@ public class CompressionModule extends AbstractInternalModule {
         new FunctionDef(UnTarFunction.signatures[1], UnTarFunction.class)
     };
 
-    public CompressionModule(Map<String, List<? extends Object>> parameters) {
+    public CompressionModule(Map<String, List<?>> parameters) {
         super(functions, parameters);
     }
 

extensions/modules/src/org/exist/xquery/modules/compression/UnGZipFunction.java
@@ -21,7 +21,6 @@
  */
 package org.exist.xquery.modules.compression;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.zip.GZIPInputStream;
 
@@ -86,7 +85,7 @@ public class UnGZipFunction extends BasicFunction
                 baos.write(b, 0, read);
             }
 
-            return BinaryValueFromInputStream.getInstance(context, new Base64BinaryValueType(), new ByteArrayInputStream(baos.toByteArray()));
+            return BinaryValueFromInputStream.getInstance(context, new Base64BinaryValueType(), baos.toInputStream());
         } catch(final IOException ioe) {
             throw new XPathException(this, ioe.getMessage(), ioe);
         }

extensions/modules/src/org/exist/xquery/modules/compression/UnTarFunction.java
@@ -21,7 +21,6 @@
  */
 package org.exist.xquery.modules.compression;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
@@ -33,7 +32,6 @@ import org.exist.xquery.Cardinality;
 import org.exist.xquery.FunctionSignature;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
-import org.exist.xquery.value.Base64BinaryValueType;
 import org.exist.xquery.value.BinaryValue;
 import org.exist.xquery.value.FunctionParameterSequenceType;
 import org.exist.xquery.value.Sequence;

extensions/modules/src/org/exist/xquery/modules/context/ContextModule.java
@@ -52,7 +52,7 @@ public class ContextModule extends AbstractInternalModule {
 		new FunctionDef( GetID.signatures[0], GetID.class )
 	};
 
-	public ContextModule(Map<String, List<? extends Object>> parameters) {
+	public ContextModule(Map<String, List<?>> parameters) {
 		super( signatures, parameters );
 	}
 

commit c9f642ebb1521e69595232847edc8c950bcdda88

[bugfix] Undo change to autogenerated code caused by 48e2073

extensions/modules/src/org/exist/xquery/modules/simpleql/SimpleQLLexer.java
@@ -8,20 +8,23 @@ import antlr.TokenStreamIOException;
 import antlr.TokenStreamRecognitionException;
 import antlr.CharStreamException;
 import antlr.CharStreamIOException;
-
+import antlr.ANTLRException;
 import java.io.Reader;
 import java.util.Hashtable;
-
+import antlr.CharScanner;
 import antlr.InputBuffer;
 import antlr.ByteBuffer;
 import antlr.CharBuffer;
 import antlr.Token;
+import antlr.CommonToken;
 import antlr.RecognitionException;
 import antlr.NoViableAltForCharException;
+import antlr.MismatchedCharException;
 import antlr.TokenStream;
 import antlr.ANTLRHashString;
 import antlr.LexerSharedInputState;
 import antlr.collections.impl.BitSet;
+import antlr.SemanticException;
 
 public class SimpleQLLexer extends antlr.CharScanner implements SimpleQLParserTokenTypes, TokenStream
  {
@@ -39,12 +42,12 @@ public SimpleQLLexer(LexerSharedInputState state) {
 	caseSensitiveLiterals = true;
 	setCaseSensitive(true);
 	literals = new Hashtable();
-	literals.put(new ANTLRHashString("NOT", this), 11);
-	literals.put(new ANTLRHashString("OR", this), 9);
-	literals.put(new ANTLRHashString("NICHT", this), 12);
-	literals.put(new ANTLRHashString("UND", this), 8);
-	literals.put(new ANTLRHashString("ODER", this), 10);
-	literals.put(new ANTLRHashString("AND", this), 7);
+	literals.put(new ANTLRHashString("NOT", this), Integer.valueOf(11));
+	literals.put(new ANTLRHashString("OR", this), Integer.valueOf(9));
+	literals.put(new ANTLRHashString("NICHT", this), Integer.valueOf(12));
+	literals.put(new ANTLRHashString("UND", this), Integer.valueOf(8));
+	literals.put(new ANTLRHashString("ODER", this), Integer.valueOf(10));
+	literals.put(new ANTLRHashString("AND", this), Integer.valueOf(7));
 }
 
 public Token nextToken() throws TokenStreamException {