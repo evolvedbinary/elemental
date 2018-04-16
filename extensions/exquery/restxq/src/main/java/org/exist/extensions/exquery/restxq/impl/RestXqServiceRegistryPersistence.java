evolvedbinary/elemental

commit d97264ef350a169c36787aba80bf659bc08de993

[bugfix] Atomic Move cannot be used where src and dest may be on different filesystems Closes https://github.com/eXist-db/exist/issues/1803

extensions/exquery/restxq/src/main/java/org/exist/extensions/exquery/restxq/impl/RestXqServiceRegistryPersistence.java
@@ -140,16 +140,16 @@ public class RestXqServiceRegistryPersistence implements RestXqServiceRegistryLi
         //we can ignore the change in service provided to this function as args, as we just write the details of all
         //services to disk, overwriting the old registry
         
-        final Optional<Path> optNewRegistry = getRegistryFile(true);
+        final Optional<Path> optTmpNewRegistry = getRegistryFile(true);
         
-        if(!optNewRegistry.isPresent()) {
+        if(!optTmpNewRegistry.isPresent()) {
             log.error("Could not save RESTXQ Registry to disk!");
         } else {
-            final Path newRegistry = optNewRegistry.get();
-            log.info("Preparing new RESTXQ registry on disk: " + newRegistry.toAbsolutePath().toString());
+            final Path tmpNewRegistry = optTmpNewRegistry.get();
+            log.info("Preparing new RESTXQ registry on disk: " + tmpNewRegistry.toAbsolutePath().toString());
 
             try {
-                try(final PrintWriter writer = new PrintWriter(Files.newBufferedWriter(newRegistry, StandardOpenOption.TRUNCATE_EXISTING))) {
+                try (final PrintWriter writer = new PrintWriter(Files.newBufferedWriter(tmpNewRegistry, StandardOpenOption.TRUNCATE_EXISTING))) {
 
                     writer.println(VERSION_LABEL + LABEL_SEP + REGISTRY_FILE_VERSION);
 
@@ -177,18 +177,21 @@ public class RestXqServiceRegistryPersistence implements RestXqServiceRegistryLi
                 }
 
                 final Optional<Path> optRegistry = getRegistryFile(false);
-                if(optRegistry.isPresent()) {
+                if (optRegistry.isPresent()) {
                     final Path registry = optRegistry.get();
 
                     //replace the original registry with the new registry
-                    Files.move(newRegistry, registry, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE);
+                    final Path localTmpNewRegistry = Files.copy(tmpNewRegistry, registry.getParent());
+                    Files.move(localTmpNewRegistry, registry, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE);
 
-                    log.info("Replaced RESTXQ registry: " + FileUtils.fileName(newRegistry) + " -> " + FileUtils.fileName(registry));
+                    log.info("Replaced RESTXQ registry: " + FileUtils.fileName(tmpNewRegistry) + " -> " + FileUtils.fileName(registry));
                 } else {
                     throw new IOException("Unable to retrieve existing RESTXQ registry");
                 }
             } catch(final IOException ioe) {
                 log.error(ioe.getMessage(), ioe);
+            } finally {
+                TemporaryFileManager.getInstance().returnTemporaryFile(tmpNewRegistry);
             }
         }
     }

extensions/modules/src/org/exist/xquery/modules/file/FileMove.java
@@ -92,7 +92,7 @@ public class FileMove extends BasicFunction {
         final Path dest = FileModuleHelper.getFile(inputPath2);
 
 		try {
-			Files.move(src, dest, StandardCopyOption.ATOMIC_MOVE);
+			Files.move(src, dest);
 			return BooleanValue.TRUE;
 		} catch(final IOException ioe) {
 			LOG.error(ioe);

commit 0e4e8f5d6499990c13538e968725bc5da16af0b0

[bugfix] Fix a regression in RESTXQ registry introduced in d97264e Closes https://github.com/eXist-db/exist/issues/1831

extensions/exquery/restxq/src/main/java/org/exist/extensions/exquery/restxq/impl/RestXqServiceRegistryPersistence.java
@@ -181,7 +181,7 @@ public class RestXqServiceRegistryPersistence implements RestXqServiceRegistryLi
                     final Path registry = optRegistry.get();
 
                     //replace the original registry with the new registry
-                    final Path localTmpNewRegistry = Files.copy(tmpNewRegistry, registry.getParent());
+                    final Path localTmpNewRegistry = Files.copy(tmpNewRegistry, registry.getParent().resolve(tmpNewRegistry.getFileName()));
                     Files.move(localTmpNewRegistry, registry, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE);
 
                     log.info("Replaced RESTXQ registry: " + FileUtils.fileName(tmpNewRegistry) + " -> " + FileUtils.fileName(registry));