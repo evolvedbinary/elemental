adamretter/exist

[bugfix] Reinstate the Collections Concurrency Test which used to deadlock

test/src/org/exist/collections/ConcurrencyTest.java
@@ -16,7 +16,9 @@ import org.xmldb.api.modules.CollectionManagementService;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Test concurrent access to collections.
@@ -26,51 +28,66 @@ public class ConcurrencyTest {
     @ClassRule
     public static final ExistXmldbEmbeddedServer existEmbeddedServer = new ExistXmldbEmbeddedServer(false, true);
 
-    private static final int N_THREADS = 10;
-    private static final int DOC_COUNT = 200; 
+    private static int CONCURRENT_THREADS = Runtime.getRuntime().availableProcessors() * 3;
+    private static final int DOC_COUNT = CONCURRENT_THREADS * 10;
 
     private static final int QUERY_COUNT = 20;
     
-    private static final String QUERY = "/test/c";
+    private static final String QUERY = "collection('/db/test')/test/c";
 
     private static final String REMOVE =
-        "declare namespace xdb=\"http://exist-db.org/xquery/xmldb\";\n" +
+        "declare namespace xmldb=\"http://exist-db.org/xquery/xmldb\";\n" +
         "declare namespace util=\"http://exist-db.org/xquery/util\";" +
-        "declare variable $start external; " +
+        "declare variable $start as xs:integer external; " +
         "let $dummy := util:log('DEBUG', ('Removing $start: ', $start, ' to ', $start + 9)) " +
         "for $i in $start to $start + 9 " +
-        "let $resource := /test[@id = xs:integer($i)] " +
+        "let $resource := collection('/db/test')/test[xs:integer(@id) eq $i] " +
         "return" +
-        "   xdb:remove(util:collection-name($resource), util:document-name($resource))";
+        "   xmldb:remove(util:collection-name($resource), util:document-name($resource))";
 
     @Test
 	public void runTasks() {
-		ExecutorService executor = Executors.newFixedThreadPool(N_THREADS);
+        final ExecutorService executorRemove = newFixedThreadPool(CONCURRENT_THREADS, "concurrencyTest-remove");
+        final ExecutorService executorQuery = newFixedThreadPool(CONCURRENT_THREADS, "concurrencyTest-query");
 
-        for (int i = 1; i <= 20; i++) {
-            executor.submit(new QueryTask(REMOVE, i * 10, true));
+        for (int i = 1; i <= CONCURRENT_THREADS; i++) {
+            executorRemove.submit(new QueryTask(REMOVE, i * 10, true));
             for (int j = 0; j < QUERY_COUNT; j++) {
-                executor.submit(new QueryTask(QUERY, 0, true));
+                executorQuery.submit(new QueryTask(QUERY, 0, true));
             }
         }
 
-        executor.shutdown();
-		boolean terminated = false;
+        executorRemove.shutdown();
+        executorQuery.shutdown();
+		boolean terminatedRemove = false;
+        boolean terminatedQuery = false;
 		try {
-			terminated = executor.awaitTermination(60 * 60, TimeUnit.SECONDS);
-		} catch (InterruptedException e) {
+			terminatedRemove = executorRemove.awaitTermination(60 * 60, TimeUnit.SECONDS);
+            terminatedQuery = executorQuery.awaitTermination(60 * 60, TimeUnit.SECONDS);
+		} catch (final InterruptedException e) {
+            //Nothing to do
 		}
-		assertTrue(terminated);
+		assertTrue(terminatedRemove);
+        assertTrue(terminatedQuery);
     }
 
-    private class QueryTask implements Runnable {
+    private ExecutorService newFixedThreadPool(final int nThreads, final String threadsBaseName) {
+        return Executors.newFixedThreadPool(nThreads, new ThreadFactory() {
+            private final AtomicInteger counter = new AtomicInteger();
 
-        String query;
-        int start = 0;
-        @SuppressWarnings("unused")
-		boolean protect = false;
+            @Override
+            public Thread newThread(final Runnable r) {
+                return new Thread(r, threadsBaseName + "-" + counter.getAndIncrement());
+            }
+        });
+    }
+
+    private static class QueryTask implements Runnable {
+        private final String query;
+        private final int start;
+		private final boolean protect;
         
-        private QueryTask(String query, int start, boolean protect) {
+        private QueryTask(final String query, final int start, final boolean protect) {
             this.query = query;
             this.protect = protect;
             this.start = start;
@@ -79,17 +96,22 @@ public class ConcurrencyTest {
         @Override
         public void run() {
             try {
-                Collection collection = DatabaseManager.getCollection("xmldb:exist:///db/test", "admin", "");
-                XPathQueryServiceImpl service = (XPathQueryServiceImpl) collection.getService("XQueryService", "1.0");
-                service.beginProtected();
+                final Collection collection = DatabaseManager.getCollection("xmldb:exist:///db/test", "admin", "");
+                final XPathQueryServiceImpl service = (XPathQueryServiceImpl) collection.getService("XQueryService", "1.0");
+                if(protect) {
+                    service.beginProtected();
+                }
                 try {
-                    if (start > 0)
+                    if (start > 0) {
                         service.declareVariable("start", new Integer(start));
+                    }
                     service.query(query);
                 } finally {
-                    service.endProtected();
+                    if(protect) {
+                        service.endProtected();
+                    }
                 }
-            } catch (Exception e) {
+            } catch (final Exception e) {
                 e.printStackTrace();
                 fail(e.getMessage());
             }
@@ -117,11 +139,5 @@ public class ConcurrencyTest {
     public static void cleanup() throws XMLDBException {
         final CollectionManagementService cmgr = (CollectionManagementService) existEmbeddedServer.getRoot().getService("CollectionManagementService", "1.0");
         cmgr.removeCollection("test");
-
-//            Collection configRoot = DatabaseManager.getCollection("xmldb:exist://" + CollectionConfigurationManager.CONFIG_COLLECTION,
-//                    "admin", null);
-//            cmgr = (CollectionManagementService) configRoot.getService("CollectionManagementService", "1.0");
-//            cmgr.removeCollection("db");
-
     }
 }

evolvedbinary/elemental

test/src/org/exist/collections/ConcurrencyTest.java
@@ -16,7 +16,9 @@ import org.xmldb.api.modules.CollectionManagementService;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Test concurrent access to collections.
@@ -26,51 +28,66 @@ public class ConcurrencyTest {
     @ClassRule
     public static final ExistXmldbEmbeddedServer existEmbeddedServer = new ExistXmldbEmbeddedServer(false, true);
 
-    private static final int N_THREADS = 10;
-    private static final int DOC_COUNT = 200; 
+    private static int CONCURRENT_THREADS = Runtime.getRuntime().availableProcessors() * 3;
+    private static final int DOC_COUNT = CONCURRENT_THREADS * 10;
 
     private static final int QUERY_COUNT = 20;
     
-    private static final String QUERY = "/test/c";
+    private static final String QUERY = "collection('/db/test')/test/c";
 
     private static final String REMOVE =
-        "declare namespace xdb=\"http://exist-db.org/xquery/xmldb\";\n" +
+        "declare namespace xmldb=\"http://exist-db.org/xquery/xmldb\";\n" +
         "declare namespace util=\"http://exist-db.org/xquery/util\";" +
-        "declare variable $start external; " +
+        "declare variable $start as xs:integer external; " +
         "let $dummy := util:log('DEBUG', ('Removing $start: ', $start, ' to ', $start + 9)) " +
         "for $i in $start to $start + 9 " +
-        "let $resource := /test[@id = xs:integer($i)] " +
+        "let $resource := collection('/db/test')/test[xs:integer(@id) eq $i] " +
         "return" +
-        "   xdb:remove(util:collection-name($resource), util:document-name($resource))";
+        "   xmldb:remove(util:collection-name($resource), util:document-name($resource))";
 
     @Test
 	public void runTasks() {
-		ExecutorService executor = Executors.newFixedThreadPool(N_THREADS);
+        final ExecutorService executorRemove = newFixedThreadPool(CONCURRENT_THREADS, "concurrencyTest-remove");
+        final ExecutorService executorQuery = newFixedThreadPool(CONCURRENT_THREADS, "concurrencyTest-query");
 
-        for (int i = 1; i <= 20; i++) {
-            executor.submit(new QueryTask(REMOVE, i * 10, true));
+        for (int i = 1; i <= CONCURRENT_THREADS; i++) {
+            executorRemove.submit(new QueryTask(REMOVE, i * 10, true));
             for (int j = 0; j < QUERY_COUNT; j++) {
-                executor.submit(new QueryTask(QUERY, 0, true));
+                executorQuery.submit(new QueryTask(QUERY, 0, true));
             }
         }
 
-        executor.shutdown();
-		boolean terminated = false;
+        executorRemove.shutdown();
+        executorQuery.shutdown();
+		boolean terminatedRemove = false;
+        boolean terminatedQuery = false;
 		try {
-			terminated = executor.awaitTermination(60 * 60, TimeUnit.SECONDS);
-		} catch (InterruptedException e) {
+			terminatedRemove = executorRemove.awaitTermination(60 * 60, TimeUnit.SECONDS);
+            terminatedQuery = executorQuery.awaitTermination(60 * 60, TimeUnit.SECONDS);
+		} catch (final InterruptedException e) {
+            //Nothing to do
 		}
-		assertTrue(terminated);
+		assertTrue(terminatedRemove);
+        assertTrue(terminatedQuery);
     }
 
-    private class QueryTask implements Runnable {
+    private ExecutorService newFixedThreadPool(final int nThreads, final String threadsBaseName) {
+        return Executors.newFixedThreadPool(nThreads, new ThreadFactory() {
+            private final AtomicInteger counter = new AtomicInteger();
 
-        String query;
-        int start = 0;
-        @SuppressWarnings("unused")
-		boolean protect = false;
+            @Override
+            public Thread newThread(final Runnable r) {
+                return new Thread(r, threadsBaseName + "-" + counter.getAndIncrement());
+            }
+        });
+    }
+
+    private static class QueryTask implements Runnable {
+        private final String query;
+        private final int start;
+		private final boolean protect;
         
-        private QueryTask(String query, int start, boolean protect) {
+        private QueryTask(final String query, final int start, final boolean protect) {
             this.query = query;
             this.protect = protect;
             this.start = start;
@@ -79,17 +96,22 @@ public class ConcurrencyTest {
         @Override
         public void run() {
             try {
-                Collection collection = DatabaseManager.getCollection("xmldb:exist:///db/test", "admin", "");
-                EXistXPathQueryService service = (EXistXPathQueryService) collection.getService("XQueryService", "1.0");
-                service.beginProtected();
+                final Collection collection = DatabaseManager.getCollection("xmldb:exist:///db/test", "admin", "");
+                final EXistXPathQueryService service = (EXistXPathQueryService) collection.getService("XQueryService", "1.0");
+                if(protect) {
+                    service.beginProtected();
+                }
                 try {
-                    if (start > 0)
+                    if (start > 0) {
                         service.declareVariable("start", new Integer(start));
+                    }
                     service.query(query);
                 } finally {
-                    service.endProtected();
+                    if(protect) {
+                        service.endProtected();
+                    }
                 }
-            } catch (Exception e) {
+            } catch (final Exception e) {
                 e.printStackTrace();
                 fail(e.getMessage());
             }
@@ -117,11 +139,5 @@ public class ConcurrencyTest {
     public static void cleanup() throws XMLDBException {
         final CollectionManagementService cmgr = (CollectionManagementService) existEmbeddedServer.getRoot().getService("CollectionManagementService", "1.0");
         cmgr.removeCollection("test");
-
-//            Collection configRoot = DatabaseManager.getCollection("xmldb:exist://" + CollectionConfigurationManager.CONFIG_COLLECTION,
-//                    "admin", null);
-//            cmgr = (CollectionManagementService) configRoot.getService("CollectionManagementService", "1.0");
-//            cmgr.removeCollection("db");
-
     }
 }