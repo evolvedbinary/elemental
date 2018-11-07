evolvedbinary/elemental

commit 1b2bfb9dc81482197c60d3b9ba2665a5b145bffa

[bugfix] Secure the XMLReader processing Closes https://github.com/eXist-db/exist/issues/2180

src/org/exist/util/ExistSAXParserFactory.java
@@ -21,6 +21,7 @@
  */
 package org.exist.util;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import javax.xml.parsers.SAXParserFactory;
 
@@ -28,30 +29,29 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- *  Helper class for creating an instance of javax.xml.parsers.SAXParserFactory
- * 
+ * Helper class for creating an instance of javax.xml.parsers.SAXParserFactory
+ *
  * @author dizzzz@exist-db.org
  */
 public class ExistSAXParserFactory {
 
     private final static Logger LOG = LogManager.getLogger(ExistSAXParserFactory.class);
 
-    public final static String ORG_EXIST_SAXPARSERFACTORY="org.exist.SAXParserFactory";
+    public final static String ORG_EXIST_SAXPARSERFACTORY = "org.exist.SAXParserFactory";
 
     /**
-     *  Get SAXParserFactory instance specified by factory class name.
+     * Get SAXParserFactory instance specified by factory class name.
      *
      * @param className Full class name of factory
-     *
      * @return A Sax parser factory or NULL when not available.
      */
-    public static SAXParserFactory getSAXParserFactory(String className) {
+    public static SAXParserFactory getSAXParserFactory(final String className) {
 
         Class<?> clazz = null;
         try {
             clazz = Class.forName(className);
 
-        } catch (final Exception ex) { // ClassNotFoundException
+        } catch (final ClassNotFoundException ex) {
             // quick escape
             LOG.debug(className + ": " + ex.getMessage(), ex);
             return null;
@@ -61,7 +61,7 @@ public class ExistSAXParserFactory {
         Method method = null;
         try {
             method = clazz.getMethod("newInstance", (Class[]) null);
-        } catch (final Exception ex) { // SecurityException and NoSuchMethodException
+        } catch (final SecurityException | NoSuchMethodException ex) {
             // quick escape
             LOG.debug("Method " + className + ".newInstance not found.", ex);
             return null;
@@ -72,7 +72,7 @@ public class ExistSAXParserFactory {
         try {
             result = method.invoke(null, (Object[]) null);
 
-        } catch (final Exception ex) { //IllegalAccessException and InvocationTargetException
+        } catch (final IllegalAccessException | InvocationTargetException ex) {
             // quick escape
             LOG.debug("Could not invoke method " + className + ".newInstance.", ex);
             return null;
@@ -84,11 +84,10 @@ public class ExistSAXParserFactory {
         }
 
         return (SAXParserFactory) result;
-
     }
 
     /**
-     *  Get instance of a SAXParserFactory. Return factory specified by
+     * Get instance of a SAXParserFactory. Return factory specified by
      * system property org.exist.SAXParserFactory (if available) otherwise
      * return system default.
      *
@@ -109,7 +108,7 @@ public class ExistSAXParserFactory {
         // If no factory could be retrieved, create system default property.
         if (factory == null) {
             factory = SAXParserFactory.newInstance();
-            LOG.debug(String.format("Using default SAXParserFactory '%s'", factory.getClass().getCanonicalName()));
+            LOG.info(String.format("Using default SAXParserFactory '%s'", factory.getClass().getCanonicalName()));
         }
 
         return factory;

extensions/debuggee/src/org/exist/debugger/Utils.java
@@ -24,9 +24,6 @@ package org.exist.debugger;
 import java.io.IOException;
 import java.io.StringReader;
 
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
 import org.exist.Namespaces;
 import org.exist.dom.memtree.NodeImpl;
 import org.exist.dom.memtree.SAXAdapter;
@@ -44,28 +41,30 @@ public class Utils {
     public static NodeImpl nodeFromString(XQueryContext context, String source) throws IOException {
         SAXAdapter adapter = new SAXAdapter(context);
 
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setNamespaceAware(true);
-        
-        XMLReader xr;
-		try {
-			SAXParser parser = factory.newSAXParser();
-
-			xr = parser.getXMLReader();
-			xr.setContentHandler(adapter);
-			xr.setProperty(Namespaces.SAX_LEXICAL_HANDLER, adapter);
-
-		} catch (Exception e) {
-			throw new IOException(e);
-		}
-        
+        final XMLReaderPool parserPool = context.getBroker().getBrokerPool().getParserPool();
+        XMLReader xr = null;
         try {
-            InputSource src = new InputSource(new StringReader(source));
-            xr.parse(src);
-            
-            return (NodeImpl) adapter.getDocument();
-		} catch (SAXException e) {
-			throw new IOException(e);
+            try {
+                xr = parserPool.borrowXMLReader();
+                xr.setContentHandler(adapter);
+                xr.setProperty(Namespaces.SAX_LEXICAL_HANDLER, adapter);
+
+            } catch (Exception e) {
+                throw new IOException(e);
+            }
+
+            try {
+                InputSource src = new InputSource(new StringReader(source));
+                xr.parse(src);
+
+                return (NodeImpl) adapter.getDocument();
+            } catch (SAXException e) {
+                throw new IOException(e);
+            }
+        } finally {
+            if (xr != null) {
+                parserPool.returnXMLReader(xr);
+            }
         }
     }
 }

extensions/debuggee/src/org/exist/debugger/dbgp/ResponseImpl.java
@@ -32,6 +32,7 @@ import org.apache.mina.core.session.IoSession;
 import org.exist.debugger.DebuggerImpl;
 import org.exist.debugger.Response;
 import org.exist.dom.memtree.SAXAdapter;
+import org.exist.util.ExistSAXParserFactory;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -53,7 +54,7 @@ public class ResponseImpl implements Response {
 	public ResponseImpl(IoSession session, InputStream inputStream) {
 		this.session = session;
 		
-		SAXParserFactory factory = SAXParserFactory.newInstance();
+		SAXParserFactory factory = ExistSAXParserFactory.getSAXParserFactory();
 		factory.setNamespaceAware(true);
 		InputSource src = new InputSource(inputStream);
 		SAXParser parser;

extensions/indexes/spatial/src/org/exist/indexing/spatial/AbstractGMLJDBCIndexWorker.java
@@ -78,8 +78,6 @@ import org.xml.sax.helpers.AttributesImpl;
 import org.xml.sax.helpers.XMLFilterImpl;
 
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
 import java.io.StringReader;
@@ -642,15 +640,13 @@ public abstract class AbstractGMLJDBCIndexWorker implements IndexWorker {
             gmlString = gmlTransformer.transform(geometry);
         } catch (TransformerException e) {
             throw new SpatialIndexException(e);
-        } 
+        }
 
+        final XMLReaderPool parserPool = pool.getParserPool();
+        XMLReader reader = null;
         try {
-            //Copied from org.exist.xquery.functions.request.getData
-            SAXParserFactory factory = SAXParserFactory.newInstance();
-            factory.setNamespaceAware(true);
             InputSource src = new InputSource(new StringReader(gmlString));
-            SAXParser parser = factory.newSAXParser();
-            XMLReader reader = parser.getXMLReader();
+            reader = parserPool.borrowXMLReader();
             reader.setContentHandler((ContentHandler)receiver);
             reader.parse(src);
             Document doc = receiver.getDocument();
@@ -660,7 +656,11 @@ public abstract class AbstractGMLJDBCIndexWorker implements IndexWorker {
         } catch (SAXException e) {
             throw new SpatialIndexException(e);
         } catch (IOException e) {
-            throw new SpatialIndexException(e);	
+            throw new SpatialIndexException(e);
+        } finally {
+            if (reader != null) {
+                parserPool.returnXMLReader(reader);
+            }
         }
     }
 

extensions/indexes/spatial/test/src/org/exist/indexing/spatial/GMLIndexTest.java
@@ -46,6 +46,7 @@ import org.exist.dom.memtree.SAXAdapter;
 import org.exist.security.PermissionDeniedException;
 import org.exist.storage.BrokerPool;
 import org.exist.storage.DBBroker;
+import org.exist.util.ExistSAXParserFactory;
 import org.exist.xmldb.IndexQueryService;
 import org.exist.xmldb.XmldbURI;
 import org.exist.xquery.XPathException;
@@ -203,7 +204,7 @@ public class GMLIndexTest extends TestCase {
             AbstractGMLJDBCIndexWorker indexWorker = (AbstractGMLJDBCIndexWorker) broker.getIndexController().getWorkerByIndexId(AbstractGMLJDBCIndex.ID);
             //Unplugged
             if (indexWorker != null) {
-                SAXParserFactory factory = SAXParserFactory.newInstance();
+                SAXParserFactory factory = ExistSAXParserFactory.getSAXParserFactory();
                 factory.setNamespaceAware(true);
                 InputSource src = new InputSource(new StringReader(IN_MEMORY_GML));
                 SAXParser parser = factory.newSAXParser();

extensions/modules/src/org/exist/xquery/modules/sql/ExecuteFunction.java
@@ -25,6 +25,7 @@ package org.exist.xquery.modules.sql;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import org.exist.util.XMLReaderPool;
 import org.exist.util.io.FastByteArrayOutputStream;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -60,8 +61,6 @@ import java.sql.SQLXML;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.sql.Types;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 import org.exist.dom.memtree.AppendingSAXAdapter;
 import org.exist.dom.memtree.ReferenceNode;
 import org.exist.dom.memtree.SAXAdapter;
@@ -263,17 +262,21 @@ public class ExecuteFunction extends BasicFunction
                                         // Add a null indicator attribute if the value was SQL Null
                                         builder.addAttribute( new QName( "null", SQLModule.NAMESPACE_URI, SQLModule.PREFIX ), "true" );
                                     } else {
-
-                                        SAXParserFactory factory = SAXParserFactory.newInstance();
-                                        factory.setNamespaceAware(true);
                                         InputSource src = new InputSource(sqlXml.getCharacterStream());
-                                        SAXParser parser = factory.newSAXParser();
-                                        XMLReader xr = parser.getXMLReader();
+                                        final XMLReaderPool parserPool = context.getBroker().getBrokerPool().getParserPool();
+                                        XMLReader reader = null;
+                                        try {
+                                            reader = parserPool.borrowXMLReader();
 
-                                        SAXAdapter adapter = new AppendingSAXAdapter(builder);
-                                        xr.setContentHandler(adapter);
-                                        xr.setProperty(Namespaces.SAX_LEXICAL_HANDLER, adapter);
-                                        xr.parse(src);
+                                            SAXAdapter adapter = new AppendingSAXAdapter(builder);
+                                            reader.setContentHandler(adapter);
+                                            reader.setProperty(Namespaces.SAX_LEXICAL_HANDLER, adapter);
+                                            reader.parse(src);
+                                        } finally {
+                                            if (reader != null) {
+                                                parserPool.returnXMLReader(reader);
+                                            }
+                                        }
                                     }
                                 } catch(Exception e) {
                                     throw new XPathException("Could not parse column of type SQLXML: " + e.getMessage(), e);

src/org/exist/backup/AbstractBackupDescriptor.java
@@ -22,6 +22,7 @@
 
 package org.exist.backup;
 
+import org.exist.util.XMLReaderPool;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
@@ -33,10 +34,6 @@ import java.io.IOException;
 
 import java.util.Date;
 import java.util.Properties;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 
 
 public abstract class AbstractBackupDescriptor implements BackupDescriptor
@@ -74,15 +71,18 @@ public abstract class AbstractBackupDescriptor implements BackupDescriptor
         return( timestamp > getDate().getTime() );
     }
 
-
-    public void parse( ContentHandler handler ) throws IOException, SAXException, ParserConfigurationException
+    @Override
+    public void parse(final XMLReaderPool parserPool, final ContentHandler handler ) throws IOException, SAXException
     {
-        final SAXParserFactory saxFactory = SAXParserFactory.newInstance();
-        saxFactory.setNamespaceAware( true );
-        saxFactory.setValidating( false );
-        final SAXParser sax    = saxFactory.newSAXParser();
-        final XMLReader reader = sax.getXMLReader();
-        reader.setContentHandler( handler );
-        reader.parse( getInputSource() );
+        XMLReader reader = null;
+        try {
+            reader = parserPool.borrowXMLReader();
+            reader.setContentHandler(handler);
+            reader.parse(getInputSource());
+        } finally {
+            if (reader != null) {
+                parserPool.returnXMLReader(reader);
+            }
+        }
     }
 }

src/org/exist/backup/BackupDescriptor.java
@@ -21,6 +21,7 @@
  */
 package org.exist.backup;
 
+import org.exist.util.XMLReaderPool;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
@@ -31,8 +32,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Date;
 import java.util.Properties;
-
-import javax.xml.parsers.ParserConfigurationException;
 
 
 public interface BackupDescriptor
@@ -86,7 +85,8 @@ public interface BackupDescriptor
     boolean before( long timestamp );
 
 
-    void parse( ContentHandler handler ) throws IOException, SAXException, ParserConfigurationException;
+    void parse(XMLReaderPool parserPool, ContentHandler handler)
+            throws IOException, SAXException;
 
     Path getRepoBackup() throws IOException;
 }

commit 21af0c2c95772dcf2d60e08f5b9240188298429a

[bugfix] Fix a regression with verbose logging in ExistSAXParserFactory introduced by 1b2bfb9

src/org/exist/util/ExistSAXParserFactory.java
@@ -53,7 +53,9 @@ public class ExistSAXParserFactory {
 
         } catch (final ClassNotFoundException ex) {
             // quick escape
-            LOG.debug(className + ": " + ex.getMessage(), ex);
+            if (LOG.isDebugEnabled()) {
+                LOG.debug(className + ": " + ex.getMessage(), ex);
+            }
             return null;
         }
 
@@ -63,7 +65,9 @@ public class ExistSAXParserFactory {
             method = clazz.getMethod("newInstance", (Class[]) null);
         } catch (final SecurityException | NoSuchMethodException ex) {
             // quick escape
-            LOG.debug("Method " + className + ".newInstance not found.", ex);
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Method " + className + ".newInstance not found.", ex);
+            }
             return null;
         }
 
@@ -74,12 +78,16 @@ public class ExistSAXParserFactory {
 
         } catch (final IllegalAccessException | InvocationTargetException ex) {
             // quick escape
-            LOG.debug("Could not invoke method " + className + ".newInstance.", ex);
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Could not invoke method " + className + ".newInstance.", ex);
+            }
             return null;
         }
 
         if (!(result instanceof SAXParserFactory)) {
-            LOG.debug("Could not create instance of SAXParserFactory: " + result.toString());
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Could not create instance of SAXParserFactory: " + result.toString());
+            }
             return null;
         }
 
@@ -108,7 +116,9 @@ public class ExistSAXParserFactory {
         // If no factory could be retrieved, create system default property.
         if (factory == null) {
             factory = SAXParserFactory.newInstance();
-            LOG.info(String.format("Using default SAXParserFactory '%s'", factory.getClass().getCanonicalName()));
+            if (LOG.isDebugEnabled()) {
+                LOG.debug(String.format("Using default SAXParserFactory '%s'", factory.getClass().getCanonicalName()));
+            }
         }
 
         return factory;