evolvedbinary/exist

[feature] Serialize preserved XML Declaration by default from Java Admin Client

exist-core/src/main/java/org/exist/client/InteractiveClient.java
@@ -84,6 +84,8 @@ import org.xmldb.api.modules.XUpdateQueryService;
 import se.softhouse.jargo.ArgumentException;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static javax.xml.transform.OutputKeys.OMIT_XML_DECLARATION;
+import static org.exist.storage.serializers.EXistOutputKeys.OMIT_ORIGINAL_XML_DECLARATION;
 import static org.exist.storage.serializers.EXistOutputKeys.OUTPUT_DOCTYPE;
 
 /**
@@ -138,6 +140,8 @@ public class InteractiveClient {
         defaultProps.setProperty(USER, USER_DEFAULT);
         defaultProps.setProperty(EDITOR, EDIT_CMD);
         defaultProps.setProperty(INDENT, "true");
+        defaultProps.setProperty(OMIT_XML_DECLARATION, "no");
+        defaultProps.setProperty(OMIT_ORIGINAL_XML_DECLARATION, "no");
         defaultProps.setProperty(OUTPUT_DOCTYPE, "true");
         defaultProps.setProperty(ENCODING, ENCODING_DEFAULT.name());
         defaultProps.setProperty(COLORS, "false");

evolvedbinary/elemental

exist-core/src/main/java/org/exist/client/InteractiveClient.java
@@ -89,6 +89,8 @@ import org.xmldb.api.modules.XUpdateQueryService;
 import se.softhouse.jargo.ArgumentException;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static javax.xml.transform.OutputKeys.OMIT_XML_DECLARATION;
+import static org.exist.storage.serializers.EXistOutputKeys.OMIT_ORIGINAL_XML_DECLARATION;
 import static org.exist.storage.serializers.EXistOutputKeys.OUTPUT_DOCTYPE;
 import static org.xmldb.api.base.ResourceType.XML_RESOURCE;
 
@@ -143,6 +145,8 @@ public class InteractiveClient {
         DEFAULT_PROPERTIES.setProperty(USER, USER_DEFAULT);
         DEFAULT_PROPERTIES.setProperty(EDITOR, EDIT_CMD);
         DEFAULT_PROPERTIES.setProperty(INDENT, "true");
+        DEFAULT_PROPERTIES.setProperty(OMIT_XML_DECLARATION, "no");
+        DEFAULT_PROPERTIES.setProperty(OMIT_ORIGINAL_XML_DECLARATION, "no");
         DEFAULT_PROPERTIES.setProperty(OUTPUT_DOCTYPE, "true");
         DEFAULT_PROPERTIES.setProperty(ENCODING, ENCODING_DEFAULT.name());
         DEFAULT_PROPERTIES.setProperty(COLORS, "false");