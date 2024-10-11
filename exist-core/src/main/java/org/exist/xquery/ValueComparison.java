evolvedbinary/exist

[refactor] Implement fn:deep-equal in terms of deepCompare (needed for GroupByClause)

exist-core/src/main/java/org/exist/xquery/ValueComparison.java
@@ -21,8 +21,7 @@
  */
 package org.exist.xquery;
 
-import java.util.Iterator;
-
+import com.evolvedbinary.j8fu.tuple.Tuple2;
 import com.ibm.icu.text.Collator;
 import org.exist.dom.persistent.ContextItem;
 import org.exist.dom.persistent.ExtArrayNodeSet;
@@ -36,6 +35,10 @@ import org.exist.xquery.value.BooleanValue;
 import org.exist.xquery.value.Item;
 import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.Type;
+
+import javax.annotation.Nullable;
+
+import static com.evolvedbinary.j8fu.tuple.Tuple.Tuple;
 
 /**
  * @author <a href="mailto:wolfgang@exist-db.org">Wolfgang Meier</a>
@@ -132,57 +135,12 @@ public class ValueComparison extends GeneralComparison {
 	 * @return the result of the comparison
 	 * @throws XPathException in case of dynamic error
 	 */
-	public static boolean compareAtomic(Collator collator, AtomicValue lv, AtomicValue rv, StringTruncationOperator truncation, Comparison relation) throws XPathException {
-		int ltype = lv.getType();
-		int rtype = rv.getType();
-		if (ltype == Type.UNTYPED_ATOMIC) {
-			//If the atomized operand is of type xs:untypedAtomic, it is cast to xs:string.
-			lv = lv.convertTo(Type.STRING);
-		} 
-		if (rtype == Type.UNTYPED_ATOMIC) {
-			//If the atomized operand is of type xs:untypedAtomic, it is cast to xs:string.
-			rv = rv.convertTo(Type.STRING);
-		}
-		ltype = lv.getType();
-		rtype = rv.getType();
-		final int ctype = Type.getCommonSuperType(ltype, rtype);
-		//Next, if possible, the two operands are converted to their least common type 
-		//by a combination of type promotion and subtype substitution.
-		if (ctype == Type.NUMBER) {
-			//Numeric type promotion:
+	public static boolean compareAtomic(@Nullable final Collator collator, AtomicValue lv, AtomicValue rv, final StringTruncationOperator truncation, final Comparison relation) throws XPathException {
+		final Tuple2<AtomicValue, AtomicValue> comparableLvRv = makeComparable(lv, rv, truncation);
+		lv = comparableLvRv._1;
+		rv = comparableLvRv._2;
 
-			//A value of type xs:decimal (or any type derived by restriction from xs:decimal) 
-			//can be promoted to either of the types xs:float or xs:double. The result of this promotion is created by casting the original value to the required type. This kind of promotion may cause loss of precision.
-			if (ltype == Type.DECIMAL) {
-				if (rtype == Type.FLOAT)
-					{lv = lv.convertTo(Type.FLOAT);}
-				else if (rtype == Type.DOUBLE)
-					{lv = lv.convertTo(Type.DOUBLE);}				
-			} else if (rtype == Type.DECIMAL) {
-				if (ltype == Type.FLOAT)
-					{rv = rv.convertTo(Type.FLOAT);}
-				else if (ltype == Type.DOUBLE)
-					{rv = rv.convertTo(Type.DOUBLE);}				
-			} else {
-				//A value of type xs:float (or any type derived by restriction from xs:float) 
-				//can be promoted to the type xs:double. 
-				//The result is the xs:double value that is the same as the original value.
-				if (ltype == Type.FLOAT && rtype == Type.DOUBLE)
-					{lv = lv.convertTo(Type.DOUBLE);}
-				if (rtype == Type.FLOAT && ltype == Type.DOUBLE)
-					{rv = rv.convertTo(Type.DOUBLE);}
-			}
-		} else {
-			lv = lv.convertTo(ctype);
-			rv = rv.convertTo(ctype);
-		}
-
-		// if truncation is set, we always do a string comparison
-        if (truncation != StringTruncationOperator.NONE) {
-        	//TODO : log this ?
-            lv = lv.convertTo(Type.STRING);
-        }
-		switch(truncation) {
+		switch (truncation) {
 			case RIGHT:
 				return lv.startsWith(collator, rv);
 			case LEFT:
@@ -192,7 +150,93 @@ public class ValueComparison extends GeneralComparison {
 			default:
 				return lv.compareTo(collator, relation, rv);
 		}
-	}	
+	}
+
+	/**
+	 * Cast the atomic operands into a comparable type
+	 * and compare them.
+	 *
+	 * @param collator the collator to use
+	 * @param lv left hand operand value
+	 * @param rv right hand operand value
+	 * @return the result of the comparison
+	 * @throws XPathException in case of dynamic error
+	 */
+	public static int compareAtomic(@Nullable final Collator collator, AtomicValue lv, AtomicValue rv) throws XPathException {
+		final Tuple2<AtomicValue, AtomicValue> comparableLvRv = makeComparable(lv, rv, StringTruncationOperator.NONE);
+		lv = comparableLvRv._1;
+		rv = comparableLvRv._2;
+
+		return lv.compareTo(collator, rv);
+	}
+
+	/**
+	 * Cast the atomic operands into a comparable type.
+	 *
+	 * @param lv left hand operand value
+	 * @param rv right hand operand value
+	 * @param truncation should strings be truncated before comparison
+	 *                   ({@link StringTruncationOperator#NONE} by default)
+	 * @return the result of the comparison
+	 * @throws XPathException in case of dynamic error
+	 */
+	private static Tuple2<AtomicValue, AtomicValue> makeComparable(AtomicValue lv, AtomicValue rv, final StringTruncationOperator truncation) throws XPathException {
+			int ltype = lv.getType();
+			int rtype = rv.getType();
+			if (ltype == Type.UNTYPED_ATOMIC) {
+				//If the atomized operand is of type xs:untypedAtomic, it is cast to xs:string.
+				lv = lv.convertTo(Type.STRING);
+			}
+			if (rtype == Type.UNTYPED_ATOMIC) {
+				//If the atomized operand is of type xs:untypedAtomic, it is cast to xs:string.
+				rv = rv.convertTo(Type.STRING);
+			}
+			ltype = lv.getType();
+			rtype = rv.getType();
+			final int ctype = Type.getCommonSuperType(ltype, rtype);
+			//Next, if possible, the two operands are converted to their least common type
+			//by a combination of type promotion and subtype substitution.
+			if (ctype == Type.NUMBER) {
+				//Numeric type promotion:
+
+				//A value of type xs:decimal (or any type derived by restriction from xs:decimal)
+				//can be promoted to either of the types xs:float or xs:double. The result of this promotion is created by casting the original value to the required type. This kind of promotion may cause loss of precision.
+				if (ltype == Type.DECIMAL) {
+					if (rtype == Type.FLOAT) {
+						lv = lv.convertTo(Type.FLOAT);
+					} else if (rtype == Type.DOUBLE) {
+						lv = lv.convertTo(Type.DOUBLE);
+					}
+				} else if (rtype == Type.DECIMAL) {
+					if (ltype == Type.FLOAT) {
+						rv = rv.convertTo(Type.FLOAT);
+					} else if (ltype == Type.DOUBLE) {
+						rv = rv.convertTo(Type.DOUBLE);
+					}
+				} else {
+					//A value of type xs:float (or any type derived by restriction from xs:float)
+					//can be promoted to the type xs:double.
+					//The result is the xs:double value that is the same as the original value.
+					if (ltype == Type.FLOAT && rtype == Type.DOUBLE) {
+						lv = lv.convertTo(Type.DOUBLE);
+					}
+					if (rtype == Type.FLOAT && ltype == Type.DOUBLE) {
+						rv = rv.convertTo(Type.DOUBLE);
+					}
+				}
+			} else {
+				lv = lv.convertTo(ctype);
+				rv = rv.convertTo(ctype);
+			}
+
+			// if truncation is set, we always do a string comparison
+			if (truncation != StringTruncationOperator.NONE) {
+				//TODO : log this ?
+				lv = lv.convertTo(Type.STRING);
+			}
+
+			return Tuple(lv, rv);
+	}
     
     public void dump(ExpressionDumper dumper) {
         getLeft().dump(dumper);

evolvedbinary/elemental

exist-core/src/main/java/org/exist/xquery/ValueComparison.java
@@ -21,6 +21,7 @@
  */
 package org.exist.xquery;
 
+import com.evolvedbinary.j8fu.tuple.Tuple2;
 import com.ibm.icu.text.Collator;
 import org.exist.dom.persistent.ContextItem;
 import org.exist.dom.persistent.ExtArrayNodeSet;
@@ -34,6 +35,10 @@ import org.exist.xquery.value.BooleanValue;
 import org.exist.xquery.value.Item;
 import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.Type;
+
+import javax.annotation.Nullable;
+
+import static com.evolvedbinary.j8fu.tuple.Tuple.Tuple;
 
 /**
  * @author <a href="mailto:wolfgang@exist-db.org">Wolfgang Meier</a>
@@ -130,63 +135,108 @@ public class ValueComparison extends GeneralComparison {
 	 * @return the result of the comparison
 	 * @throws XPathException in case of dynamic error
 	 */
-	public static boolean compareAtomic(Collator collator, AtomicValue lv, AtomicValue rv, StringTruncationOperator truncation, Comparison relation) throws XPathException {
-		int ltype = lv.getType();
-		int rtype = rv.getType();
-		if (ltype == Type.UNTYPED_ATOMIC) {
-			//If the atomized operand is of type xs:untypedAtomic, it is cast to xs:string.
-			lv = lv.convertTo(Type.STRING);
-		} 
-		if (rtype == Type.UNTYPED_ATOMIC) {
-			//If the atomized operand is of type xs:untypedAtomic, it is cast to xs:string.
-			rv = rv.convertTo(Type.STRING);
-		}
-		ltype = lv.getType();
-		rtype = rv.getType();
-		final int ctype = Type.getCommonSuperType(ltype, rtype);
-		//Next, if possible, the two operands are converted to their least common type 
-		//by a combination of type promotion and subtype substitution.
-		if (ctype == Type.NUMERIC) {
-			//Numeric type promotion:
+	public static boolean compareAtomic(@Nullable final Collator collator, AtomicValue lv, AtomicValue rv, final StringTruncationOperator truncation, final Comparison relation) throws XPathException {
+		final Tuple2<AtomicValue, AtomicValue> comparableLvRv = makeComparable(lv, rv, truncation);
+		lv = comparableLvRv._1;
+		rv = comparableLvRv._2;
 
-			//A value of type xs:decimal (or any type derived by restriction from xs:decimal) 
-			//can be promoted to either of the types xs:float or xs:double. The result of this promotion is created by casting the original value to the required type. This kind of promotion may cause loss of precision.
-			if (ltype == Type.DECIMAL) {
-				if (rtype == Type.FLOAT)
-					{lv = lv.convertTo(Type.FLOAT);}
-				else if (rtype == Type.DOUBLE)
-					{lv = lv.convertTo(Type.DOUBLE);}				
-			} else if (rtype == Type.DECIMAL) {
-				if (ltype == Type.FLOAT)
-					{rv = rv.convertTo(Type.FLOAT);}
-				else if (ltype == Type.DOUBLE)
-					{rv = rv.convertTo(Type.DOUBLE);}				
-			} else {
-				//A value of type xs:float (or any type derived by restriction from xs:float) 
-				//can be promoted to the type xs:double. 
-				//The result is the xs:double value that is the same as the original value.
-				if (ltype == Type.FLOAT && rtype == Type.DOUBLE)
-					{lv = lv.convertTo(Type.DOUBLE);}
-				if (rtype == Type.FLOAT && ltype == Type.DOUBLE)
-					{rv = rv.convertTo(Type.DOUBLE);}
+		switch (truncation) {
+			case RIGHT:
+				return lv.startsWith(collator, rv);
+			case LEFT:
+				return lv.endsWith(collator, rv);
+			case BOTH:
+				return lv.contains(collator, rv);
+			default:
+				return lv.compareTo(collator, relation, rv);
+		}
+	}
+
+	/**
+	 * Cast the atomic operands into a comparable type
+	 * and compare them.
+	 *
+	 * @param collator the collator to use
+	 * @param lv left hand operand value
+	 * @param rv right hand operand value
+	 * @return the result of the comparison
+	 * @throws XPathException in case of dynamic error
+	 */
+	public static int compareAtomic(@Nullable final Collator collator, AtomicValue lv, AtomicValue rv) throws XPathException {
+		final Tuple2<AtomicValue, AtomicValue> comparableLvRv = makeComparable(lv, rv, StringTruncationOperator.NONE);
+		lv = comparableLvRv._1;
+		rv = comparableLvRv._2;
+
+		return lv.compareTo(collator, rv);
+	}
+
+	/**
+	 * Cast the atomic operands into a comparable type.
+	 *
+	 * @param lv left hand operand value
+	 * @param rv right hand operand value
+	 * @param truncation should strings be truncated before comparison
+	 *                   ({@link StringTruncationOperator#NONE} by default)
+	 * @return the result of the comparison
+	 * @throws XPathException in case of dynamic error
+	 */
+	private static Tuple2<AtomicValue, AtomicValue> makeComparable(AtomicValue lv, AtomicValue rv, final StringTruncationOperator truncation) throws XPathException {
+			int ltype = lv.getType();
+			int rtype = rv.getType();
+			if (ltype == Type.UNTYPED_ATOMIC) {
+				//If the atomized operand is of type xs:untypedAtomic, it is cast to xs:string.
+				lv = lv.convertTo(Type.STRING);
 			}
-		} else {
-			lv = lv.convertTo(ctype);
-			rv = rv.convertTo(ctype);
-		}
+			if (rtype == Type.UNTYPED_ATOMIC) {
+				//If the atomized operand is of type xs:untypedAtomic, it is cast to xs:string.
+				rv = rv.convertTo(Type.STRING);
+			}
+			ltype = lv.getType();
+			rtype = rv.getType();
+			final int ctype = Type.getCommonSuperType(ltype, rtype);
+			//Next, if possible, the two operands are converted to their least common type
+			//by a combination of type promotion and subtype substitution.
+			if (ctype == Type.NUMERIC) {
+				//Numeric type promotion:
 
-		// if truncation is set, we always do a string comparison
-        if (truncation != StringTruncationOperator.NONE) {
-        	//TODO : log this ?
-            lv = lv.convertTo(Type.STRING);
-        }
-        return switch (truncation) {
-            case RIGHT -> lv.startsWith(collator, rv);
-            case LEFT -> lv.endsWith(collator, rv);
-            case BOTH -> lv.contains(collator, rv);
-            default -> lv.compareTo(collator, relation, rv);
-        };
-	}	
+				//A value of type xs:decimal (or any type derived by restriction from xs:decimal)
+				//can be promoted to either of the types xs:float or xs:double. The result of this promotion is created by casting the original value to the required type. This kind of promotion may cause loss of precision.
+				if (ltype == Type.DECIMAL) {
+					if (rtype == Type.FLOAT) {
+						lv = lv.convertTo(Type.FLOAT);
+					} else if (rtype == Type.DOUBLE) {
+						lv = lv.convertTo(Type.DOUBLE);
+					}
+				} else if (rtype == Type.DECIMAL) {
+					if (ltype == Type.FLOAT) {
+						rv = rv.convertTo(Type.FLOAT);
+					} else if (ltype == Type.DOUBLE) {
+						rv = rv.convertTo(Type.DOUBLE);
+					}
+				} else {
+					//A value of type xs:float (or any type derived by restriction from xs:float)
+					//can be promoted to the type xs:double.
+					//The result is the xs:double value that is the same as the original value.
+					if (ltype == Type.FLOAT && rtype == Type.DOUBLE) {
+						lv = lv.convertTo(Type.DOUBLE);
+					}
+					if (rtype == Type.FLOAT && ltype == Type.DOUBLE) {
+						rv = rv.convertTo(Type.DOUBLE);
+					}
+				}
+			} else {
+				lv = lv.convertTo(ctype);
+				rv = rv.convertTo(ctype);
+			}
+
+			// if truncation is set, we always do a string comparison
+			if (truncation != StringTruncationOperator.NONE) {
+				//TODO : log this ?
+				lv = lv.convertTo(Type.STRING);
+			}
+
+			return Tuple(lv, rv);
+	}
     
     public void dump(ExpressionDumper dumper) {
         getLeft().dump(dumper);