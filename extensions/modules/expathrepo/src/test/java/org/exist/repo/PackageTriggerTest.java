fc67307b736653675d50d5ba5f54f8633672745b
Adam Retter <adam.retter@googlemail.com>
2024-10-28 13:19:07 +0100

    [bugfix] Fix failing PackageTriggerTest.java due to classpath issues

--- extensions/modules/expathrepo/src/test/java/org/exist/repo/PackageTriggerTest.java
+++ extensions/modules/expathrepo/src/test/java/org/exist/repo/PackageTriggerTest.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -22,7 +46,6 @@
 package org.exist.repo;
 
 import org.exist.EXistException;
-import org.exist.Version;
 import org.exist.collections.Collection;
 import org.exist.security.PermissionDeniedException;
 import org.exist.storage.BrokerPool;
@@ -50,7 +73,7 @@ import static org.exist.collections.CollectionConfiguration.DEFAULT_COLLECTION_C
 
 public class PackageTriggerTest {
 
-    static final String xarFile = "exist-expathrepo-trigger-test-" + Version.getVersion() + ".xar";
+    static final String xarFile = "exist-expathrepo-trigger-test.xar"; // NOTE(AR) a copy of this should be present in `src/test/resources`.
     static final XmldbURI triggerTestCollection = XmldbURI.create("/db");
     static final XmldbURI xarUri = triggerTestCollection.append(xarFile);
 
