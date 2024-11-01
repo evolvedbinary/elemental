fda0131c0aaca0133a313d98cfe31af795db111d
Adam Retter <adam.retter@googlemail.com>
2024-11-01 22:01:55 +0100

    [bugfix] Allow comparison of xs:dateTime and xs:dateTimeStamp Closes https://github.com/eXist-db/exist/issues/5478

--- exist-core/src/main/java/org/exist/xquery/value/AbstractDateTimeValue.java
+++ exist-core/src/main/java/org/exist/xquery/value/AbstractDateTimeValue.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -47,6 +71,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ * @author <a href="mailto:adam@evolvedbinary.com">Adam Retter</a>
  * @author wolf
  * @author <a href="mailto:piotr@ideanest.com">Piotr Kaminski</a>
  * @author ljo
@@ -368,7 +393,7 @@ public abstract class AbstractDateTimeValue extends ComputableValue {
     }
 
     public int compareTo(Collator collator, AtomicValue other) throws XPathException {
-        if (other.getType() == getType()) {
+        if (other.getType() == getType() || other.getType() == Type.DATE_TIME_STAMP && getType() == Type.DATE_TIME || other.getType() == Type.DATE_TIME && getType() == Type.DATE_TIME_STAMP) {
             // filling in missing timezones with local timezone, should be total order as per XPath 2.0 10.4
             final int r = getImplicitCalendar().compare(((AbstractDateTimeValue) other).getImplicitCalendar());
             if (r == DatatypeConstants.INDETERMINATE) {
