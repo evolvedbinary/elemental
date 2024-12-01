26f08a014a640b4f1db53cac37a1b55374946fb8
Adam Retter <adam.retter@googlemail.com>
2024-12-01 22:46:25 +0000

    [optimise] Re-working of 'Increased locking granularity' (commit 7269e20 by @alanpaxton (Alan Paxton))

--- exist-core/src/main/java/org/exist/storage/DBBroker.java
+++ exist-core/src/main/java/org/exist/storage/DBBroker.java
@@ -1,27 +1,12 @@
 /*
- * Elemental
- * Copyright (C) 2024, Evolved Binary Ltd
+ * Copyright (C) 2014 Evolved Binary Ltd
  *
- * admin@evolvedbinary.com
- * https://www.evolvedbinary.com | https://www.elemental.xyz
+ * Changes made by Evolved Binary are proprietary and are not Open Source.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; version 2.1.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ * NOTE: Parts of this file contain code from The eXist-db Authors.
  *       The original license header is included below.
  *
- * =====================================================================
+ * ----------------------------------------------------------------------------
  *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
@@ -66,7 +51,9 @@ import org.exist.security.PermissionDeniedException;
 import org.exist.security.Subject;
 import org.exist.stax.IEmbeddedXMLStreamReader;
 import org.exist.storage.btree.BTreeCallback;
+import org.exist.storage.dom.GranularNodeIterator;
 import org.exist.storage.dom.INodeIterator;
+import org.exist.storage.dom.ManualLockNodeIterator;
 import org.exist.storage.lock.EnsureLocked;
 import org.exist.storage.lock.EnsureUnlocked;
 import org.exist.storage.lock.Lock.LockMode;
@@ -500,7 +487,7 @@ public abstract class DBBroker implements AutoCloseable {
     }
 
     /**
-     * Return a {@link org.exist.storage.dom.NodeIterator} starting at the
+     * Return a {@link GranularNodeIterator} starting at the
      * specified node.
      * 
      * @param node the NodeHandle
@@ -508,6 +495,18 @@ public abstract class DBBroker implements AutoCloseable {
      * @throws RuntimeException not implemented
      */
     public INodeIterator getNodeIterator(NodeHandle node) throws RuntimeException {
+        throw new RuntimeException("not implemented for this storage backend");
+    }
+
+    /**
+     * Return a {@link ManualLockNodeIterator} starting at the
+     * specified node.
+     *
+     * @param node the NodeHandle
+     * @return ManualLockNodeIterator of node.
+     * @throws RuntimeException not implemented
+     */
+    public ManualLockNodeIterator getManualLockNodeIterator(NodeHandle node) throws RuntimeException {
         throw new RuntimeException("not implemented for this storage backend");
     }
 
