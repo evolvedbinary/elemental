26c2bb4002ff3af869cdb45faf2f92289fb0d265
Adam Retter <adam.retter@googlemail.com>
2025-01-16 14:07:48 +0100

    [bugfix] Improve handling of XML BOM Closes https://github.com/eXist-db/exist/issues/5610

--- exist-core/src/main/java/org/exist/xquery/functions/xmldb/XMLDBStore.java
+++ exist-core/src/main/java/org/exist/xquery/functions/xmldb/XMLDBStore.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -65,12 +89,14 @@ import org.xmldb.api.base.XMLDBException;
 import org.xmldb.api.modules.BinaryResource;
 import org.xmldb.api.modules.XMLResource;
 
+import static org.exist.util.ByteOrderMark.stripXmlBom;
 import static org.exist.xquery.FunctionDSL.*;
 import static org.exist.xquery.XPathException.execAndAddErrorIfMissing;
 import static org.exist.xquery.functions.xmldb.XMLDBModule.functionSignature;
 import static org.exist.xquery.functions.xmldb.XMLDBModule.functionSignatures;
 
 /**
+ * @author <a href="mailto:adam@evolvedbinary.com">Adam Retter</a>
  * @author wolf
  */
 public class XMLDBStore extends XMLDBAbstractCollectionManipulator {
@@ -196,7 +222,7 @@ public class XMLDBStore extends XMLDBAbstractCollectionManipulator {
                 }
 
                 if (Type.subTypeOf(item.getType(), Type.STRING)) {
-                    resource.setContent(item.getStringValue());
+                    resource.setContent(stripXmlBom(item.getStringValue()));
 
                 } else if (item.getType() == Type.BASE64_BINARY) {
                     resource.setContent(((BinaryValue) item).toJavaObject());
