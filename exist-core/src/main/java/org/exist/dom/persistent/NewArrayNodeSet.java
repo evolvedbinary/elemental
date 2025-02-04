89a32287bb87b53677c9f18feff18a80606f01be
Alan Paxton <alan.paxton@gmail.com>
2025-02-04 15:13:00 +0000

    find siblings is known to be sorted
    IS IT ? Need more confirmation from @adamretter, but it appears to work for this workload. Take forward and we can discuss later..

--- exist-core/src/main/java/org/exist/dom/persistent/NewArrayNodeSet.java
+++ exist-core/src/main/java/org/exist/dom/persistent/NewArrayNodeSet.java
@@ -527,6 +527,13 @@ public class NewArrayNodeSet extends AbstractArrayNodeSet implements ExtNodeSet,
     }
 
     @Override
+    public void setKnownSorted(final boolean mergeContexts) {
+        isSorted = true;
+        removeDuplicates(mergeContexts);
+        updateDocs();
+    }
+
+    @Override
     public void sort(final boolean mergeContexts) {
         if(isSorted) {
             return;
--- exist-core/src/main/java/org/exist/xquery/LocationStep.java
+++ exist-core/src/main/java/org/exist/xquery/LocationStep.java
@@ -898,6 +898,7 @@ public class LocationStep extends Step {
                                         + "'");
                     }
                     currentSet = index.findElementsByTagName(ElementValue.ELEMENT, docs, test.getName(), null, this);
+                    currentSet.setKnownSorted(true);
                     currentDocs = docs;
                     registerUpdateListener();
                 }
