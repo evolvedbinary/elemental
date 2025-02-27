f40c2a7acb7feafac2f04393fdd7f96d002db42a
Adam Retter <adam.retter@googlemail.com>
2025-02-27 23:54:06 +0100

    fixup! WIP Refactoring and cleanup of BTree storage classes

--- exist-core/src/main/java/org/exist/storage/btree/TreeMetrics.java
+++ exist-core/src/main/java/org/exist/storage/btree/TreeMetrics.java
@@ -63,8 +63,8 @@ public class TreeMetrics {
         this.btreeName = btreeName;
     }
 
-    public void addPage(final PageStatus pageStatus) {
-        if (pageStatus == PageStatus.BRANCH) {
+    public void addPage(final PageType pageType) {
+        if (pageType == PageType.BRANCH) {
             addInnerPage();
         } else {
             addLeafPage();
