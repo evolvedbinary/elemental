35f858587f2ee7a22ac75c70bbaa193166df769d
Adam Retter <adam.retter@googlemail.com>
2025-05-09 20:28:01 -0400

    [bugfix] Fix issues that stop the JMX Client from running correctly Closes https://github.com/eXist-db/exist/issues/5706

--- exist-core/src/main/java/org/exist/management/impl/Database.java
+++ exist-core/src/main/java/org/exist/management/impl/Database.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -22,8 +46,7 @@
 package org.exist.management.impl;
 
 import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
@@ -88,15 +111,15 @@ public class Database implements DatabaseMXBean {
     }
 
     @Override
-    public List<ActiveBroker> getActiveBrokersMap() {
-        final List<ActiveBroker> brokersList = new ArrayList<>();
+    public Map<String, ActiveBroker> getActiveBrokersMap() {
+        final Map<String, ActiveBroker> brokersList = new HashMap<>();
 
         for (final Map.Entry<Thread, DBBroker> entry : pool.getActiveBrokers().entrySet()) {
             final Thread thread = entry.getKey();
             final DBBroker broker = entry.getValue();
             final String trace = printStackTrace(thread);
             final String watchdogTrace = pool.getWatchdog().map(wd -> wd.get(broker)).orElse(null);
-            brokersList.add(new ActiveBroker(thread.getName(), broker.getReferenceCount(), trace, watchdogTrace));
+            brokersList.put(broker.getId(), new ActiveBroker(thread.getName(), broker.getReferenceCount(), trace, watchdogTrace));
         }
         return brokersList;
     }
