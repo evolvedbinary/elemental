9b5058f4deb2704dbaa07393d7cfa68f8a44f4e6
Adam Retter <adam.retter@googlemail.com>
2025-05-11 20:54:11 -0400

    [bugfix] Make sure to reload the collection after removing a resource Closes https://github.com/eXist-db/exist/issues/5711

--- exist-core/src/main/java/org/exist/client/NewResourceDialog.java
+++ exist-core/src/main/java/org/exist/client/NewResourceDialog.java
@@ -1,4 +1,31 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * Use of this software is governed by the Business Source License 1.1
+ * included in the LICENSE file and at www.mariadb.com/bsl11.
+ *
+ * Change Date: 2028-04-27
+ *
+ * On the date above, in accordance with the Business Source License, use
+ * of this software will be governed by the Apache License, Version 2.0.
+ *
+ * Additional Use Grant: Production use of the Licensed Work for a permitted
+ * purpose. A Permitted Purpose is any purpose other than a Competing Use.
+ * A Competing Use means making the Software available to others in a commercial
+ * product or service that: substitutes for the Software; substitutes for any
+ * other product or service we offer using the Software that exists as of the
+ * date we make the Software available; or offers the same or substantially
+ * similar functionality as the Software.
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -44,6 +71,8 @@ import org.xmldb.api.base.Resource;
 import org.xmldb.api.base.XMLDBException;
 import org.xmldb.api.modules.BinaryResource;
 import org.xmldb.api.modules.XMLResource;
+
+import static org.exist.util.StringUtil.isNullOrEmpty;
 
 /**
  *
@@ -296,9 +325,5 @@ public class NewResourceDialog extends JFrame {
         } catch(final XMLDBException xmldbe) {
             ClientFrame.showErrorMessage(xmldbe.getMessage(), xmldbe);
         }
-    }
-
-    private boolean isNullOrEmpty(String str) {
-        return str == null || str.isEmpty();
     }
 }
