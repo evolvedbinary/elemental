38c0d24239686ac2f1d4ad17b36c25f399056290
Adam Retter <adam.retter@googlemail.com>
2025-06-04 16:33:56 +0100

    [bugfix] Improve macOS notarization process Closes https://github.com/evolvedbinary/elemental/issues/2

--- exist-distribution/src/main/scripts/codesign-lz4-java-mac.sh
+++ exist-distribution/src/main/scripts/codesign-lz4-java-mac.sh
@@ -22,7 +22,7 @@
 
 
 # $1 is .app/Contents/Java dir
-# $2 is the jansi version
+# $2 is the lz4-java version
 # $3 is temp work directory
 # $4 the mac codesign identity
 
@@ -31,31 +31,32 @@ set -e
 #set -x  ## enable to help debug
 
 # ensure a clean temp work directory
-if [ -d "${3}/org" ]
+if [ -d "${3}/net" ]
 then
-  rm -rf "${3}/org"
+  rm -rf "${3}/net"
 fi
 
 # for each native arch
-archs=('arm64' 'x86' 'x86_64')
+archs=('aarch64' 'x86_64')
 for arch in ${archs[@]}
 do
   # create the temp output dirs
-  mkdir -p "${3}/org/fusesource/jansi/internal/native/Mac/${arch}"
+  mkdir -p "${3}/net/jpountz/util/darwin/${arch}"
 
   # switch to temp output dir
   pushd "${3}"
 
   # extract the native files
-  jar -xf "${1}/jansi-${2}.jar" "org/fusesource/jansi/internal/native/Mac/${arch}/libjansi.jnilib"
+  jar -xf "${1}/lz4-java-${2}.jar" "net/jpountz/util/darwin/${arch}/liblz4-java.dylib"
 
   # test if the file is unsigned, and sign if needed
-  /usr/bin/codesign --verbose --test-requirement="=anchor trusted" --verify "org/fusesource/jansi/internal/native/Mac/${arch}/libjansi.jnilib" || /usr/bin/codesign --verbose --force --timestamp --sign "${4}" "org/fusesource/jansi/internal/native/Mac/${arch}/libjansi.jnilib"
+  /usr/bin/codesign --verbose --test-requirement="=anchor trusted" --verify "net/jpountz/util/darwin/${arch}/liblz4-java.dylib" || /usr/bin/codesign --verbose --force --timestamp --sign "${4}" "net/jpountz/util/darwin/${arch}/liblz4-java.dylib"
 
   # overwrite the file in the jar
-  jar -uf "${1}/jansi-${2}.jar" "org/fusesource/jansi/internal/native/Mac/${arch}/libjansi.jnilib"
+  jar -uf "${1}/lz4-java-${2}.jar" "net/jpountz/util/darwin/${arch}/liblz4-java.dylib"
 
   # switch back from temp output dir
   popd
 
 done
+
