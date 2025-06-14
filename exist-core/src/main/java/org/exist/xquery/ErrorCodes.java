evolvedbinary/elemental

commit 91ab58fe4fa019d68c3c2cb98c0a8561a50900c9

[feature] New implementation of Java Binding from XQuery Closes https://github.com/eXist-db/exist/issues/5090

exist-core/pom.xml
@@ -753,17 +753,21 @@
                                 <include>src/main/java/org/exist/util/JREUtil.java</include>
                                 <include>src/main/java/org/exist/util/OSUtil.java</include>
                                 <include>src/main/java/org/exist/util/StringUtil.java</include>
+                                <include>src/main/java/org/exist/xquery/JavaBinding.java</include>
                                 <include>src/main/java/org/exist/xquery/functions/fn/FunDocAvailable.java</include>
                                 <include>src/test/java/org/exist/xquery/ImportFromPkgTest.java</include>
+                                <include>src/test/java/org/exist/xquery/JavaBindingTest.java</include>
                                 <include>src/test/java/org/exist/xquery/WatchdogTest.java</include>
                                 <include>src/test/java/org/exist/xquery/value/DateTimeTypesTest.java</include>
                                 <include>src/test/java/org/exist/xquery/functions/fn/FunXmlToJsonTest.java</include>
                                 <include>src/test/java/org/exist/xquery/functions/fn/ParsingFunctionsTest.java</include>
                                 <include>src/test/java/org/exist/xquery/functions/fn/transform/ConvertTest.java</include>
                                 <include>src/test/java/org/exist/xquery/functions/fn/transform/FunTransformITTest.java</include>
+                                <include>src/main/java/org/exist/xquery/functions/system/FunctionAvailable.java</include>
                                 <include>src/test/java/org/exist/xquery/functions/xmldb/XMLDBStoreTest.java</include>
                                 <include>src/test/java/org/exist/xquery/functions/xquery3/SerializeTest.java</include>
                                 <include>src/test/resources-filtered/org/exist/xquery/import-from-pkg-test.conf.xml</include>
+                                <include>src/test/resources-filtered/org/exist/xquery/JavaBindingTest.conf.xml</include>
                                 <include>src/test/xquery/securitymanager/acl.xqm</include>
                             </includes>
                         </licenseSet>
@@ -903,16 +907,19 @@
                                 <include>src/main/java/org/exist/xmldb/RemoteRestoreService.java</include>
                                 <include>src/main/java/org/exist/xmlrpc/ExistRpcTypeFactory.java</include>
                                 <include>src/main/java/org/exist/xqj/Marshaller.java</include>
+                                <include>src/main/java/org/exist/xquery/DynamicTypeCheck.java</include>
+                                <include>src/main/java/org/exist/xquery/ErrorCodes.java</include>
                                 <include>src/main/java/org/exist/xquery/UserDefinedFunction.java</include>
                                 <include>src/main/java/org/exist/xquery/FunctionFactory.java</include>
                                 <include>src/main/java/org/exist/xquery/Optimizer.java</include>
+                                <include>src/main/java/org/exist/xquery/XPathUtil.java</include>
                                 <include>src/main/java/org/exist/xquery/XQueryContext.java</include>
                                 <include>src/main/java/org/exist/xquery/functions/fn/FunDeepEqual.java</include>
                                 <include>src/main/java/org/exist/xquery/functions/fn/FunUriCollection.java</include>
                                 <include>src/main/java/org/exist/xquery/functions/fn/ParsingFunctions.java</include>
                                 <include>src/main/java/org/exist/xquery/functions/integer/WordPicture.java</include>
-                                <include>src/main/java/org/exist/xquery/functions/system/FunctionAvailable.java</include>
                                 <include>src/main/java/org/exist/xquery/functions/system/GetUptime.java</include>
+                                <include>src/main/java/org/exist/xquery/functions/system/SystemModule.java</include>
                                 <include>src/main/java/org/exist/xquery/functions/system/Shutdown.java</include>
                                 <include>src/main/java/org/exist/xquery/functions/system/TriggerSystemTask.java</include>
                                 <include>src/main/java/org/exist/xquery/functions/util/BuiltinFunctions.java</include>
@@ -924,6 +931,7 @@
                                 <include>src/test/java/org/exist/xquery/update/UpdateReplaceTest.java</include>
                                 <include>src/main/java/org/exist/xquery/util/SerializerUtils.java</include>
                                 <include>src/main/java/org/exist/xquery/value/AbstractDateTimeValue.java</include>
+                                <include>src/main/java/org/exist/xquery/value/SequenceType.java</include>
                                 <include>src/main/java/org/exist/xquery/value/Type.java</include>
                                 <include>src/main/java/org/exist/xslt/EXistURIResolver.java</include>
                                 <include>src/main/resources-filtered/org/exist/system.properties</include>
@@ -946,6 +954,7 @@
                                 <include>src/main/java/org/exist/management/impl/JMXAgent.java</include>
                                 <include>src/test/java/org/exist/xmldb/CreateCollectionsTest.java</include>
                                 <include>src/test/java/org/exist/xquery/XQueryFunctionsTest.java</include>
+                                <include>src/main/java/org/exist/xquery/functions/array/ArrayType.java</include>
                                 <include>src/test/java/org/exist/xquery/functions/fn/DocTest.java</include>
                                 <include>src/main/java/org/exist/xquery/functions/fn/FnModule.java</include>
                                 <include>src/main/java/org/exist/xquery/functions/fn/FunBaseURI.java</include>
@@ -997,6 +1006,7 @@
                                 <exclude>src/test/resources-filtered/conf.xml</exclude>
                                 <exclude>src/test/resources-filtered/org/exist/storage/statistics/conf.xml</exclude>
                                 <exclude>src/test/resources-filtered/org/exist/xquery/import-from-pkg-test.conf.xml</exclude>
+                                <exclude>src/test/resources-filtered/org/exist/xquery/JavaBindingTest.conf.xml</exclude>
                                 <exclude>src/test/resources-filtered/org/exist/xquery/functions/transform/transform-from-pkg-test.conf.xml</exclude>
                                 <exclude>src/main/antlr/org/exist/xquery/parser/XQueryTree.g</exclude>
                                 <exclude>src/main/java/org/exist/Indexer.java</exclude>
@@ -1126,13 +1136,19 @@
                                 <exclude>src/main/java/org/exist/xmldb/RemoteRestoreService.java</exclude>
                                 <exclude>src/main/java/org/exist/xmlrpc/ExistRpcTypeFactory.java</exclude>
                                 <exclude>src/main/java/org/exist/xqj/Marshaller.java</exclude>
+                                <exclude>src/main/java/org/exist/xquery/DynamicTypeCheck.java</exclude>
+                                <exclude>src/main/java/org/exist/xquery/ErrorCodes.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/FunctionFactory.java</exclude>
                                 <exclude>src/test/java/org/exist/xquery/ImportFromPkgTest.java</exclude>
+                                <exclude>src/main/java/org/exist/xquery/JavaBinding.java</exclude>
+                                <exclude>src/test/java/org/exist/xquery/JavaBindingTest.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/NameTest.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/Optimizer.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/UserDefinedFunction.java</exclude>
                                 <exclude>src/test/java/org/exist/xquery/WatchdogTest.java</exclude>
+                                <exclude>src/main/java/org/exist/xquery/XPathUtil.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/XQueryContext.java</exclude>
+                                <exclude>src/main/java/org/exist/xquery/functions/array/ArrayType.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/functions/fn/FnModule.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/functions/fn/FunDeepEqual.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/functions/fn/FunDocAvailable.java</exclude>
@@ -1152,6 +1168,7 @@
                                 <exclude>src/main/java/org/exist/xquery/functions/system/FunctionAvailable.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/functions/system/GetUptime.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/functions/system/Shutdown.java</exclude>
+                                <exclude>src/main/java/org/exist/xquery/functions/system/SystemModule.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/functions/system/TriggerSystemTask.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/functions/xmldb/XMLDBStore.java</exclude>
                                 <exclude>src/test/java/org/exist/xquery/functions/xmldb/XMLDBStoreTest.java</exclude>
@@ -1278,6 +1295,7 @@
                                 <exclude>src/main/java/org/exist/xquery/value/AtomicValueComparator.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/value/ItemComparator.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/value/SequenceComparator.java</exclude>
+                                <exclude>src/main/java/org/exist/xquery/value/SequenceType.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/value/SubSequence.java</exclude>
                                 <exclude>src/test/java/org/exist/xquery/value/SubSequenceRangeTest.java</exclude>
                                 <exclude>src/test/java/org/exist/xquery/value/SubSequenceTest.java</exclude>

exist-core/src/main/java/org/exist/xquery/ErrorCodes.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -262,6 +286,10 @@ public class ErrorCodes {
     public static final ErrorCode EXXQDY0005 = new EXistErrorCode("EXXQDY0005", "No function call details were provided when trying to execute a Library Module.");
     public static final ErrorCode EXXQDY0006 = new EXistErrorCode("EXXQDY0006", "Unable to find named function when trying to execute a Library Module.");
 
+    public static final ErrorCode EXXQST0001 = new EXistErrorCode("EXXQST0001", "Java binding is disabled in the current configuration.");
+    public static final ErrorCode EXXQST0002 = new EXistErrorCode("EXXQST0002", "No Java binding possible for the indicated Java class.");
+    public static final ErrorCode EXXQST0003 = new EXistErrorCode("EXXQST0003", "No Java binding possible for the indicated Java field/method.");
+
     public static final ErrorCode ERROR = new EXistErrorCode("ERROR", "Error.");
 
     public static class ErrorCode {
@@ -309,12 +337,12 @@ public class ErrorCodes {
 
     public static class JavaErrorCode extends ErrorCode {
 
-        public JavaErrorCode(Throwable throwable) {
+        public JavaErrorCode(final Throwable throwable) {
             super(new QName(
                         throwable.getClass().getName(),
                         Namespaces.EXIST_JAVA_BINDING_NS,
                         Namespaces.EXIST_JAVA_BINDING_NS_PREFIX),
-                  throwable.getMessage());
+                  throwable.getMessage() != null ? throwable.getMessage() : throwable.getCause().getMessage());
         }
     }
 }

exist-core/src/main/java/org/exist/util/Configuration.java
@@ -229,6 +229,7 @@ import static org.exist.util.io.VirtualTempPath.DEFAULT_IN_MEMORY_SIZE;
 import static org.exist.xquery.FunctionFactory.DISABLE_DEPRECATED_FUNCTIONS_ATTRIBUTE;
 import static org.exist.xquery.FunctionFactory.DISABLE_DEPRECATED_FUNCTIONS_BY_DEFAULT;
 import static org.exist.xquery.FunctionFactory.ENABLE_JAVA_BINDING_ATTRIBUTE;
+import static org.exist.xquery.FunctionFactory.ENABLE_JAVA_BINDING_BY_DEFAULT;
 import static org.exist.xquery.FunctionFactory.PROPERTY_DISABLE_DEPRECATED_FUNCTIONS;
 import static org.exist.xquery.FunctionFactory.PROPERTY_ENABLE_JAVA_BINDING;
 import static org.exist.xquery.XQueryContext.BUILT_IN_MODULE_CLASS_ATTRIBUTE;
@@ -544,7 +545,7 @@ public class Configuration implements ErrorHandler {
 
     private void configureXQuery(final Element xquery) throws DatabaseConfigurationException {
         //java binding
-        configureProperty(xquery, ENABLE_JAVA_BINDING_ATTRIBUTE, PROPERTY_ENABLE_JAVA_BINDING);
+        configureProperty(xquery, ENABLE_JAVA_BINDING_ATTRIBUTE, PROPERTY_ENABLE_JAVA_BINDING, Configuration::asBoolean, ENABLE_JAVA_BINDING_BY_DEFAULT);
         configureProperty(xquery, DISABLE_DEPRECATED_FUNCTIONS_ATTRIBUTE, PROPERTY_DISABLE_DEPRECATED_FUNCTIONS, Configuration::asBoolean, DISABLE_DEPRECATED_FUNCTIONS_BY_DEFAULT);
         configureProperty(xquery, ENABLE_QUERY_REWRITING_ATTRIBUTE, PROPERTY_ENABLE_QUERY_REWRITING);
         configureProperty(xquery, ENFORCE_INDEX_USE_ATTRIBUTE, PROPERTY_ENFORCE_INDEX_USE);

exist-core/src/main/java/org/exist/xquery/Cardinality.java
@@ -188,6 +188,20 @@ public enum Cardinality {
         throw new IllegalArgumentException("No know cardinality for intValue: " + intValue);
     }
 
+    /**
+     * Compare this Cardinality to another Cardinality.
+     *
+     * NOTE we can't implement (override) {@link Comparable#compareTo(Object)}
+     * here as it is final in {@link java.lang.Enum}.
+     *
+     * @param other the other Cardinality to be compared.
+     *
+     * @return a negative integer, zero, or a positive integer as this Cardinality is less than, equal to, or greater than the other Cardinality.
+     */
+    public int compare(final Cardinality other) {
+        return Byte.compare(val, other.val);
+    }
+
     static class InternalValue {
         static final byte ZERO = 1;
         static final byte ONE = 2;

exist-core/src/main/java/org/exist/xquery/DynamicTypeCheck.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -81,6 +105,9 @@ public class DynamicTypeCheck extends AbstractExpression {
             if (type == NodeProxy.UNKNOWN_NODE_TYPE)
                 //Retrieve the actual node
                 {type= ((NodeProxy) item).getNode().getNodeType();}
+        }
+        if (requiredType == Type.JAVA_OBJECT) {
+            type = Type.JAVA_OBJECT;
         }
         if(type != requiredType && !Type.subTypeOf(type, requiredType)) {
             //TODO : how to make this block more generic ? -pb

commit 0e3cde8046073a0dbf05a4ff388959de7127cfba

[bugfix] Avoid NPE under rare circumstances in translating a Java Exception to an XQuery Error Code. This is a regression that was introduced in 91ab58f. Closes https://github.com/evolvedbinary/elemental/issues/7

exist-core/pom.xml
@@ -924,6 +924,7 @@
                                 <include>src/main/java/org/exist/xquery/ErrorCodes.java</include>
                                 <include>src/main/java/org/exist/xquery/FunctionFactory.java</include>
                                 <include>src/main/java/org/exist/xquery/Optimizer.java</include>
+                                <include>src/main/java/org/exist/xquery/TryCatchExpression.java</include>
                                 <include>src/main/java/org/exist/xquery/UserDefinedFunction.java</include>
                                 <include>src/main/java/org/exist/xquery/XPathUtil.java</include>
                                 <include>src/main/java/org/exist/xquery/XQueryContext.java</include>
@@ -1233,6 +1234,7 @@
                                 <exclude>src/main/java/org/exist/xquery/Materializable.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/NameTest.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/Optimizer.java</exclude>
+                                <exclude>src/main/java/org/exist/xquery/TryCatchExpression.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/UserDefinedFunction.java</exclude>
                                 <exclude>src/test/java/org/exist/xquery/WatchdogTest.java</exclude>
                                 <exclude>src/main/java/org/exist/xquery/XPathUtil.java</exclude>

exist-core/src/main/java/org/exist/xquery/ErrorCodes.java
@@ -48,6 +48,8 @@ package org.exist.xquery;
 import org.exist.Namespaces;
 import org.exist.dom.QName;
 
+import javax.annotation.Nullable;
+
 /**
  *
  * @author aretter
@@ -295,14 +297,14 @@ public class ErrorCodes {
     public static class ErrorCode {
 
         private final QName errorQName;
-        private final String description;
+        private @Nullable final String description;
 
-        public ErrorCode(String code, String description) {
+        public ErrorCode(final String code, @Nullable final String description) {
             this.errorQName = new QName(code, Namespaces.EXIST_XQUERY_XPATH_ERROR_NS, Namespaces.EXIST_XQUERY_XPATH_ERROR_PREFIX);
             this.description = description;
         }
 
-        public ErrorCode(QName errorQName, String description) {
+        public ErrorCode(final QName errorQName, final String description) {
             this.errorQName = errorQName;
             this.description = description;
         }
@@ -313,10 +315,10 @@ public class ErrorCodes {
 
         @Override
         public String toString() {
-            return "(" + errorQName.getNamespaceURI() + "#" + errorQName.getLocalPart() + "):" + description;
+            return "(" + errorQName.toString() + "): " + description;
         }
 
-        public String getDescription(){
+        public @Nullable String getDescription(){
             return description;
         }
     }
@@ -337,12 +339,21 @@ public class ErrorCodes {
 
     public static class JavaErrorCode extends ErrorCode {
 
-        public JavaErrorCode(final Throwable throwable) {
-            super(new QName(
-                        throwable.getClass().getName(),
-                        Namespaces.EXIST_JAVA_BINDING_NS,
-                        Namespaces.EXIST_JAVA_BINDING_NS_PREFIX),
-                  throwable.getMessage() != null ? throwable.getMessage() : throwable.getCause().getMessage());
+        private JavaErrorCode(final QName errorQName, @Nullable final String description) {
+            super(errorQName, description);
+        }
+
+        public static JavaErrorCode fromThrowable(final Throwable throwable) {
+            final QName errorQName = new QName(throwable.getClass().getName(), Namespaces.EXIST_JAVA_BINDING_NS, Namespaces.EXIST_JAVA_BINDING_NS_PREFIX);
+            @Nullable final String description;
+            if (throwable.getMessage() != null) {
+                description = throwable.getMessage();
+            } else if (throwable.getCause() != null) {
+                description = throwable.getCause().getMessage();
+            } else {
+                description = null;
+            }
+            return new JavaErrorCode(errorQName, description);
         }
     }
 }

exist-core/src/main/java/org/exist/xquery/JavaBinding.java
@@ -558,7 +558,7 @@ public class JavaBinding extends BasicFunction {
             return XPathUtil.javaObjectToXPath(javaResult, context, true, false, false, this);
 
         } catch (final IllegalAccessException | InvocationTargetException | InstantiationException | IllegalArgumentException e) {
-            throw new XPathException(this, new ErrorCodes.JavaErrorCode(e), e.getMessage());
+            throw new XPathException(this, ErrorCodes.JavaErrorCode.fromThrowable(e), e.getMessage());
         }
     }
 

exist-core/src/main/java/org/exist/xquery/TryCatchExpression.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -166,7 +190,7 @@ public class TryCatchExpression extends AbstractExpression {
                 }
             } else {
                 // Get errorcode from all other errors and exceptions
-                errorCode = new JavaErrorCode(throwable);
+                errorCode = JavaErrorCode.fromThrowable(throwable);
             }
 
             // We need the qname in the end
@@ -414,7 +438,7 @@ public class TryCatchExpression extends AbstractExpression {
         }
 
         // Fallback, create java error
-        return new ErrorCodes.JavaErrorCode(retVal);
+        return JavaErrorCode.fromThrowable(retVal);
     }
 
     @Override

extensions/modules/exi/pom.xml
@@ -139,9 +139,9 @@
                             -->
                             <header>${project.parent.relativePath}/../../elemental-parent/elemental-LGPL-21-ONLY-license.template.txt</header>
                             <excludes>
+                                <exclude>exi-functions-demo.xql</exclude>
                                 <exclude>pom.xml</exclude>
                                 <exclude>src/**</exclude>
-                                <exclude>exi-functions-demo.xql</exclude>
                             </excludes>
                         </licenseSet>
 
@@ -160,6 +160,8 @@
                             </multi>
                             <includes>
                                 <include>pom.xml</include>
+                                <include>src/main/java/org/exist/xquery/modules/exi/DecodeExiFunction.java</include>
+                                <include>src/main/java/org/exist/xquery/modules/exi/EncodeExiFunction.java</include>
                             </includes>
                         </licenseSet>
 
@@ -170,6 +172,8 @@
                             <header>${project.parent.relativePath}/../../exist-parent/existdb-LGPL-21-license.template.txt</header>
                             <excludes>
                                 <exclude>pom.xml</exclude>
+                                <exclude>src/main/java/org/exist/xquery/modules/exi/DecodeExiFunction.java</exclude>
+                                <exclude>src/main/java/org/exist/xquery/modules/exi/EncodeExiFunction.java</exclude>
                             </excludes>
                         </licenseSet>
 

extensions/modules/exi/src/main/java/org/exist/xquery/modules/exi/DecodeExiFunction.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -125,8 +149,8 @@ public class DecodeExiFunction extends BasicFunction {
 				context.popDocumentContext();
 			}
 		}
-		catch(EXIException | SAXException | IOException exie) {
-			throw new XPathException(this, new JavaErrorCode(exie.getCause()), exie.getMessage());
+		catch(final EXIException | SAXException | IOException exie) {
+			throw new XPathException(this, JavaErrorCode.fromThrowable(exie.getCause()), exie.getMessage());
 		}
 	}
 

extensions/modules/exi/src/main/java/org/exist/xquery/modules/exi/EncodeExiFunction.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -114,8 +138,8 @@ public class EncodeExiFunction extends BasicFunction {
 			// TODO - test!
 			throw new XPathException(this, ErrorCodes.FODC0002, ioex.getMessage());
 		}
-		catch(EXIException | SAXException exie) {
-			throw new XPathException(this, new JavaErrorCode(exie.getCause()), exie.getMessage());
+		catch(final EXIException | SAXException exie) {
+			throw new XPathException(this, JavaErrorCode.fromThrowable(exie.getCause()), exie.getMessage());
 		}
 	}
 	
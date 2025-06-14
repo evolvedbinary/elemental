0e3cde8046073a0dbf05a4ff388959de7127cfba
Adam Retter <adam.retter@googlemail.com>
2025-06-14 12:21:23 +0200

    [bugfix] Avoid NPE under rare circumstances in translating a Java Exception to an XQuery Error Code. This is a regression that was introduced in 91ab58f. Closes https://github.com/evolvedbinary/elemental/issues/7

--- extensions/modules/exi/src/main/java/org/exist/xquery/modules/exi/DecodeExiFunction.java
+++ extensions/modules/exi/src/main/java/org/exist/xquery/modules/exi/DecodeExiFunction.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -125,8 +149,8 @@ public class DecodeExiFunction extends BasicFunction {
 				context.popDocumentContext();
 			}
 		}
-		catch(EXIException | SAXException | IOException exie) {
-			throw new XPathException(this, new JavaErrorCode(exie.getCause()), exie.getMessage());
+		catch(final EXIException | SAXException | IOException exie) {
+			throw new XPathException(this, JavaErrorCode.fromThrowable(exie.getCause()), exie.getMessage());
 		}
 	}
 
