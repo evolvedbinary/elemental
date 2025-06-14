d4f063d536b3b27c860418883462c06589c164fc
Adam Retter <adam.retter@googlemail.com>
2025-06-14 12:21:23 +0200

    [bugfix] Avoid NPE under rare circumstances in translating a Java Exception to an XQuery Error Code. This is a regression that was introduced in 91ab58f. Closes https://github.com/evolvedbinary/elemental/issues/7

--- exist-core/src/main/java/org/exist/xquery/TryCatchExpression.java
+++ exist-core/src/main/java/org/exist/xquery/TryCatchExpression.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -169,7 +193,7 @@ public class TryCatchExpression extends AbstractExpression {
                 }
             } else {
                 // Get errorcode from all other errors and exceptions
-                errorCode = new JavaErrorCode(throwable);
+                errorCode = JavaErrorCode.fromThrowable(throwable);
             }
 
             // We need the qname in the end
@@ -417,7 +441,7 @@ public class TryCatchExpression extends AbstractExpression {
         }
 
         // Fallback, create java error
-        return new ErrorCodes.JavaErrorCode(retVal);
+        return JavaErrorCode.fromThrowable(retVal);
     }
 
     @Override
