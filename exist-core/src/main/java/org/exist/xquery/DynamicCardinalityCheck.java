74f399ebc14e49610606afb74496c28d2ef76fcf
Adam Retter <adam.retter@googlemail.com>
2025-06-30 18:28:45 +0200

    [bugfix] Make sure to expose any root error when type checking deferred function calls Closes https://github.com/eXist-db/exist/issues/5139

--- exist-core/src/main/java/org/exist/xquery/DynamicCardinalityCheck.java
+++ exist-core/src/main/java/org/exist/xquery/DynamicCardinalityCheck.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -27,6 +51,8 @@ import org.exist.xquery.util.ExpressionDumper;
 import org.exist.xquery.value.Item;
 import org.exist.xquery.value.Sequence;
 
+import javax.annotation.Nullable;
+
 /**
  * Runtime-check for the cardinality of a function parameter.
  * 
@@ -47,52 +73,71 @@ public class DynamicCardinalityCheck extends AbstractExpression {
         setLocation(expression.getLine(), expression.getColumn());
     }
 
-    /* (non-Javadoc)
-     * @see org.exist.xquery.Expression#analyze(org.exist.xquery.Expression)
-     */
-    public void analyze(AnalyzeContextInfo contextInfo) throws XPathException {
+    @Override
+    public void analyze(final AnalyzeContextInfo contextInfo) throws XPathException {
         contextInfo.setParent(this);
         expression.analyze(contextInfo);
     }
 
-    /* (non-Javadoc)
-     * @see org.exist.xquery.Expression#eval(org.exist.xquery.StaticContext, org.exist.dom.persistent.DocumentSet, org.exist.xquery.value.Sequence, org.exist.xquery.value.Item)
-     */
-    public Sequence eval(Sequence contextSequence, Item contextItem) throws XPathException {
+    @Override
+    public Sequence eval(final Sequence contextSequence, final Item contextItem) throws XPathException {
         if (context.getProfiler().isEnabled()) {
             context.getProfiler().start(this);
-            context.getProfiler().message(this, Profiler.DEPENDENCIES,
-                "DEPENDENCIES", Dependency.getDependenciesName(this.getDependencies()));
-            if (contextSequence != null)
-                {context.getProfiler().message(this, Profiler.START_SEQUENCES,
-                "CONTEXT SEQUENCE", contextSequence);}
-            if (contextItem != null)
-                {context.getProfiler().message(this, Profiler.START_SEQUENCES,
-                "CONTEXT ITEM", contextItem.toSequence());}
+            context.getProfiler().message(this, Profiler.DEPENDENCIES, "DEPENDENCIES", Dependency.getDependenciesName(this.getDependencies()));
+            if (contextSequence != null) {
+                context.getProfiler().message(this, Profiler.START_SEQUENCES, "CONTEXT SEQUENCE", contextSequence);
+            }
+            if (contextItem != null) {
+                context.getProfiler().message(this, Profiler.START_SEQUENCES, "CONTEXT ITEM", contextItem.toSequence());
+            }
         }
+
         final Sequence seq = expression.eval(contextSequence, contextItem);
-        Cardinality actualCardinality;
-        if (seq.isEmpty())
-            {actualCardinality = Cardinality.EMPTY_SEQUENCE;}
-        else if (seq.hasMany())
-            {actualCardinality = Cardinality._MANY;}
-        else
-            {actualCardinality = Cardinality.EXACTLY_ONE;}
+
+        final Cardinality actualCardinality;
+        if (isEmpty(seq)) {
+            actualCardinality = Cardinality.EMPTY_SEQUENCE;
+        } else if (hasMany(seq)) {
+            actualCardinality = Cardinality._MANY;
+        } else {
+            actualCardinality = Cardinality.EXACTLY_ONE;
+        }
+
         if (!requiredCardinality.isSuperCardinalityOrEqualOf(actualCardinality)) {
-            error.addArgs(ExpressionDumper.dump(expression),
-                requiredCardinality.getHumanDescription(),
-                    seq.getItemCount());
+            error.addArgs(ExpressionDumper.dump(expression), requiredCardinality.getHumanDescription(), seq.getItemCount());
             throw new XPathException(this, error.toString());
         }
-        if (context.getProfiler().isEnabled())
-            {context.getProfiler().end(this, "", seq);}
+
+        if (context.getProfiler().isEnabled()) {
+            context.getProfiler().end(this, "", seq);
+        }
+
         return seq;
     }
 
-    /* (non-Javadoc)
-     * @see org.exist.xquery.Expression#dump(org.exist.xquery.util.ExpressionDumper)
-     */
-    public void dump(ExpressionDumper dumper) {
+    private boolean isEmpty(final Sequence sequence) throws XPathException {
+        final boolean empty = sequence.isEmpty();
+        throwIfDeferredFunctionCallAndHasException(sequence);
+        return empty;
+    }
+
+    private boolean hasMany(final Sequence sequence) throws XPathException {
+        final boolean hasMany = sequence.hasMany();
+        throwIfDeferredFunctionCallAndHasException(sequence);
+        return hasMany;
+    }
+
+    private void throwIfDeferredFunctionCallAndHasException(final Sequence sequence) throws XPathException {
+        if (sequence instanceof DeferredFunctionCall) {
+            @Nullable final XPathException caughtException = ((DeferredFunctionCall) sequence).getCaughtException();
+            if (caughtException != null) {
+                throw caughtException;
+            }
+        }
+    }
+
+    @Override
+    public void dump(final ExpressionDumper dumper) {
         if(dumper.verbosity() > 1) {
             dumper.display("dynamic-cardinality-check"); 
             dumper.display("("); 
@@ -108,44 +153,42 @@ public class DynamicCardinalityCheck extends AbstractExpression {
         return expression.toString();
     }
 
-    /* (non-Javadoc)
-     * @see org.exist.xquery.Expression#returnsType()
-     */
+    @Override
     public int returnsType() {
         return expression.returnsType();
     }
 
-    /* (non-Javadoc)
-     * @see org.exist.xquery.AbstractExpression#getDependencies()
-     */
+    @Override
     public int getDependencies() {
         return expression.getDependencies();
     }
 
-    public void setContextDocSet(DocumentSet contextSet) {
+    public void setContextDocSet(final DocumentSet contextSet) {
         super.setContextDocSet(contextSet);
         expression.setContextDocSet(contextSet);
     }
 
-    /* (non-Javadoc)
-     * @see org.exist.xquery.AbstractExpression#resetState()
-     */
-    public void resetState(boolean postOptimization) {
+    @Override
+    public void resetState(final boolean postOptimization) {
         super.resetState(postOptimization);
         expression.resetState(postOptimization);
     }
 
-    public void accept(ExpressionVisitor visitor) {
+    @Override
+    public void accept(final ExpressionVisitor visitor) {
         expression.accept(visitor);
     }
 
+    @Override
     public int getSubExpressionCount() {
         return 1;
     }
-    
-    public Expression getSubExpression(int index) {
-        if (index == 0)
-            {return expression;}
-        throw new IndexOutOfBoundsException("Index: " + index + ", Size: "+getSubExpressionCount());
+
+    @Override
+    public Expression getSubExpression(final int index) {
+        if (index == 0) {
+            return expression;
+        }
+        throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + getSubExpressionCount());
     }
 }
