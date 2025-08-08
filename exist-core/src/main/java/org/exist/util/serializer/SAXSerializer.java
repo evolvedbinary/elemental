cc5f69b87b195781476ceccecaffe93180449f7f
Adam Retter <adam.retter@googlemail.com>
2025-08-08 19:06:34 +0200

    [bugfix] Further improvements to coalescing namespace prefixes See https://github.com/evolvedbinary/elemental/pull/30

--- exist-core/src/main/java/org/exist/util/serializer/SAXSerializer.java
+++ exist-core/src/main/java/org/exist/util/serializer/SAXSerializer.java
@@ -327,7 +327,7 @@ public class SAXSerializer extends AbstractSerializer implements ContentHandler,
             for (final Map.Entry<String, String> nsEntry : optionalNamespaceDecls.entrySet()) {
                 final String prefix = nsEntry.getKey();
                 final String uri = nsEntry.getValue();
-                if (!(elemPrefixedNsIsDefaultNs && uri.equals(namespaceURI) && elemPrefix.equals(prefix))) {
+                if (!(elemPrefixedNsIsDefaultNs && uri.equals(namespaceURI) && StringUtil.equals(qname.getPrefix(), prefix))) {
                     receiver.namespace(prefix, uri);
                 }
             }
@@ -338,7 +338,7 @@ public class SAXSerializer extends AbstractSerializer implements ContentHandler,
                 }
                 final String uri = nsEntry.getValue();
                 if(!optionalNamespaceDecls.containsKey(prefix)) {
-                    if (!(elemPrefixedNsIsDefaultNs && uri.equals(namespaceURI) && elemPrefix.equals(prefix))) {
+                    if (!(elemPrefixedNsIsDefaultNs && uri.equals(namespaceURI) && StringUtil.equals(qname.getPrefix(), prefix))) {
                         receiver.namespace(prefix, uri);
                     }
                 }
