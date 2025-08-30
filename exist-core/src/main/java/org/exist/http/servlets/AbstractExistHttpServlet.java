4a410d421753c0cc4b83493d035a587dce53279d
Adam Retter <adam.retter@googlemail.com>
2025-08-30 14:59:00 +0200

    [refator] Change extraneous log messages to debug level

--- exist-core/src/main/java/org/exist/http/servlets/AbstractExistHttpServlet.java
+++ exist-core/src/main/java/org/exist/http/servlets/AbstractExistHttpServlet.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -98,7 +122,9 @@ public abstract class AbstractExistHttpServlet extends HttpServlet {
 
         // Configure BrokerPool
         if(BrokerPool.isConfigured()) {
-            getLog().info("Database already started. Skipping configuration ...");
+            if (getLog().isDebugEnabled()) {
+                getLog().debug("Database already started. Skipping configuration ...");
+            }
         } else {
             final String confFile = Optional.ofNullable(config.getInitParameter("configuration")).orElse("conf.xml");
 
@@ -186,9 +212,11 @@ public abstract class AbstractExistHttpServlet extends HttpServlet {
             }
 
             if (getDefaultUser() != null) {
-                getLog().info("Using default user {} for all unauthorized requests.", getDefaultUsername());
+                if (getLog().isDebugEnabled()) {
+                    getLog().debug("Using default user '{}' for all unauthorized requests.", getDefaultUsername());
+                }
             } else {
-                getLog().error("Default user {} cannot be found.  A BASIC AUTH challenge will be the default.", getDefaultUsername());
+                getLog().warn("Default user '{}' cannot be found.  A BASIC AUTH challenge will be the default.", getDefaultUsername());
             }
         } else {
             getLog().info("No default user.  All requires must be authorized or will result in a BASIC AUTH challenge.");
