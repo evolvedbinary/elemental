271cff2ee5ea4de4a33d79293d19b04919b0a192
Adam Retter <adam.retter@googlemail.com>
2025-10-07 15:15:21 +0200

    [bugfix] Enable some tests that were previously not running under Surefire Closes https://github.com/eXist-db/exist/issues/5903

--- exist-core/src/test/java/org/exist/storage/RecoverBinary2Test.java
+++ exist-core/src/test/java/org/exist/storage/RecoverBinary2Test.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -22,10 +46,9 @@
 package org.exist.storage;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Optional;
 
 import org.exist.EXistException;
@@ -33,28 +56,29 @@ import org.exist.collections.Collection;
 import org.exist.collections.triggers.TriggerException;
 import org.exist.dom.persistent.BinaryDocument;
 import org.exist.dom.persistent.DocumentImpl;
+import org.exist.samples.Samples;
 import org.exist.security.PermissionDeniedException;
 import org.exist.storage.txn.TransactionManager;
 import org.exist.storage.txn.Txn;
 import org.exist.test.ExistEmbeddedServer;
 import org.exist.test.TestConstants;
 import org.exist.util.*;
+import org.exist.util.io.InputStreamUtil;
 import org.exist.xmldb.XmldbURI;
 import org.junit.After;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
+import static org.exist.samples.Samples.SAMPLES;
 import static org.junit.Assert.assertNotNull;
 
-public class RecoverBinaryTest2 {
+public class RecoverBinary2Test {
 
     // we don't use @ClassRule/@Rule as we want to force corruption in some tests
-    private ExistEmbeddedServer existEmbeddedServer = new ExistEmbeddedServer(true, true);
-
-    private static String directory = "webapp/resources";
+    private ExistEmbeddedServer existEmbeddedServer = new ExistEmbeddedServer(true, false);
 
     @Test
-    public void storeAndRead() throws SAXException, PermissionDeniedException, DatabaseConfigurationException, IOException, LockException, EXistException {
+    public void storeAndRead() throws SAXException, PermissionDeniedException, DatabaseConfigurationException, IOException, LockException, EXistException, URISyntaxException {
         BrokerPool.FORCE_CORRUPTION = true;
         BrokerPool pool = startDb();
         store(pool);
@@ -72,7 +96,7 @@ public class RecoverBinaryTest2 {
         read2(pool);
     }
 
-    public void store(final BrokerPool pool) throws EXistException, DatabaseConfigurationException, PermissionDeniedException, IOException, SAXException, LockException {
+    public void store(final BrokerPool pool) throws EXistException, DatabaseConfigurationException, PermissionDeniedException, IOException, SAXException, LockException, URISyntaxException {
         final TransactionManager transact = pool.getTransactionManager();
 
         try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()));
@@ -91,7 +115,7 @@ public class RecoverBinaryTest2 {
         }
     }
 
-    public void read(final BrokerPool pool) throws EXistException, DatabaseConfigurationException, PermissionDeniedException, LockException, IOException, SAXException {
+    public void read(final BrokerPool pool) throws EXistException, DatabaseConfigurationException, PermissionDeniedException, LockException, IOException, SAXException, URISyntaxException {
         try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()))) {
             final Collection test2 = broker.getCollection(TestConstants.TEST_COLLECTION_URI2);
             for (final Iterator<DocumentImpl> i = test2.iterator(broker); i.hasNext(); ) {
@@ -110,7 +134,7 @@ public class RecoverBinaryTest2 {
         }
     }
 
-    public void read2(final BrokerPool pool) throws EXistException, DatabaseConfigurationException, PermissionDeniedException, IOException, TriggerException, LockException {
+    public void read2(final BrokerPool pool) throws EXistException, PermissionDeniedException, IOException, TriggerException, LockException {
         try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()))) {
 
             final Collection test2 = broker.getCollection(TestConstants.TEST_COLLECTION_URI2);
@@ -130,22 +154,17 @@ public class RecoverBinaryTest2 {
     }
     
     private void storeFiles(final DBBroker broker, final Txn transaction, final Collection test2) throws IOException, EXistException, PermissionDeniedException, LockException, SAXException {
-        // Get files in directory
-        final Path dir = FileUtils.resolve(ConfigurationHelper.getExistHome(), directory);
-        final List<Path> files = FileUtils.list(dir);
-        assertNotNull("Check directory '"+dir.toAbsolutePath().toString()+"'.",files);
-        
         // store some documents.
         for (int j = 0; j < 10; j++) {
-            for (final Path f : files) {
-                assertNotNull(f);
-                if (Files.isRegularFile(f)) {
-                    final XmldbURI uri = test2.getURI().append(j + "_" + FileUtils.fileName(f));
-
-                    broker.storeDocument(transaction, uri, new FileInputSource(f), MimeType.BINARY_TYPE, test2);
-                    final BinaryDocument doc = (BinaryDocument) test2.getDocument(broker, uri);
-                    assertNotNull(doc);
+            for (final String modsFilename : Samples.SAMPLES.getModsXmlSampleNames()) {
+                final XmldbURI uri = test2.getURI().append(j + "_" + modsFilename);
+                final byte[] modsContent;
+                try (final InputStream is = SAMPLES.getModsSample(modsFilename)) {
+                    modsContent = InputStreamUtil.readAll(is);
                 }
+                broker.storeDocument(transaction, uri, new StringInputSource(modsContent), MimeType.BINARY_TYPE, test2);
+                final BinaryDocument doc = (BinaryDocument) test2.getDocument(broker, uri);
+                assertNotNull(doc);
             }
         }
     }
@@ -157,6 +176,7 @@ public class RecoverBinaryTest2 {
 
     @After
     public void stopDb() {
+        BrokerPool.FORCE_CORRUPTION = false;
         existEmbeddedServer.stopDb();
     }
 }
--- exist-core/src/test/java/org/exist/xquery/functions/xmldb/DbStore2Test.java
+++ exist-core/src/test/java/org/exist/xquery/functions/xmldb/DbStore2Test.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -50,7 +74,7 @@ import static org.junit.Assert.assertNotNull;
  * Due to limitation of ExistXmldbEmbeddedServer we need to split this test to two files.
  * It's not possible to have two instances of ExistXmldbEmbeddedServer at the same time.
  */
-public class DbStoreTest2 {
+public class DbStore2Test {
 
     @ClassRule
     public static final ExistXmldbEmbeddedServer existEmbeddedServerWithAnyURI = new ExistXmldbEmbeddedServer(false, true,
