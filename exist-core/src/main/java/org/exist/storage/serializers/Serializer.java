03929357e42f93eb03cb64aea9de33e7087db7aa
Adam Retter <adam.retter@googlemail.com>
2025-10-14 14:32:24 +0200

    [bugfix] Fix an issue with confusion between wrapping and typing in the serializer Closes https://github.com/eXist-db/exist/issues/5910

--- exist-core/src/main/java/org/exist/storage/serializers/Serializer.java
+++ exist-core/src/main/java/org/exist/storage/serializers/Serializer.java
@@ -1205,7 +1205,7 @@ public abstract class Serializer implements XMLReader {
                 serializeToReceiver(node, false);
             }
         } else {
-            if (wrap) {
+            if (typed) {
                 final AttrList attrs = new AttrList();
                 attrs.addAttribute(ATTR_TYPE_QNAME, Type.getTypeName(item.getType()));
                 receiver.startElement(ELEM_VALUE_QNAME, attrs);
@@ -1215,7 +1215,7 @@ public abstract class Serializer implements XMLReader {
             } catch (final XPathException e) {
                 throw new SAXException(e.getMessage(), e);
             }
-            if (wrap) {
+            if (typed) {
                 receiver.endElement(ELEM_VALUE_QNAME);
             }
         }
