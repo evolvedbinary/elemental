7d4d5cc6da13dd9fb283cc0cb04d6a94d67ba9e2
Adam Retter <adam.retter@googlemail.com>
2025-11-05 20:32:51 +0100

    [featue] Add a new xmldb:update#3 function that allows to update a single document

--- exist-core/src/main/java/org/exist/xquery/functions/xmldb/XMLDBModule.java
+++ exist-core/src/main/java/org/exist/xquery/functions/xmldb/XMLDBModule.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -64,7 +88,8 @@ public class XMLDBModule extends AbstractInternalModule {
             new FunctionDef(XMLDBLoadFromPattern.signatures[1], XMLDBLoadFromPattern.class),
             new FunctionDef(XMLDBLoadFromPattern.signatures[2], XMLDBLoadFromPattern.class),
             new FunctionDef(XMLDBLoadFromPattern.signatures[3], XMLDBLoadFromPattern.class),
-            new FunctionDef(XMLDBXUpdate.signature, XMLDBXUpdate.class),
+            new FunctionDef(XMLDBXUpdate.FS_UPDATE[0], XMLDBXUpdate.class),
+            new FunctionDef(XMLDBXUpdate.FS_UPDATE[1], XMLDBXUpdate.class),
             new FunctionDef(XMLDBCopy.FS_COPY_COLLECTION[0], XMLDBCopy.class),
             new FunctionDef(XMLDBCopy.FS_COPY_COLLECTION[1], XMLDBCopy.class),
             new FunctionDef(XMLDBCopy.FS_COPY_RESOURCE[0], XMLDBCopy.class),
