72ce76a2d7b37a0b8eabb3ec1f4af754edf5ccb4
Adam Retter <adam.retter@googlemail.com>
2025-11-05 23:04:30 +0100

    [bugfix] Avoid an NPE when there are no XUpdate Conditionals

--- exist-core/src/main/java/org/exist/xupdate/XUpdateProcessor.java
+++ exist-core/src/main/java/org/exist/xupdate/XUpdateProcessor.java
@@ -949,7 +949,7 @@ public class XUpdateProcessor implements ContentHandler, LexicalHandler {
 
     private @Nullable Conditional peekConditional() {
         if (conditionals == null) {
-            throw null;
+            return null;
         }
         return conditionals.peek();
     }
