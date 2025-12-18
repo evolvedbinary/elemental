7ba51e6bd07ed6ef10ab581f92cb6e2c157ff14a
Adam Retter <adam.retter@googlemail.com>
2025-12-18 17:34:07 +0100

    [bugfix] Fix an issue with //following::node(), //preceding::node(),  //function(following::node()), and //function(preceding::node()) whereby these were incorrectly parsed and flattened to just a descendant-or-self::node() lookup, resulting in incorrect results during evaluation.

--- exist-core/src/test/java/org/exist/xquery/XPathQueryTest.java
+++ exist-core/src/test/java/org/exist/xquery/XPathQueryTest.java
@@ -819,6 +819,54 @@ public class XPathQueryTest {
 
         result = queryResource(service, "siblings.xml", "//a/s[. = 'B']/following::s[2]", 1);
         assertThat(result.getResource(0).getContent().toString(), CompareMatcher.isIdenticalTo("<s>C</s>"));
+
+        String query = "declare variable $i := \n" +
+            "  <root>\n" +
+            "     <child/>\n" +
+            "     <child/>\n" +
+            "     <child>\n" +
+            "        <child2>\n" +
+            "           <child3>\n" +
+            "              <leaf/>\n" +
+            "           </child3>\n" +
+            "        </child2>\n" +
+            "     </child>\n" +
+            "  </root>;\n" +
+            "\n" +
+            "root($i)//following::node()";
+
+        result = service.query(query);
+        assertEquals(5, result.getSize());
+        assertThat(result.getResource(0).getContent().toString(), CompareMatcher.isIdenticalTo("<child/>"));
+        assertThat(result.getResource(1).getContent().toString(), CompareMatcher.isIdenticalTo("<child><child2><child3><leaf/></child3></child2></child>"));
+        assertThat(result.getResource(2).getContent().toString(), CompareMatcher.isIdenticalTo("<child2><child3><leaf/></child3></child2>"));
+        assertThat(result.getResource(3).getContent().toString(), CompareMatcher.isIdenticalTo("<child3><leaf/></child3>"));
+        assertThat(result.getResource(4).getContent().toString(), CompareMatcher.isIdenticalTo("<leaf/>"));
+
+        query = "declare variable $i := \n" +
+            "  <root>\n" +
+            "     <child/>\n" +
+            "     <child/>\n" +
+            "     <child>\n" +
+            "        <child2>\n" +
+            "           <child3>\n" +
+            "              <leaf/>\n" +
+            "           </child3>\n" +
+            "        </child2>\n" +
+            "     </child>\n" +
+            "  </root>;\n" +
+            "\n" +
+            "root($i)//count(following::node())";
+
+        result = service.query(query);
+        assertEquals(7, result.getSize());
+        assertEquals("0", result.getResource(0).getContent());
+        assertEquals("5", result.getResource(1).getContent());
+        assertEquals("4", result.getResource(2).getContent());
+        assertEquals("0", result.getResource(3).getContent());
+        assertEquals("0", result.getResource(4).getContent());
+        assertEquals("0", result.getResource(5).getContent());
+        assertEquals("0", result.getResource(6).getContent());
     }
 
     @Test
