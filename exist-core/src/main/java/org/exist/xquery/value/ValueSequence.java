1b8e9222b8a97334c52592ba9d2098ea15325e0f
Adam Retter <adam.retter@googlemail.com>
2025-12-30 17:08:18 +0100

    [bugfix] Fix a bug in removing duplicate in-memory nodes from a sequence

--- exist-core/src/main/java/org/exist/xquery/value/ValueSequence.java
+++ exist-core/src/main/java/org/exist/xquery/value/ValueSequence.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -455,15 +479,13 @@ public class ValueSequence extends AbstractSequence implements MemoryNodeSet {
             return;
         }
         if (inMemNodeSet) {
-            int j = 0;
+            int writeIdx = 1;
             for (int i = 1; i <= size; i++) {
-                if (!values[i].equals(values[j])) {
-                    if (i != ++j) {
-                        values[j] = values[i];
-                    }
+                if (!values[i].equals(values[i - 1])) {
+                    values[writeIdx++] = values[i];
                 }
             }
-            size = j;
+            size = writeIdx - 1;
         } else {
             if (itemType != Type.ANY_TYPE && Type.subTypeOf(itemType, Type.ATOMIC)) {
                 return;
