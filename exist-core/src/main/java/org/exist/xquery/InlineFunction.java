0a064ef0aae63205f23e64ffacd4f93dab3c8b2e
Adam Retter <adam.retter@googlemail.com>
2026-03-09 22:35:08 +0100

    [ignore] Code cleanup

--- exist-core/src/main/java/org/exist/xquery/InlineFunction.java
+++ exist-core/src/main/java/org/exist/xquery/InlineFunction.java
@@ -1,4 +1,28 @@
 /*
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
+ *
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; version 2.1.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * NOTE: Parts of this file contain code from 'The eXist-db Authors'.
+ *       The original license header is included below.
+ *
+ * =====================================================================
+ *
  * eXist-db Open Source Native XML Database
  * Copyright (C) 2001 The eXist-db Authors
  *
@@ -33,67 +57,65 @@ import org.exist.xquery.value.Type;
 
 /**
  * An XQuery 3.0 inline function expression.
- * 
- * @author wolf
  *
+ * @author wolf
  */
 public class InlineFunction extends AbstractExpression {
 
-	public final static QName INLINE_FUNCTION_QNAME = QName.EMPTY_QNAME;
-	
-	private UserDefinedFunction function;
-	private ArrayDeque<FunctionCall> calls = new ArrayDeque<>();
-	
+    public static final QName INLINE_FUNCTION_QNAME = QName.EMPTY_QNAME;
+
+    private final UserDefinedFunction function;
+    private final ArrayDeque<FunctionCall> calls = new ArrayDeque<>();
+
     private AnalyzeContextInfo cachedContextInfo;
 
-    public InlineFunction(XQueryContext context, UserDefinedFunction function) {
-		super(context);
-		this.function = function;
-	}
+    public InlineFunction(final XQueryContext context, final UserDefinedFunction function) {
+        super(context);
+        this.function = function;
+    }
 
-	@Override
-	public void analyze(AnalyzeContextInfo contextInfo) throws XPathException {
-
+    @Override
+    public void analyze(final AnalyzeContextInfo contextInfo) throws XPathException {
         cachedContextInfo = new AnalyzeContextInfo(contextInfo);
         cachedContextInfo.addFlag(SINGLE_STEP_EXECUTION);
         cachedContextInfo.setParent(this);
-	}
-
-	@Override
-	public void dump(ExpressionDumper dumper) {
-		dumper.display("function");
-		function.dump(dumper);
-	}
-
-	/**
-	 * Wraps a function call around the function and returns a
-	 * reference to it. Make sure local variables in the context
-	 * are visible.
-	 */
-	public Sequence eval(Sequence contextSequence, Item contextItem)
-			throws XPathException {
-		// local variable context is known within inline function
-		final List<ClosureVariable> closureVars = context.getLocalStack();
-		
-		final FunctionCall call = new FunctionCall(context, function);
-		call.getFunction().setClosureVariables(closureVars);
-		call.setLocation(function.getLine(), function.getColumn());
-		call.analyze(new AnalyzeContextInfo(cachedContextInfo));
-
-		// push the created function call to the stack so we can clear
-        // it after execution
-		calls.push(call);
-
-		return new FunctionReference(this, call);
-	}
-
-	@Override
-	public int returnsType() {
-		return Type.FUNCTION;
-	}
+    }
 
     @Override
-    public void resetState(boolean postOptimization) {
+    public void dump(final ExpressionDumper dumper) {
+        dumper.display("function");
+        function.dump(dumper);
+    }
+
+    /**
+     * Wraps a function call around the function and returns a
+     * reference to it. Make sure local variables in the context
+     * are visible.
+     */
+    @Override
+    public Sequence eval(final Sequence contextSequence, final Item contextItem) throws XPathException {
+        // local variable context is known within inline function
+        final List<ClosureVariable> closureVars = context.getLocalStack();
+
+        final FunctionCall call = new FunctionCall(context, function);
+        call.getFunction().setClosureVariables(closureVars);
+        call.setLocation(function.getLine(), function.getColumn());
+        call.analyze(new AnalyzeContextInfo(cachedContextInfo));
+
+        // push the created function call to the stack so we can clear
+        // it after execution
+        calls.push(call);
+
+        return new FunctionReference(this, call);
+    }
+
+    @Override
+    public int returnsType() {
+        return Type.FUNCTION;
+    }
+
+    @Override
+    public void resetState(final boolean postOptimization) {
         super.resetState(postOptimization);
         calls.clear();
         function.resetState(postOptimization);
