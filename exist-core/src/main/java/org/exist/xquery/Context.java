f702de7eac0ee6a807bcd0ed4c4be1fbd9ad387f
Adam Retter <adam.retter@googlemail.com>
2026-03-10 14:22:10 +0100

    [bugfix] The XQueryContext used by the XPath function util:eval must be correctly reset to prevent a leak of Update Listeners which can hold large context sequences from Binding Expressions

--- exist-core/src/main/java/org/exist/xquery/Context.java
+++ exist-core/src/main/java/org/exist/xquery/Context.java
@@ -83,6 +83,12 @@ public interface Context {
 
     XQueryContext getRootContext();
 
+    /**
+     * Create a new context ("Inner Context") that will share some state with this context.
+     * After calling this {@link #setShared(boolean)} should be set to true.
+     *
+     * @return an inner context.
+     */
     XQueryContext copyContext();
 
     /**
@@ -338,6 +344,13 @@ public interface Context {
      */
     boolean lockDocumentsOnLoad();
 
+    /**
+     * When set as true, it indicates that this Context (often known as the Inner Context)
+     * shares some state with another Context (often known as the Outer Context).
+     * This should be set to true after any call to {@link #copyContext()}.
+     *
+     * @param shared set to true if this context shares state with another context, false otherwise.
+     */
     void setShared(boolean shared);
 
     boolean isShared();
