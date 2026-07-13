9faaec1713c69c5864c723c7cec1054a71c21985
Adam Retter <adam.retter@googlemail.com>
2026-07-13 03:15:40 +0200

    [feature] New implementation of system:get-module-load-path() XQuery function

--- exist-core/src/main/java/org/exist/xquery/functions/system/GetModuleLoadPath.java
+++ exist-core/src/main/java/org/exist/xquery/functions/system/GetModuleLoadPath.java
@@ -1,14 +1,13 @@
 /*
- * eXist-db Open Source Native XML Database
- * Copyright (C) 2001 The eXist-db Authors
+ * Elemental
+ * Copyright (C) 2024, Evolved Binary Ltd
  *
- * info@exist-db.org
- * http://www.exist-db.org
+ * admin@evolvedbinary.com
+ * https://www.evolvedbinary.com | https://www.elemental.xyz
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * License as published by the Free Software Foundation; version 2.1.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,41 +20,32 @@
  */
 package org.exist.xquery.functions.system;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.exist.dom.QName;
 import org.exist.xquery.BasicFunction;
-import org.exist.xquery.Cardinality;
 import org.exist.xquery.FunctionSignature;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
-import org.exist.xquery.value.FunctionReturnSequenceType;
 import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.StringValue;
 import org.exist.xquery.value.Type;
 
+import static org.exist.xquery.FunctionDSL.returns;
+import static org.exist.xquery.functions.system.SystemModule.functionSignature;
+
 public class GetModuleLoadPath extends BasicFunction {
 
-    protected final static Logger logger = LogManager.getLogger(GetModuleLoadPath.class);
+    private static final String FS_GET_MODULE_LOAD_PATH_NAME = "get-module-load-path";
+    public static final FunctionSignature FS_GET_MODULE_LOAD_PATH = functionSignature(
+			FS_GET_MODULE_LOAD_PATH_NAME,
+			"Returns the path from which the module was loaded. Either a filesystem directory, or database collection. If the path cannot be determined, for example because the query module is in-memory then '.' is returned.",
+			returns(Type.STRING, "The path from which the module was loaded")
+    );
 
-    public final static FunctionSignature signature =
-		new FunctionSignature(
-			new QName("get-module-load-path", SystemModule.NAMESPACE_URI, SystemModule.PREFIX),
-			"Returns the module load path from the current query context. The module load path " +
-            "corresponds to the location on the file system from where modules are loaded " +
-            "into an XQuery. This is usually the directory from which the main XQuery was " +
-            "compiled, or - when executing a stored XQuery - the collection in which the main " +
-            "query resides. The module load path " +
-            "is also used to resolve relative XInclude paths.",
-			FunctionSignature.NO_ARGS,
-			new FunctionReturnSequenceType(Type.STRING, Cardinality.EXACTLY_ONE, "the load path"));
-
-
-    public GetModuleLoadPath(XQueryContext context) {
-        super(context, signature);
+    public GetModuleLoadPath(final XQueryContext context) {
+        super(context, FS_GET_MODULE_LOAD_PATH_);
     }
 
-    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
+    @Override
+    public Sequence eval(final Sequence[] args, final Sequence contextSequence) throws XPathException {
         return new StringValue(this, context.getModuleLoadPath());
     }
 }
