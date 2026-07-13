d04c0a4398f3819bc22a6834803ecbde90dab63a
Adam Retter <adam.retter@googlemail.com>
2026-07-13 03:31:53 +0200

    [feature] Add new system:get-main-module-load-path() XQuery function (required by TEI Publisher)

--- exist-core/src/main/java/org/exist/xquery/functions/system/GetModuleLoadPath.java
+++ exist-core/src/main/java/org/exist/xquery/functions/system/GetModuleLoadPath.java
@@ -28,6 +28,8 @@ import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.StringValue;
 import org.exist.xquery.value.Type;
 
+import javax.annotation.Nullable;
+
 import static org.exist.xquery.FunctionDSL.returns;
 import static org.exist.xquery.functions.system.SystemModule.functionSignature;
 
@@ -40,12 +42,30 @@ public class GetModuleLoadPath extends BasicFunction {
 			returns(Type.STRING, "The path from which the module was loaded")
     );
 
-    public GetModuleLoadPath(final XQueryContext context) {
-        super(context, FS_GET_MODULE_LOAD_PATH_);
+    private static final String FS_GET_MAIN_MODULE_LOAD_PATH_NAME = "get-main-module-load-path";
+    public static final FunctionSignature FS_GET_MAIN_MODULE_LOAD_PATH = functionSignature(
+            FS_GET_MAIN_MODULE_LOAD_PATH_NAME,
+            "Returns the path from which the main module was loaded. If called from a library module, it finds the path of the ancestor main module. Either a filesystem directory, or database collection. If the path cannot be determined, for example because the query module is in-memory then '.' is returned.",
+            returns(Type.STRING, "The path from which the main module was loaded")
+    );
+
+    public GetModuleLoadPath(final XQueryContext context, final FunctionSignature signature) {
+        super(context, signature);
     }
 
     @Override
     public Sequence eval(final Sequence[] args, final Sequence contextSequence) throws XPathException {
-        return new StringValue(this, context.getModuleLoadPath());
+        final String moduleLoadPath;
+        if (isCalledAs(FS_GET_MODULE_LOAD_PATH_NAME)) {
+           moduleLoadPath = context.getModuleLoadPath();
+        } else {
+            @Nullable final XQueryContext rootContext = context.getRootContext();
+            if (rootContext == null) {
+                // there may not be a main module, e.g. RESTXQ resource functions
+                throw new XPathException(this, "There is no root context as the library module was executed via a named function");
+            }
+            moduleLoadPath = rootContext.getModuleLoadPath();
+        }
+        return new StringValue(this, moduleLoadPath);
     }
 }
