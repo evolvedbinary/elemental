939573f125e741a15d45e18f462f05d819784409
marmoure <tinky93winky@gmail.com>
2026-09-09 18:43:51 +0200

    [bugfix] Typo

--- exist-core/src/main/java/org/exist/xquery/functions/fn/FnModule.java
+++ exist-core/src/main/java/org/exist/xquery/functions/fn/FnModule.java
@@ -300,7 +300,7 @@ public class FnModule extends AbstractInternalModule {
         new FunctionDef(FnRandomNumberGenerator.FS_RANDOM_NUMBER_GENERATOR[1], FnRandomNumberGenerator.class),
         new FunctionDef(FunContainsToken.FS_CONTAINS_TOKEN[0], FunContainsToken.class),
         new FunctionDef(FunContainsToken.FS_CONTAINS_TOKEN[1], FunContainsToken.class),
-        new FunctionDef(FnInvisibleXml.FNS_INVISIBLE_XML, FnInvisibleXml.class)
+        new FunctionDef(FnInvisibleXml.FS_INVISIBLE_XML, FnInvisibleXml.class)
     };
 
     static {
